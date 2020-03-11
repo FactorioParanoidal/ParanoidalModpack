be5cda8cef67ecbe7b26c5189174db59216b023e
drd-avel <54510478+drd-avel@users.noreply.github.com>
2020-03-11 21:22:57 +0300

    Удаление сломавшегося мода, заменён новым.

--- mods/air-filtering-patched_9.17.1/migrations/0.4.2.lua
+++
@@ -1,6 +0,0 @@
-for index, force in pairs(game.forces) do
-  local technologies = force.technologies
-  local recipes = force.recipes
-
-  recipes["unused-air-filter"].enabled = technologies["air-filtering"].researched
-end
