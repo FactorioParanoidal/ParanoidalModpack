44e30031d2c607969bd1d40c8b0bd8dab4a189dd
drd-avel <avelinadmitrieva@gmail.com>
2020-03-18 19:53:31 +0300

    Добавлен мод на ветряки с переработанной логикой от Yur_Ra. Всё ещё требуется уменьшить размер самого мощного ветряка.

---
+++ mods/Texugo_windgenerator_9.17.0/settings.lua
@@ -0,0 +1,12 @@
+data:extend(
+{
+  {
+    type = "int-setting",
+    name = "texugo-wind-power",
+    setting_type = "startup",
+    default_value = 1,
+    maximum_value = 10,
+    minimum_value = 0,
+  }
+ }
+)
