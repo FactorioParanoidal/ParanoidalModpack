928b5b9953ec12e9ba4a1446ee4f22924cc3e1f8
drd-avel <avelinadmitrieva@gmail.com>
2020-03-19 23:45:18 +0300

    Важные изменения по теплотворности топлива (моды ангела перебивали нормальные показатели)

--- mods/bobpower_0.18.3/data-updates.lua
+++ mods/bobpower_0.18.3/data-updates.lua
@@ -15,13 +15,13 @@ require("prototypes.burner-reactor-updates")
 require("prototypes.heat-pipes-updates")
 
 
-data.raw.fluid["crude-oil"].fuel_value = "1.9MJ" --"3.8MJ"
+--data.raw.fluid["crude-oil"].fuel_value = "1.9MJ" --"3.8MJ" --DrD
 data.raw.fluid["crude-oil"].emissions_multiplier = 10
-data.raw.fluid["light-oil"].fuel_value = "1.5MJ" --"3MJ"
+--data.raw.fluid["light-oil"].fuel_value = "1.5MJ" --"3MJ" --DrD
 data.raw.fluid["light-oil"].emissions_multiplier = 2
-data.raw.fluid["heavy-oil"].fuel_value = "1MJ" --"2MJ"
+--data.raw.fluid["heavy-oil"].fuel_value = "1MJ" --"2MJ" --DrD
 data.raw.fluid["heavy-oil"].emissions_multiplier = 3
-data.raw.fluid["petroleum-gas"].fuel_value = "2.3MJ" --"4.6MJ"
+--data.raw.fluid["petroleum-gas"].fuel_value = "2.3MJ" --"4.6MJ" --DrD
 
 data.raw.item["coal"].fuel_emissions_multiplier = 2
 data.raw.item["solid-fuel"].fuel_emissions_multiplier = 0.8
