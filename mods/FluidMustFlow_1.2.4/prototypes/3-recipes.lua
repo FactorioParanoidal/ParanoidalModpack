a19dc51d3ea1219aacf70a8f7400fb60c7e07757
drd-avel <avelinadmitrieva@gmail.com>
2020-03-24 23:44:18 +0300

    Сжатие графики модов

--- mods/FluidMustFlow_1.2.4/prototypes/3-recipes.lua
+++ mods/FluidMustFlow_1.2.4/prototypes/3-recipes.lua
@@ -9,7 +9,7 @@ data:extend
 		enabled = "false",
 		ingredients = 
         {
-            {type="item", name="iron-plate", amount=4}
+            {type="item", name="steel-plate", amount=40}
         },		
 		result = "duct-small",
 		result_count = 1,
@@ -22,7 +22,7 @@ data:extend
 		enabled = "false",
 		ingredients = 
         {
-            {type="item", name="iron-plate", amount=8}
+            {type="item", name="steel-plate", amount=80}
         },		
 		result = "duct-t-junction",
 		result_count = 1,
@@ -35,7 +35,7 @@ data:extend
 		enabled = "false",
 		ingredients = 
         {
-            {type="item", name="iron-plate", amount=8}
+            {type="item", name="steel-plate", amount=80}
         },		
 		result = "duct-curve",
 		result_count = 1,
@@ -48,7 +48,7 @@ data:extend
 		enabled = "false",
 		ingredients = 
         {
-            {type="item", name="iron-plate", amount=8}
+            {type="item", name="steel-plate", amount=80}
         },		
 		result = "duct-cross",
 		result_count = 1,
@@ -61,7 +61,7 @@ data:extend
 		enabled = "false",
 		ingredients = 
         {
-            {type="item", name="iron-plate", amount=60}
+            {type="item", name="steel-plate", amount=400}
         },		
 		result = "duct-underground",
 		result_count = 2,
@@ -74,8 +74,8 @@ data:extend
 		enabled = "false",
 		ingredients = 
 		{
-			{type="item", name="iron-plate", amount=8},
-			{type="item", name="iron-gear-wheel", amount=4}
+			{type="item", name="steel-plate", amount=80},
+			{type="item", name="steel-gear-wheel", amount=40}
 		},		
 		result = "non-return-duct",
 		result_count = 1,
@@ -90,7 +90,7 @@ data:extend
         {
 			{type="item", name="engine-unit", amount=6},		
             {type="item", name="pipe", amount=6},
-			{type="item", name="steel-plate", amount=6}
+			{type="item", name="steel-plate", amount=60}
         },		
 		result = "duct-end-point-intake",
 		result_count = 1,
@@ -105,7 +105,7 @@ data:extend
         {
 			{type="item", name="engine-unit", amount=6},		
             {type="item", name="pipe", amount=6},
-			{type="item", name="steel-plate", amount=6}
+			{type="item", name="steel-plate", amount=60}
         },		
 		result = "duct-end-point-outtake",
 		result_count = 1,
@@ -123,7 +123,7 @@ if not settings.startup["fmf-enable-duct-auto-join"].value then
 			enabled = "false",
 			ingredients = 
 			{
-				{type="item", name="iron-plate", amount=8}
+				{type="item", name="steel-plate", amount=80}
 			},		
 			result = "duct",
 			result_count = 1,
@@ -136,7 +136,7 @@ if not settings.startup["fmf-enable-duct-auto-join"].value then
 			enabled = "false",
 			ingredients = 
 			{
-				{type="item", name="iron-plate", amount=16}
+				{type="item", name="steel-plate", amount=160}
 			},		
 			result = "duct-long",
 			result_count = 1,
