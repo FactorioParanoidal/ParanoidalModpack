d3d91aff3aafa65694fe4f9c050275534d36a632
drd-avel <avelinadmitrieva@gmail.com>
2020-03-24 23:50:33 +0300

    Незначительные фиксы

--- mods/marathon_9.1.17/prototypes/new.lua
+++ mods/marathon_9.1.17/prototypes/new.lua
@@ -20,23 +20,27 @@ bobmods.lib.recipe.add_result("chrome-ore-processing", (type="item", name="slag"
 bobmods.lib.recipe.add_result("platinum-ore-processing", (type="item", name="slag", amount=1, probability=0.1,))
 
 ]]
-
-marathon.update_recipe("iron-ore-processing",
-{
-    {
+data:extend(
+--marathon.update_recipe("iron-ore-processing",
+  {
     type = "recipe",
     name = "iron-ore-processing",
     category = "ore-processing",
-	subgroup = "angels-iron",
+    subgroup = "angels-iron",
     energy_required = 2,
-	enabled = "false",
-    ingredients ={{"iron-ore", 4}},
-    results=
+    enabled = "false",
+    ingredients =
+    {
+      {"iron-ore", 4}
+    },
+    results =
     {
       {type="item", name="processed-iron", amount=2},
-	  {type="item", name="slag", amount=1, probability=0.5},
+	  {type="item", name="slag", amount=1, probability=0.5}, --DrD
     },
+	icon = "__angelssmelting__/graphics/icons/processed-iron.png", --DrD
 	icon_size = 32,
     order = "aa",
-    },
-})
+    }
+)
+
