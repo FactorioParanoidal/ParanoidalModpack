314bf74a11494e80b845dc511e8ea62b8d52162d
drd-avel <avelinadmitrieva@gmail.com>
2020-03-25 16:27:00 +0300

    Попытка вырезать назойливый Picker Clamp Tool чтобы трубы хорошо соединялись

--- mods/PickerPipeTools_1.1.0/data.lua
+++ mods/PickerPipeTools_1.1.0/data.lua
@@ -1,4 +1,4 @@
 require('prototypes/pipe-highlight')
 require('prototypes/pipe-cleaner')
 require('prototypes/controls')
-require('prototypes/pipe-clamp-tool')
+--require('prototypes/pipe-clamp-tool')
