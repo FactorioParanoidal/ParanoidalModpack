c55abfac0882f77bcb179d600d5ef5c1dca07135
drd-avel <avelinadmitrieva@gmail.com>
2020-03-31 02:11:34 +0300

    Массивное обновление и улучшение качества графики иконок вплоть до 128х128, почти все металлы, слитки, электроника и т.д. Продолжение следует.

---
+++ mods/bobicons_0.18.1/data-final-fixes.lua
@@ -0,0 +1,232 @@
+-- CIRCUITS --
+
+if data.raw.item["advanced-circuit"]  then data.raw.item["advanced-circuit"].icon = "__bobicons__/graphics/icons/electronics/electronic-circuit-board-64.png"
+data.raw.item["advanced-circuit"].icon_size = 64 end
+if data.raw.item["advanced-processing-unit"]  then data.raw.item["advanced-processing-unit"].icon = "__bobicons__/graphics/icons/electronics/electronic-processing-board-64.png"
+data.raw.item["advanced-processing-unit"].icon_size = 64 end 
+if data.raw.item["basic-circuit-board"]  then data.raw.item["basic-circuit-board"].icon = "__bobicons__/graphics/icons/electronics/basic-circuit-board-64.png"
+data.raw.item["basic-circuit-board"].icon_size = 64 end
+if data.raw.item["basic-electronic-components"]  then data.raw.item["basic-electronic-components"].icon = "__bobicons__/graphics/icons/electronics/basic-electronic-components-64.png"
+data.raw.item["basic-electronic-components"].icon_size = 64 end
+if data.raw.item["electronic-components"]  then data.raw.item["electronic-components"].icon = "__bobicons__/graphics/icons/electronics/electronic-components-64.png"
+data.raw.item["electronic-components"].icon_size = 64 end
+if data.raw.item["circuit-board"]  then data.raw.item["circuit-board"].icon = "__bobicons__/graphics/icons/electronics/circuit-board-64.png"
+data.raw.item["circuit-board"].icon_size = 64 end 
+if data.raw.item["electronic-circuit"]  then data.raw.item["electronic-circuit"].icon = "__bobicons__/graphics/icons/electronics/basic-electronic-circuit-board-64.png"
+data.raw.item["electronic-circuit"].icon_size = 64 end
+if data.raw.item["fibreglass-board"]  then data.raw.item["fibreglass-board"].icon = "__bobicons__/graphics/icons/electronics/fibreglass-board-64.png"
+data.raw.item["fibreglass-board"].icon_size = 64 end 
+if data.raw.item["intergrated-electronics"]  then data.raw.item["intergrated-electronics"].icon = "__bobicons__/graphics/icons/electronics/integrated-electronics-64.png"
+data.raw.item["intergrated-electronics"].icon_size = 64 end 
+if data.raw.item["multi-layer-circuit-board"]  then data.raw.item["multi-layer-circuit-board"].icon = "__bobicons__/graphics/icons/electronics/multi-layer-circuit-board-64.png"
+data.raw.item["multi-layer-circuit-board"].icon_size = 64 end
+if data.raw.item["phenolic-board"]  then data.raw.item["phenolic-board"].icon = "__bobicons__/graphics/icons/electronics/phenolic-board-64.png"
+data.raw.item["phenolic-board"].icon_size = 64 end 
+if data.raw.item["processing-electronics"]  then data.raw.item["processing-electronics"].icon = "__bobicons__/graphics/icons/electronics/cpu-64.png"
+data.raw.item["processing-electronics"].icon_size = 64 end
+if data.raw.item["processing-unit"]  then data.raw.item["processing-unit"].icon = "__bobicons__/graphics/icons/electronics/electronic-logic-board-64.png"
+data.raw.item["processing-unit"].icon_size = 64 end 
+if data.raw.item["superior-circuit-board"]  then data.raw.item["superior-circuit-board"].icon = "__bobicons__/graphics/icons/electronics/superior-circuit-board-64.png"
+data.raw.item["superior-circuit-board"].icon_size = 64 end 
+if data.raw.item["wooden-board"]  then data.raw.item["wooden-board"].icon = "__bobicons__/graphics/icons/electronics/wooden-board-64.png" data.raw.item["wooden-board"].icon_size = 64 end 
+
+if data.raw.item["simple-io"] then
+data.raw.item["simple-io"].icon = "__bobicons__/graphics/icons/electronics/simple-io-128.png"
+data.raw.item["simple-io"].icon_size = 128
+data.raw.item["standart-io"].icon = "__bobicons__/graphics/icons/electronics/standart-io-128.png"
+data.raw.item["standart-io"].icon_size = 128
+data.raw.item["advanced-io"].icon = "__bobicons__/graphics/icons/electronics/advanced-io-128.png"
+data.raw.item["advanced-io"].icon_size = 128
+data.raw.item["predictive-io"].icon = "__bobicons__/graphics/icons/electronics/predictive-io-128.png"
+data.raw.item["predictive-io"].icon_size = 128 
+data.raw.item["intelligent-io"].icon = "__bobicons__/graphics/icons/electronics/intelligent-io-128.png"
+data.raw.item["intelligent-io"].icon_size = 128 
+end
+if data.raw.item["basic-structure-components"] then
+data.raw.item["basic-structure-components"].icon = "__bobicons__/graphics/icons/other/basicSC-64.png"
+data.raw.item["basic-structure-components"].icon_size = 64
+data.raw.item["intermediate-structure-components"].icon = "__bobicons__/graphics/icons/other/intermediateSC-64.png"
+data.raw.item["intermediate-structure-components"].icon_size = 64
+data.raw.item["advanced-structure-components"].icon = "__bobicons__/graphics/icons/other/advancedSC-64.png"
+data.raw.item["advanced-structure-components"].icon_size = 64
+data.raw.item["anotherworld-structure-components"].icon = "__bobicons__/graphics/icons/other/anotherworldSC-64.png"
+data.raw.item["anotherworld-structure-components"].icon_size = 64 
+end
+if data.raw.item["condensator"] then
+data.raw.item["condensator"].icon = "__bobicons__/graphics/icons/electronics/condensator-128.png"
+data.raw.item["condensator"].icon_size = 128
+data.raw.item["condensator2"].icon = "__bobicons__/graphics/icons/electronics/condensator2-128.png"
+data.raw.item["condensator2"].icon_size = 128
+data.raw.item["condensator3"].icon = "__bobicons__/graphics/icons/electronics/condensator3-128.png"
+data.raw.item["condensator3"].icon_size = 128
+end
+
+-- PLATES --
+
+if data.raw.item["aluminium-plate"] then data.raw.item["aluminium-plate"].icon = "__bobicons__/graphics/icons/plate/aluminium-plate-128.png"
+data.raw.item["aluminium-plate"].icon_size = 128 end
+if data.raw.item["cobalt-plate"] then data.raw.item["cobalt-plate"].icon = "__bobicons__/graphics/icons/plate/cobalt-plate-128.png"
+data.raw.item["cobalt-plate"].icon_size = 128 end
+if data.raw.item["copper-plate"] then data.raw.item["copper-plate"].icon = "__bobicons__/graphics/icons/plate/copper-plate-128.png"
+data.raw.item["copper-plate"].icon_size = 128 end
+if data.raw.item["gold-plate"] then data.raw.item["gold-plate"].icon = "__bobicons__/graphics/icons/plate/gold-plate-128.png"
+data.raw.item["gold-plate"].icon_size = 128 end
+if data.raw.item["lead-plate"] then data.raw.item["lead-plate"].icon = "__bobicons__/graphics/icons/plate/lead-plate-128.png"
+data.raw.item["lead-plate"].icon_size = 128 end
+if data.raw.item["lithium"] then data.raw.item["lithium"].icon = "__bobicons__/graphics/icons/plate/lithium-128.png"
+data.raw.item["lithium"].icon_size = 128 end
+if data.raw.item["iron-plate"] then data.raw.item["iron-plate"].icon = "__bobicons__/graphics/icons/plate/iron-plate-128.png"
+data.raw.item["iron-plate"].icon_size = 128 end
+if data.raw.item["nickel-plate"]  then data.raw.item["nickel-plate"].icon = "__bobicons__/graphics/icons/plate/nickel-plate-128.png"
+data.raw.item["nickel-plate"].icon_size = 128 end
+if data.raw.item["silicon"] then data.raw.item["silicon"].icon = "__bobicons__/graphics/icons/plate/silicon-128.png"
+data.raw.item["silicon"].icon_size = 128 end
+if data.raw.item["silver-plate"] then data.raw.item["silver-plate"].icon = "__bobicons__/graphics/icons/plate/silver-plate-128.png"
+data.raw.item["silver-plate"].icon_size = 128 end
+if data.raw.item["steel-plate"] then data.raw.item["steel-plate"].icon = "__bobicons__/graphics/icons/plate/steel-plate-128.png"
+data.raw.item["steel-plate"].icon_size = 128 end
+if data.raw.item["tin-plate"] then data.raw.item["tin-plate"].icon = "__bobicons__/graphics/icons/plate/tin-plate-128.png"
+data.raw.item["tin-plate"].icon_size = 128 end
+if data.raw.item["titanium-plate"] then data.raw.item["titanium-plate"].icon = "__bobicons__/graphics/icons/plate/titanium-plate-128.png"
+data.raw.item["titanium-plate"].icon_size = 128 end
+if data.raw.item["tungsten-plate"] then data.raw.item["tungsten-plate"].icon = "__bobicons__/graphics/icons/plate/tungsten-plate-128.png"
+data.raw.item["tungsten-plate"].icon_size = 128 end
+if data.raw.item["zinc-plate"] then data.raw.item["zinc-plate"].icon = "__bobicons__/graphics/icons/plate/zinc-plate-128.png"
+data.raw.item["zinc-plate"].icon_size = 128 end
+
+
+-- PLATES - Alloys --
+-- name of items: bronze-alloy, brass-alloy, cobalt-steel-alloy, copper-tungsten-alloy, electrum-alloy, gunmetal-alloy, invar-alloy, nitinol-alloy, solder-alloy, tungsten-carbide
+
+if data.raw.item["bronze-alloy"]  then data.raw.item["bronze-alloy"].icon = "__bobicons__/graphics/icons/plate/bronze-alloy-128.png"
+data.raw.item["bronze-alloy"].icon_size = 128 end
+if data.raw.item["brass-alloy"]  then data.raw.item["brass-alloy"].icon = "__bobicons__/graphics/icons/plate/brass-alloy-128.png"
+data.raw.item["brass-alloy"].icon_size = 128 end
+if data.raw.item["cobalt-steel-alloy"]  then data.raw.item["cobalt-steel-alloy"].icon = "__bobicons__/graphics/icons/plate/cobalt-steel-alloy-128.png"
+data.raw.item["cobalt-steel-alloy"].icon_size = 128 end
+if data.raw.item["copper-tungsten-alloy"]  then data.raw.item["copper-tungsten-alloy"].icon = "__bobicons__/graphics/icons/plate/copper-tungsten-alloy-128.png"
+data.raw.item["copper-tungsten-alloy"].icon_size = 128 end
+if data.raw.item["electrum-alloy"] then data.raw.item["electrum-alloy"].icon = "__bobicons__/graphics/icons/plate/electrum-alloy-128.png"
+data.raw.item["electrum-alloy"].icon_size = 128 end
+if data.raw.item["gunmetal-alloy"]  then data.raw.item["gunmetal-alloy"].icon = "__bobicons__/graphics/icons/plate/gunmetal-alloy-128.png"
+data.raw.item["gunmetal-alloy"].icon_size = 128 end
+if data.raw.item["invar-alloy"]  then data.raw.item["invar-alloy"].icon = "__bobicons__/graphics/icons/plate/invar-alloy-128.png"
+data.raw.item["invar-alloy"].icon_size = 128 end
+if data.raw.item["nitinol-alloy"] then data.raw.item["nitinol-alloy"].icon = "__bobicons__/graphics/icons/plate/nitinol-alloy-128.png"
+data.raw.item["nitinol-alloy"].icon_size = 128 end
+if data.raw.item["solder-alloy"] then data.raw.item["solder-alloy"].icon = "__bobicons__/graphics/icons/plate/solder-alloy-128.png"
+data.raw.item["solder-alloy"].icon_size = 128 end
+if data.raw.item["tungsten-carbide"] then data.raw.item["tungsten-carbide"].icon = "__bobicons__/graphics/icons/plate/tungsten-carbide-128.png"
+data.raw.item["tungsten-carbide"].icon_size = 128 end
+
+
+
+-- ORES --
+
+if data.raw.item["gem-ore"] then data.raw.item["gem-ore"].icon = "__bobicons__/graphics/icons/ores/gem-ore.png" end
+
+-- INTERMEDIATES --
+data.raw.item["iron-gear-wheel"].icon = "__bobicons__/graphics/icons/intermediates/iron-gear-wheel-128.png"
+data.raw.item["iron-gear-wheel"].icon_size = 128
+
+if data.raw.item["green-wire"]  then data.raw.item["green-wire"].icon = "__bobicons__/graphics/icons/electronics/green-wire-128.png"
+data.raw.item["green-wire"].icon_size = 128
+data.raw.item["red-wire"].icon = "__bobicons__/graphics/icons/electronics/red-wire-128.png"
+data.raw.item["red-wire"].icon_size = 128 end
+
+if data.raw.item["gilded-copper-cable"]  then data.raw.item["gilded-copper-cable"].icon = "__bobicons__/graphics/icons/intermediates/gilded-copper-cable-128.png"
+data.raw.item["gilded-copper-cable"].icon_size = 128 end
+if data.raw.item["silver-cable"]  then data.raw.item["silver-cable"].icon = "__bobicons__/graphics/icons/intermediates/silver-cable-128.png"
+data.raw.item["silver-cable"].icon_size = 128 end
+if data.raw.item["resin"]  then data.raw.item["resin"].icon = "__bobicons__/graphics/icons/intermediates/resin-128.png"
+data.raw.item["resin"].icon_size = 128 end
+if data.raw.item["battery"] then data.raw.item["battery"].icon = "__bobicons__/graphics/icons/intermediates/battery-128.png"
+data.raw.item["battery"].icon_size = 128 end
+if data.raw.item["lithium-ion-battery"] then data.raw.item["lithium-ion-battery"].icon = "__bobicons__/graphics/icons/intermediates/lithium-ion-battery-128.png"
+data.raw.item["lithium-ion-battery"].icon_size = 128 end
+if data.raw.item["silver-zinc-battery"] then data.raw.item["silver-zinc-battery"].icon = "__bobicons__/graphics/icons/intermediates/silver-zinc-battery-128.png"
+data.raw.item["silver-zinc-battery"].icon_size = 128 end
+
+-- SCIENCE --
+
+data.raw.tool["automation-science-pack"].icon_size = 128
+data.raw.tool["automation-science-pack"].icon = "__bobicons__/graphics/icons/science/automation-science-pack-128.png"
+data.raw.tool["logistic-science-pack"].icon_size = 128
+data.raw.tool["logistic-science-pack"].icon = "__bobicons__/graphics/icons/science/logistic-science-pack-128.png"
+data.raw.tool["chemical-science-pack"].icon_size = 128
+data.raw.tool["chemical-science-pack"].icon = "__bobicons__/graphics/icons/science/chemical-science-pack-128.png"
+data.raw.tool["utility-science-pack"].icon_size = 128
+data.raw.tool["utility-science-pack"].icon = "__bobicons__/graphics/icons/science/utility-science-pack-128.png"
+data.raw.tool["military-science-pack"].icon_size = 128
+data.raw.tool["military-science-pack"].icon = "__bobicons__/graphics/icons/science/military-science-pack-128.png"
+data.raw.tool["production-science-pack"].icon_size = 128
+data.raw.tool["production-science-pack"].icon = "__bobicons__/graphics/icons/science/production-science-pack-128.png"
+if data.raw.tool["logistic-science-pack-2"] then data.raw.tool["logistic-science-pack-2"].icon = "__bobicons__/graphics/icons/science/logistic-science-pack-2-128.png"
+data.raw.tool["logistic-science-pack-2"].icon_size = 128 end
+data.raw.tool["space-science-pack"].icon_size = 128
+data.raw.tool["space-science-pack"].icon = "__bobicons__/graphics/icons/science/space-science-pack-128.png"
+
+-- ALIEN -- (bobenemies)
+
+if data.raw.item["alien-artifact"] then data.raw.item["alien-artifact"].icon = "__bobicons__/graphics/icons/alien/alien-artifact-128.png"
+ data.raw.item["alien-artifact"].icon_size = 128 end
+ 
+if data.raw.item["alien-artifact-blue"] then
+data.raw.item["alien-artifact-blue"].icon = "__bobicons__/graphics/icons/alien/alien-artifact-blue-128.png"
+data.raw.item["alien-artifact-blue"].icon_size = 128
+data.raw.item["alien-artifact-green"].icon = "__bobicons__/graphics/icons/alien/alien-artifact-green-128.png"
+data.raw.item["alien-artifact-green"].icon_size = 128
+data.raw.item["alien-artifact-orange"].icon = "__bobicons__/graphics/icons/alien/alien-artifact-orange-128.png"
+data.raw.item["alien-artifact-orange"].icon_size = 128
+data.raw.item["alien-artifact-purple"].icon = "__bobicons__/graphics/icons/alien/alien-artifact-purple-128.png"
+data.raw.item["alien-artifact-purple"].icon_size = 128
+data.raw.item["alien-artifact-red"].icon = "__bobicons__/graphics/icons/alien/alien-artifact-red-128.png"
+data.raw.item["alien-artifact-red"].icon_size = 128
+data.raw.item["alien-artifact-yellow"].icon = "__bobicons__/graphics/icons/alien/alien-artifact-yellow-128.png"
+data.raw.item["alien-artifact-yellow"].icon_size = 128
+end
+
+if data.raw.item["small-alien-artifact"]  then data.raw.item["small-alien-artifact"].icon = "__bobicons__/graphics/icons/alien/small-alien-artifact-128.png" end
+if data.raw.item["small-alien-artifact-blue"] then
+data.raw.item["small-alien-artifact-blue"].icon = "__bobicons__/graphics/icons/alien/small-alien-artifact-blue-128.png"
+data.raw.item["small-alien-artifact-blue"].icon_size = 128
+data.raw.item["small-alien-artifact-green"].icon = 
+"__bobicons__/graphics/icons/alien/small-alien-artifact-green-128.png"
+data.raw.item["small-alien-artifact-green"].icon_size = 128
+data.raw.item["small-alien-artifact-orange"].icon = "__bobicons__/graphics/icons/alien/small-alien-artifact-orange-128.png"
+data.raw.item["small-alien-artifact-orange"].icon_size = 128
+data.raw.item["small-alien-artifact-purple"].icon = "__bobicons__/graphics/icons/alien/small-alien-artifact-purple-128.png"
+data.raw.item["small-alien-artifact-purple"].icon_size = 128
+data.raw.item["small-alien-artifact-red"].icon = "__bobicons__/graphics/icons/alien/small-alien-artifact-red-128.png"
+data.raw.item["small-alien-artifact-red"].icon_size = 128
+data.raw.item["small-alien-artifact-yellow"].icon = "__bobicons__/graphics/icons/alien/small-alien-artifact-yellow-128.png"
+data.raw.item["small-alien-artifact-yellow"].icon_size = 128
+end
+
+-- ALIEN -- (bobplates)
+
+if data.raw.fluid["alien-acid"] then data.raw.fluid["alien-acid"].icon = "__bobicons__/graphics/icons/alien/alien-acid-128.png"
+data.raw.fluid["alien-acid"].icon_size = 128
+end
+if data.raw.fluid["alien-explosive"] then data.raw.fluid["alien-explosive"].icon = "__bobicons__/graphics/icons/alien/alien-explosive-128.png"
+data.raw.fluid["alien-explosive"].icon_size = 128
+end
+if data.raw.fluid["alien-fire"] then data.raw.fluid["alien-fire"].icon = "__bobicons__/graphics/icons/alien/alien-fire-128.png"
+data.raw.fluid["alien-fire"].icon_size = 128
+end
+if data.raw.fluid["alien-poison"] then data.raw.fluid["alien-poison"].icon = "__bobicons__/graphics/icons/alien/alien-poison-128.png"
+data.raw.fluid["alien-poison"].icon_size = 128
+end
+
+if data.raw.item["alien-blue-alloy"] then data.raw.item["alien-blue-alloy"].icon = "__bobicons__/graphics/icons/alien/alien-blue-alloy-128.png"
+ data.raw.item["alien-blue-alloy"].icon_size = 128 end
+if data.raw.item["alien-orange-alloy"] then data.raw.item["alien-orange-alloy"].icon = "__bobicons__/graphics/icons/alien/alien-orange-alloy-128.png"
+ data.raw.item["alien-orange-alloy"].icon_size = 128 end
+
+
+-- if data.raw.item["XXXXXXXX"]  then data.raw.item["XXXXXXXX"].icon = "__bobicons__/graphics/icons/XXXXXXXX.png" end
+
+--bobExtended.getTech("sci-component-1").icon_size = 128
+--bobExtended.getTech("sci-component-2").icon_size = 128
+--bobExtended.getTech("sci-component-3").icon_size = 128
+--bobExtended.getTech("sci-component-alien").icon_size = 128
