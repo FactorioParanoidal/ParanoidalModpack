381c303d92de4aebcf352014f3272a3086c1ae02
drd-avel <avelinadmitrieva@gmail.com>
2020-05-17 20:47:19 +0300

    удаляем устаревшие версии модов helmod, ks_power, nightbrightness, rampant

--- mods/helmod-optimized_0.8.8/data.lua
+++
@@ -1,7 +0,0 @@
-require("core.defines")
-require("prototypes.fonts")
-require("prototypes.style")
-require("prototypes.hotkey")
-require("prototypes.sprites")
-
---log(serpent.block(data.raw["gui-style"]))
