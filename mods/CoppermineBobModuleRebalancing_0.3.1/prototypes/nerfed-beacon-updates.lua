cac8881f89be8b01947389671e7f650a1cdd2278
drd-avel <avelinadmitrieva@gmail.com>
2020-05-18 21:12:55 +0300

    Маяки Т2 и Т3 прожорливее

--- mods/CoppermineBobModuleRebalancing_0.3.1/prototypes/nerfed-beacon-updates.lua
+++ mods/CoppermineBobModuleRebalancing_0.3.1/prototypes/nerfed-beacon-updates.lua
@@ -3,8 +3,9 @@ if settings.startup["coppermine-bob-module-nerfed-beacons"].value then
   data.raw.beacon["beacon-3"].supply_area_distance = 7
   data.raw.beacon["beacon-2"].module_specification.module_slots = 3
   data.raw.beacon["beacon-3"].module_specification.module_slots = 4
-  data.raw.beacon["beacon-2"].energy_usage = "1000kW"
-  data.raw.beacon["beacon-3"].energy_usage = "1500kW"
-  data.raw.beacon["beacon-2"].energy_source["emissions"] = 0.01
-  data.raw.beacon["beacon-3"].energy_source["emissions"] = 0.02
+  data.raw.beacon["beacon"].energy_usage = "950kW" --DrD
+  data.raw.beacon["beacon-2"].energy_usage = "2500kW" --DrD
+  data.raw.beacon["beacon-3"].energy_usage = "7500kW" --DrD
+  data.raw.beacon["beacon-2"].energy_source["emissions"] = 0.02
+  data.raw.beacon["beacon-3"].energy_source["emissions"] = 0.05
 end
