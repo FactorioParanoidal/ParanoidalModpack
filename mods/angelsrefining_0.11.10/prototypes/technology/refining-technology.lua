1258b1d623ef35cd0068c2b1a4011b313fee063d
drd-avel <avelinadmitrieva@gmail.com>
2020-05-19 09:49:18 +0300

    Дешевле исследования добычи термальных вод (убраны голубые банки)

--- mods/angelsrefining_0.11.10/prototypes/technology/refining-technology.lua
+++ mods/angelsrefining_0.11.10/prototypes/technology/refining-technology.lua
@@ -524,7 +524,7 @@ data:extend(
         ingredients = {
           {"automation-science-pack", 1},
           {"logistic-science-pack", 1},
-          {"chemical-science-pack", 1}
+          --{"chemical-science-pack", 1} --DrD
         },
         time = 15
       },
