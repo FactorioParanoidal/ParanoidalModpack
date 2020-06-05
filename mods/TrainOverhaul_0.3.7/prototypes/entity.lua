a87c77d82ae0315a6f3fe4b879a7e32d6e10ee86
drd-avel <avelinadmitrieva@gmail.com>
2020-06-05 22:53:42 +0300

    обновление до TrainOverhaul 0.3.7

--- mods/TrainOverhaul_0.3.7/prototypes/entity.lua
+++ mods/TrainOverhaul_0.3.7/prototypes/entity.lua
@@ -4,6 +4,8 @@
  * See LICENSE.md in the project directory for license information.
 --]]
 
+local flib = require('__flib__.data_util')
+
 engine_working_sound = {
   sound = {
     filename = "__TrainOverhaul__/sound/train-engine.ogg",
@@ -50,16 +52,17 @@ tie_sound = {
   }
 }
 
-
 local base_loco = data.raw["locomotive"]["locomotive"]
 base_loco.icon = "__TrainOverhaul__/graphics/icons/locomotive.png"
-base_loco.icon_size = 32
-base_loco.icon_mipmaps = nil
+base_loco.icon_size = 64
+base_loco.icon_mipmaps = 0
+base_loco.icons = nil
 base_loco.max_health = 1000
 base_loco.weight = 4000 * settings.startup["train-overhaul-weight-multiplicator"].value
 -- base_loco.max_speed = 1.2 --259.2km/h
 base_loco.max_speed = 1.0 --216.0km/h
-base_loco.max_power = optera_lib.multiply_energy_value("1000kW", settings.startup["train-overhaul-power-multiplicator"].value)
+-- base_loco.max_power = multiply_energy_value("1000kW", settings.startup["train-overhaul-power-multiplicator"].value)
+base_loco.max_power = (1000000 * settings.startup["train-overhaul-power-multiplicator"].value).."W"
 base_loco.reversing_power_modifier = 1 --no effect on automatic trains
 base_loco.braking_force = 20
 base_loco.friction_force = 0.50 -- constant acceleration reduction
@@ -71,13 +74,14 @@ if settings.startup["train-overhaul-old-sounds"].value then
   base_loco.drive_over_tie_trigger = tie_sound
 end
 
-local heavy_loco = optera_lib.copy_prototype(base_loco, "heavy-locomotive")
+local heavy_loco = flib.copy_prototype(base_loco, "heavy-locomotive")
 heavy_loco.icon = "__TrainOverhaul__/graphics/icons/heavy-locomotive.png"
 heavy_loco.color = {r = 0, g = 0.53, b = 0, a = 0.5}
 heavy_loco.max_health = 1500
 heavy_loco.weight = 6400 * settings.startup["train-overhaul-weight-multiplicator"].value
 heavy_loco.max_speed = 0.95 --205,2km/h
-heavy_loco.max_power = optera_lib.multiply_energy_value("2610kW", settings.startup["train-overhaul-power-multiplicator"].value)
+-- heavy_loco.max_power = multiply_energy_value("2610kW", settings.startup["train-overhaul-power-multiplicator"].value)
+heavy_loco.max_power = (2610000 * settings.startup["train-overhaul-power-multiplicator"].value).."W"
 heavy_loco.reversing_power_modifier = 1
 heavy_loco.braking_force = 40
 heavy_loco.friction_force = 0.50
@@ -85,14 +89,15 @@ heavy_loco.air_resistance = 0.015
 heavy_loco.burner.effectivity = 0.90
 heavy_loco.burner.fuel_inventory_size = 4
 
-local express_loco = optera_lib.copy_prototype(base_loco, "express-locomotive")
+local express_loco = flib.copy_prototype(base_loco, "express-locomotive")
 express_loco.icon = "__TrainOverhaul__/graphics/icons/express-locomotive.png"
 express_loco.color = {r = 0.10, g = 0.19, b = 0.80, a = 0.5}
-express_loco.max_health = 1500
+express_loco.max_health = 800
 express_loco.weight = 3800 * settings.startup["train-overhaul-weight-multiplicator"].value
 -- express_loco.max_speed = 1.5 --324,0km/h
 express_loco.max_speed = 1.4 --302,4km/h
-express_loco.max_power = optera_lib.multiply_energy_value("1200kW", settings.startup["train-overhaul-power-multiplicator"].value)
+-- express_loco.max_power = multiply_energy_value("1200kW", settings.startup["train-overhaul-power-multiplicator"].value)
+express_loco.max_power = (1200000 * settings.startup["train-overhaul-power-multiplicator"].value).."W"
 express_loco.reversing_power_modifier = 1
 express_loco.braking_force = 32
 express_loco.friction_force = 0.50
@@ -100,14 +105,15 @@ express_loco.air_resistance = 0.0062
 express_loco.burner.effectivity = 0.60
 express_loco.burner.fuel_inventory_size = 4
 
-local nuclear_loco = optera_lib.copy_prototype(base_loco, "nuclear-locomotive")
+local nuclear_loco = flib.copy_prototype(base_loco, "nuclear-locomotive")
 nuclear_loco.icon = "__TrainOverhaul__/graphics/icons/nuclear-locomotive.png"
 nuclear_loco.color = { r = 0, g = 0.75, b = 0.5, a = 0.5 }
 nuclear_loco.max_health = 2500
 nuclear_loco.weight = 8000 * settings.startup["train-overhaul-weight-multiplicator"].value
 -- nuclear_loco.max_speed = 1.4 --302.4km/h
 nuclear_loco.max_speed = 1.2 --259.2km/h
-nuclear_loco.max_power = optera_lib.multiply_energy_value("3200kW", settings.startup["train-overhaul-power-multiplicator"].value)
+-- nuclear_loco.max_power = multiply_energy_value("3200kW", settings.startup["train-overhaul-power-multiplicator"].value)
+nuclear_loco.max_power = (3200000 * settings.startup["train-overhaul-power-multiplicator"].value).."W"
 nuclear_loco.reversing_power_modifier = 1
 nuclear_loco.braking_force = 40
 nuclear_loco.friction_force = 0.50
@@ -121,14 +127,15 @@ nuclear_loco.working_sound = nuclear_working_sound
 data:extend({
   heavy_loco,
   express_loco,
-	nuclear_loco,
+  nuclear_loco,
 })
 
 local base_cargo_wagon = data.raw["cargo-wagon"]["cargo-wagon"]
 base_cargo_wagon.icon = "__TrainOverhaul__/graphics/icons/cargo-wagon.png"
 base_cargo_wagon.icon_size = 32
-base_cargo_wagon.icon_mipmaps = nil
-base_cargo_wagon.max_health = 1000 --base 600
+base_cargo_wagon.icon_mipmaps = 0
+base_cargo_wagon.icons = nil
+base_cargo_wagon.max_health = 800 --base 600
 base_cargo_wagon.weight = 3000 * settings.startup["train-overhaul-weight-multiplicator"].value -- base 1000
 base_cargo_wagon.max_speed = 1.0 -- base 1.5
 base_cargo_wagon.braking_force = 5
@@ -140,8 +147,9 @@ end
 local base_fluid_wagon = data.raw["fluid-wagon"]["fluid-wagon"]
 base_fluid_wagon.icon = "__TrainOverhaul__/graphics/icons/fluid-wagon.png"
 base_fluid_wagon.icon_size = 32
-base_fluid_wagon.icon_mipmaps = nil
-base_fluid_wagon.max_health = 1000 --base 600
+base_fluid_wagon.icon_mipmaps = 0
+base_fluid_wagon.icons = nil
+base_fluid_wagon.max_health = 800 --base 600
 base_fluid_wagon.weight = 3000 * settings.startup["train-overhaul-weight-multiplicator"].value -- base 1000
 base_fluid_wagon.max_speed = 1.0 -- base 1.5
 base_fluid_wagon.braking_force = 5
@@ -153,7 +161,7 @@ if settings.startup["train-overhaul-old-sounds"].value then
 end
 
 local artillery_wagon = data.raw["artillery-wagon"]["artillery-wagon"]
-artillery_wagon.max_health = 1000 --base 600
+artillery_wagon.max_health = 1200 --base 600
 artillery_wagon.weight = 8000 * settings.startup["train-overhaul-weight-multiplicator"].value -- base 4000
 artillery_wagon.max_speed = 0.95 -- base 1.5
 artillery_wagon.braking_force = 6
@@ -162,7 +170,7 @@ if settings.startup["train-overhaul-old-sounds"].value then
   artillery_wagon.drive_over_tie_trigger = tie_sound
 end
 
-local heavy_cargo_wagon = optera_lib.copy_prototype(base_cargo_wagon, "heavy-cargo-wagon")
+local heavy_cargo_wagon = flib.copy_prototype(base_cargo_wagon, "heavy-cargo-wagon")
 heavy_cargo_wagon.icon = "__TrainOverhaul__/graphics/icons/heavy-cargo-wagon.png"
 heavy_cargo_wagon.color = {r = 0, g = 0.53, b = 0, a = 0.5}
 heavy_cargo_wagon.max_health = 1000
@@ -171,7 +179,7 @@ heavy_cargo_wagon.max_speed = 1.09 -- 235.4 km/h
 heavy_cargo_wagon.braking_force = 6
 heavy_cargo_wagon.inventory_size = 60
 
-local heavy_fluid_wagon = optera_lib.copy_prototype(base_fluid_wagon, "heavy-fluid-wagon")
+local heavy_fluid_wagon = flib.copy_prototype(base_fluid_wagon, "heavy-fluid-wagon")
 heavy_fluid_wagon.icon = "__TrainOverhaul__/graphics/icons/heavy-fluid-wagon.png"
 heavy_fluid_wagon.color = {r = 0, g = 0.53, b = 0, a = 0.5}
 heavy_fluid_wagon.max_health = 1000
@@ -180,19 +188,19 @@ heavy_fluid_wagon.max_speed = 1.09
 heavy_fluid_wagon.braking_force = 6
 heavy_fluid_wagon.capacity = 35000
 
-local express_cargo_wagon = optera_lib.copy_prototype(base_cargo_wagon, "express-cargo-wagon")
+local express_cargo_wagon = flib.copy_prototype(base_cargo_wagon, "express-cargo-wagon")
 express_cargo_wagon.icon = "__TrainOverhaul__/graphics/icons/express-cargo-wagon.png"
 express_cargo_wagon.color = {r = 0.10, g = 0.19, b = 0.80, a = 0.5}
-express_cargo_wagon.max_health = 1000
+express_cargo_wagon.max_health = 500
 express_cargo_wagon.weight = 2500 * settings.startup["train-overhaul-weight-multiplicator"].value
 express_cargo_wagon.max_speed = 1.4
 express_cargo_wagon.braking_force = 7
 express_cargo_wagon.inventory_size = 40
 
-local express_fluid_wagon = optera_lib.copy_prototype(base_fluid_wagon, "express-fluid-wagon")
+local express_fluid_wagon = flib.copy_prototype(base_fluid_wagon, "express-fluid-wagon")
 express_fluid_wagon.icon = "__TrainOverhaul__/graphics/icons/express-fluid-wagon.png"
 express_fluid_wagon.color = {r = 0.10, g = 0.19, b = 0.80, a = 0.5}
-express_fluid_wagon.max_health = 1000
+express_fluid_wagon.max_health = 500
 express_fluid_wagon.weight = 2500 * settings.startup["train-overhaul-weight-multiplicator"].value
 express_fluid_wagon.max_speed = 1.4
 express_fluid_wagon.braking_force = 7
--- mods/TrainOverhaul_0.3.7/prototypes/farl_rebalance.lua
+++ mods/TrainOverhaul_0.3.7/prototypes/farl_rebalance.lua
@@ -7,10 +7,11 @@
 if mods["FARL"] then
   local loco = data.raw["locomotive"]["farl"]
   if loco then
-    loco.max_health = 2500
+    loco.max_health = 2000
     loco.weight = 7500 * settings.startup["train-overhaul-weight-multiplicator"].value
     loco.max_speed = 0.8 --172.8km/h
-    loco.max_power = optera_lib.multiply_energy_value("2500kW", settings.startup["train-overhaul-power-multiplicator"].value)
+    -- loco.max_power = multiply_energy_value("2500kW", settings.startup["train-overhaul-power-multiplicator"].value)
+    loco.max_power = (2500000 * settings.startup["train-overhaul-power-multiplicator"].value).."W"
     loco.reversing_power_modifier = 1 --no effect on automatic trains
     loco.braking_force = 60
     loco.friction_force = 0.60 -- constant acceleration reduction
