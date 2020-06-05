1ca9977190f2d46b7179553ca5d8f1017c727eb2
drd-avel <avelinadmitrieva@gmail.com>
2020-06-05 23:08:16 +0300

    тяжёлое обновление Angelsbioprocessing на актуальную 0.7.10 с животными и прочим

---
+++ mods/angelsbioprocessing_0.7.10/prototypes/items/bio-processing-alien-crystal.lua
@@ -0,0 +1,237 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "crystal-powder",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-powder.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "d[powder]",
+    stack_size = 200
+  },
+
+  -- RAW
+  {
+    type = "item",
+    name = "crystal-splinter-raw",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-splinter-raw.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "a[raw]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-shard-raw",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-shard-raw.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-shard",
+    order = "a[raw]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-full-raw",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-full-raw.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-full",
+    order = "a[raw]",
+    stack_size = 200
+  },
+
+  -- CUT
+  {
+    type = "item",
+    name = "crystal-splinter-blue-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-splinter-blue.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "b[split]-a[blue]-a[cut]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-splinter-red-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-splinter-red.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "b[split]-b[red]-a[cut]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-splinter-green-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-splinter-green.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "b[split]-c[green]-a[cut]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-shard-blue-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-shard-blue.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-shard",
+    order = "b[split]-a[blue]-a[cut]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-shard-red-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-shard-red.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-shard",
+    order = "b[split]-b[red]-a[cut]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-shard-green-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-shard-green.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-shard",
+    order = "b[split]-c[green]-a[cut]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-full-blue-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-full-blue.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-full",
+    order = "b[split]-a[blue]-a[cut]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-full-red-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-full-red.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-full",
+    order = "b[split]-b[red]-a[cut]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-full-green-cut",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-full-green.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-full",
+    order = "b[split]-c[green]-a[cut]",
+    stack_size = 200
+  },
+
+
+
+  -- CRYSTAL
+  {
+    type = "item",
+    name = "crystal-splinter-blue",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-splinter-blue.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "b[split]-a[blue]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-splinter-red",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-splinter-red.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "b[split]-b[red]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-splinter-green",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-splinter-green.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "b[split]-c[green]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-splinter-harmonic",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-splinter-harmonic.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-splinter",
+    order = "c[harmonic]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-shard-blue",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-shard-blue.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-shard",
+    order = "b[split]-a[blue]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-shard-red",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-shard-red.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-shard",
+    order = "b[split]-b[red]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-shard-green",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-shard-green.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-shard",
+    order = "b[split]-c[green]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-shard-harmonic",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-shard-harmonic.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-shard",
+    order = "c[harmonic]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-full-blue",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-full-blue.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-full",
+    order = "b[split]-a[blue]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-full-red",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-full-red.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-full",
+    order = "b[split]-b[red]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-full-green",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-full-green.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-full",
+    order = "b[split]-c[green]-b[polished]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "crystal-full-harmonic",
+    icon = "__angelsbioprocessing__/graphics/icons/crystal-full-harmonic.png",
+    icon_size = 32,
+    subgroup = "bio-biter-processing-crystal-full",
+    order = "c[harmonic]",
+    stack_size = 200
+  },
+}
+)
