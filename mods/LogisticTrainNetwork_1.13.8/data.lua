65d006e78650bd5d7be5d753aea2b838606d2646
drd-avel <avelinadmitrieva@gmail.com>
2020-06-06 08:27:07 +0300

    Обновление до LTN 1.13.8

---
+++ mods/LogisticTrainNetwork_1.13.8/data.lua
@@ -0,0 +1,13 @@
+--[[ Copyright (c) 2017 Optera
+ * Part of Logistics Train Network
+ *
+ * See LICENSE.md in the project directory for license information.
+--]]
+
+flib = require('__flib__.data_util')
+require ("prototypes.technology")
+require ("prototypes.recipes")
+require ("prototypes.items")
+require ("prototypes.entities")
+require ("prototypes.signals")
+flib = nil
