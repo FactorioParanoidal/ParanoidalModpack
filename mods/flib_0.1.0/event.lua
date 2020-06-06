efc582e1a359a065a7363cd69a55739645b41c6c
drd-avel <avelinadmitrieva@gmail.com>
2020-06-06 08:31:40 +0300

    НОВЫЙ мод-библиотека FLIB - замена для OpteraLib, возможно последний можно уже удалить

---
+++ mods/flib_0.1.0/event.lua
@@ -0,0 +1,179 @@
+--- Syntax sugar for event manipulation.
+-- Along with the list of functions below, this module dynamically generates syntax-shortcuts for all @{defines.events}
+-- events. These shortcuts are only to be used when registering a handler to a single event. To register a handler to
+-- multiple events, use @{event.register}.
+--
+-- To use a shortcut, replace `event.register(defines.events.on_built_entity, handler, filters)` with
+-- `event.on_built_entity(handler, filters)`. You can also deregister the handler using `event.on_built_entity(nil)`.
+-- @module event
+-- @alias flib_event
+-- @usage local event = require("__flib__.event")
+local flib_event = {}
+
+-- generate syntax shortcuts
+for name, id in pairs(defines.events) do
+  flib_event[name] = function(handler, filters)
+    return script.on_event(id, handler, filters)
+  end
+end
+
+--- Functions
+-- @section
+
+--- Register or deregister a handler to be run during mod init.
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @usage
+-- -- register a handler to run during mod init
+-- event.on_init(function() log("on_init") end)
+-- -- deregister the registered handler, if one exists
+-- event.on_init(nil)
+function flib_event.on_init(handler)
+  return script.on_init(handler)
+end
+
+--- Register or deregister a handler to be run during mod load.
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @usage
+-- -- register a handler to run during mod load
+-- event.on_load(function() log("on_load") end)
+-- -- deregister the registered handler, if one exists
+-- event.on_load(nil)
+function flib_event.on_load(handler)
+  return script.on_load(handler)
+end
+
+--- Register or deregister a handler to be run when mod configuration changes.
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @usage
+-- -- register a handler to run when mod configuration changes
+-- event.on_configuration_changed(function() log("on_configuration_changed") end)
+-- -- deregister the registered handler, if one exists
+-- event.on_configuration_changed(nil)
+function flib_event.on_configuration_changed(handler)
+  return script.on_configuration_changed(handler)
+end
+
+--- Register or deregister a handler to run every N ticks.
+-- @tparam uint nth_tick
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @usage
+-- -- register a handler to run every 30 ticks
+-- event.on_nth_tick(30, function(e) log("30th tick!") end)
+-- -- deregister the registered handler, if one exists
+-- event.on_nth_tick(30, nil)
+function flib_event.on_nth_tick(nth_tick, handler)
+  return script.on_nth_tick(nth_tick, handler)
+end
+
+-- TODO Nexela link EventFilters to https://lua-api.factorio.com/latest/Event-Filters.html
+
+--- Register or deregister a handler to or from an event or group of events.
+-- @tparam EventId|EventId[] ids
+-- @tparam function handler The handler to register, or `nil` to deregister the registered handler.
+-- @tparam[opt] EventFilters filters
+-- @usage
+-- -- register a handler to a defines.events event that supports filters
+-- event.register(defines.events.on_built_entity, function(e) log("ghost built!") end, {{filter="ghost"}})
+-- -- register a handler to a custom-input
+-- event.register("my-input", function(e) log("my-input pressed!") end)
+-- -- register a handler to multiple events of different types
+-- event.register({"my-input", defines.events.on_lua_shortcut}, function(e) log("do something!") end)
+-- -- deregister a handler from a single event, if one is registered
+-- event.register("my-input", nil)
+-- -- deregister a handler from multiple events, if one is registered
+-- event.register({"my-input", defines.events.on_lua_shortcut}, nil)
+function flib_event.register(ids, handler, filters)
+  if type(ids) ~= "table" then
+    ids = {ids}
+  end
+  for i=1,#ids do
+    -- dumb workaround - the game doesn't like you passing filters, even if it's nil
+    if filters then
+      script.on_event(ids[i], handler, filters)
+    else
+      script.on_event(ids[i], handler)
+    end
+  end
+  return
+end
+
+--- Generate a new, unique event ID.
+-- @treturn uint
+-- @usage
+-- -- generate a new event ID
+-- local my_event = event.generate_id()
+-- -- raise that event with custom parameters
+-- event.raise(my_event, {whatever_you_want=true, ...})
+function flib_event.generate_id()
+  return script.generate_event_name()
+end
+
+--- Retrieve the handler for an event, if one exists.
+-- @tparam EventId id
+-- @treturn function The registered handler, or `nil` if one isn't registered.
+function flib_event.get_handler(id)
+  return script.get_event_handler(id)
+end
+
+-- TODO Nexela link EventData to https://lua-api.factorio.com/latest/events.html
+
+--- Raise an event as if it were actually called.
+-- @tparam EventId id
+-- @tparam EventData event_data The event data that will be passed to the handlers.
+-- @usage
+-- event.raise(defines.events.on_gui_click, {player_index=e.player_index, element=my_button, ...})
+function flib_event.raise(id, event_data)
+  return script.raise_event(id, event_data)
+end
+
+--- Retrieve the mod event order.
+-- @treturn string
+function flib_event.get_order()
+  return script.get_event_order()
+end
+
+--- Set the filters for the given event(s).
+-- @tparam EventId|EventId[] ids
+-- @tparam EventFilters filters The filters to set, or `nil` to clear the filters.
+-- @usage
+-- -- set the filters for a single event
+-- event.set_filters(defines.events.on_built_entity, {
+--   {filter="ghost"},
+--   {filter="type", type="assembling-machine"}
+-- })
+-- -- set the filters for multiple events that have compatible formats
+-- event.set_filters({defines.events.on_built_entity, defines.events.on_robot_built_entity}, {
+--   {filter="ghost"},
+--   {filter="type", type="assembling-machine"}
+-- })
+-- -- clear event filters if any are set
+-- event.set_filters(defines.events.on_robot_built_entity, nil)
+function flib_event.set_filters(ids, filters)
+  if type(ids) ~= "table" then
+    ids = {ids}
+  end
+  for i=1,#ids do
+    script.set_event_filter(ids[i], filters)
+  end
+  return
+end
+
+--- Retrieve the filters for the given event.
+-- @tparam EventId id
+-- @treturn EventFilters filters The filters, or `nil` if there are none defined.
+function flib_event.get_filters(id)
+  return script.get_event_filter(id)
+end
+
+--- Concepts
+-- @section
+
+--- @Concept EventId
+-- One of the following:
+-- <ul>
+--   <li>A member of @{defines.events}.</li>
+--   <li>A positive @{uint} corresponding to a custom event ID.</li>
+--   <li>A @{string} corresponding to a custom-input name.</li>
+-- </ul>
+
+return flib_event
---
+++ mods/flib_0.1.0/migration.lua
@@ -0,0 +1,115 @@
+--- Mod migrations and version comparison functions.
+-- @module migration
+-- @alias flib_migration
+-- @usage local migration = require("__flib__.migration")
+local flib_migration = {}
+
+local string_format = string.format
+local table_concat = table.concat
+
+local version_pattern = "%d+"
+local version_format = "%02d"
+
+--- Functions
+-- @section
+
+--- Normalize version strings for easy comparison.
+-- @tparam string version
+-- @tparam[opt="%02d"] string format
+-- @treturn string|nil
+-- @usage
+-- migration.format_version("1.10.1234", "%04d")
+-- migration.format_version("3", "%02d")
+function flib_migration.format_version(version, format)
+  if version then
+    format = format or version_format
+    local tbl = {}
+    for v in string.gmatch(version, version_pattern) do
+      tbl[#tbl+1] = string_format(format, v)
+    end
+    if next(tbl) then
+      return table_concat(tbl, ".")
+    end
+  end
+  return nil
+end
+
+--- Check if current_version is newer than old_version.
+-- @tparam string old_version
+-- @tparam string current_version
+-- @tparam[opt=%02d] string format
+-- @treturn boolean|nil
+function flib_migration.is_newer_version(old_version, current_version, format)
+  local v1 = flib_migration.format_version(old_version, format)
+  local v2 = flib_migration.format_version(current_version, format)
+  if v1 and v2 then
+    if v2 > v1 then
+      return true
+    end
+    return false
+  end
+  return nil
+end
+
+--- Run migrations against the given version.
+-- @tparam string old_version
+-- @tparam MigrationsTable migrations
+-- @tparam[opt="%02d"] string format
+function flib_migration.run(old_version, migrations, format)
+  local migrate = false
+  for version, func in pairs(migrations) do
+    if migrate or flib_migration.is_newer_version(old_version, version, format) then
+      migrate = true
+      func()
+    end
+  end
+end
+
+--- Determine if migrations need to be run for this mod, then run them if needed.
+-- @tparam Concepts.ConfigurationChangedData event_data
+-- @tparam MigrationsTable migrations
+-- @tparam[opt] string mod_name The mod to check against, defaults to the mod this is used in.
+-- @treturn boolean If true, run generic migrations. If false, run post-init setup.
+-- @usage
+-- -- In on_configuration_changed:
+-- if migration.on_config_changed(e, migrations) then
+--   -- run generic (non-init) migrations
+--   rebuild_prototype_data()
+-- else
+--   -- run post-init setup
+--   unlock_recipes_for_cheating_forces()
+-- end
+function flib_migration.on_config_changed(event_data, migrations, mod_name)
+  local changes = event_data.mod_changes[mod_name or script.mod_name]
+  if changes then
+    local old_version = changes.old_version
+    if old_version then
+      flib_migration.run(old_version, migrations)
+    else
+      return false -- don't do generic migrations, because we just initialized
+    end
+  end
+  return true
+end
+
+return flib_migration
+
+--- Concepts
+-- @section
+
+--- @Concept MigrationsTable
+-- Dictionary string -> function. Each string is a version number, and each value is a
+-- function that will be run for that version.
+-- @tparam {[string]=function,...} ...
+-- @usage
+-- {
+--   ["1.0.1"] = function()
+--     global.foo = nil
+--     for _, player_table in pairs(global.players) do
+--       player_table.bar = "Lorem ipsum"
+--     end
+--   end,
+--   ["1.1.0"] = function()
+--     global.foo = "bar"
+--   end
+-- }
