5def0942177c0af9d330098fa1eb068cc26dec54
drd-avel <avelinadmitrieva@gmail.com>
2020-06-06 08:43:07 +0300

    Обновление до bobpower 0.18.6

--- mods/bobpower_0.18.6/prototypes/item/heat-exchangers.lua
+++ mods/bobpower_0.18.6/prototypes/item/heat-exchangers.lua
@@ -1,7 +1,7 @@
 if settings.startup["bobmods-power-steam"].value == true then
 
 data.raw.item["heat-exchanger"].order = "f[nuclear-energy]-b[heat-exchanger-1]"
-data.raw.item["heat-exchanger"].subgroup = "bob-energy-boiler"
+data.raw.item["heat-exchanger"].subgroup = "bob-energy-heat-exchanger"
 
 data:extend(
 {
@@ -11,7 +11,7 @@ data:extend(
     icon = "__base__/graphics/icons/heat-boiler.png",
     icon_size = 64,
     icon_mipmaps = 4,
-    subgroup = "bob-energy-boiler",
+    subgroup = "bob-energy-heat-exchanger",
     order = "f[nuclear-energy]-b[heat-exchanger-2]",
     place_result = "heat-exchanger-2",
     stack_size = 50,
@@ -23,7 +23,7 @@ data:extend(
     icon = "__base__/graphics/icons/heat-boiler.png",
     icon_size = 64,
     icon_mipmaps = 4,
-    subgroup = "bob-energy-boiler",
+    subgroup = "bob-energy-heat-exchanger",
     order = "f[nuclear-energy]-b[heat-exchanger-3]",
     place_result = "heat-exchanger-3",
     stack_size = 50,
