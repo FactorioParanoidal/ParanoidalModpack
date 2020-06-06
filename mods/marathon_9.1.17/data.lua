b83afd444855c9e37e21bc8cd74bd0e88624fa28
drd-avel <avelinadmitrieva@gmail.com>
2020-06-06 09:12:42 +0300

    разрешение использовать изменения рецептов труб, насосов и паровых двигателей

--- mods/marathon_9.1.17/data.lua
+++ mods/marathon_9.1.17/data.lua
@@ -10,6 +10,9 @@ require("prototypes.recipe-production")
 require("prototypes.recipe-science")
 require("prototypes.recipe-smelting")
 require("prototypes.recipe-weapon")
+require("prototypes.recipe-bob-steam-power")
+require("prototypes.recipe-bob-pipes")
+require("prototypes.recipe-bob-pump")
 --require("prototypes.new")
 
 marathon = {}
