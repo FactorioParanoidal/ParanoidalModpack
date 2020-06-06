f6af2fa4a6669ac3b8fdf1a2ce427f14d123cf91
drd-avel <avelinadmitrieva@gmail.com>
2020-06-06 09:11:25 +0300

    Восстановление слетевших значений объёмов стака предметов

--- mods/marathon_9.1.17/prototypes/item.lua
+++ mods/marathon_9.1.17/prototypes/item.lua
@@ -10,3 +10,7 @@ data.raw["item"]["solid-fuel"].fuel_value = "25MJ"
 
 data.raw["item"]["basic-electronic-components"].stack_size = 400
 --data.raw["item"]["transport-belt"].stack_size = 200
+data.raw["item"]["iron-plate"].stack_size = 400
+data.raw["item"]["copper-plate"].stack_size = 400
+data.raw["item"]["steel-plate"].stack_size = 200
+data.raw["item"]["copper-cable"].stack_size = 400
