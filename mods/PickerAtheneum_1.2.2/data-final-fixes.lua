e5879001bcd09581e58887282bc2922433049eda
drd-avel <avelinadmitrieva@gmail.com>
2020-06-06 08:11:30 +0300

    Обновление до pickeratheneum 1.2.2

--- mods/PickerAtheneum_1.2.2/data-final-fixes.lua
+++ mods/PickerAtheneum_1.2.2/data-final-fixes.lua
@@ -1,6 +1,8 @@
-if require('__PickerAtheneum__/config').DEBUG then
+require('__stdlib__/stdlib/utils/globals')
+
+if prequire('__PickerAtheneum__/.debug/debug') then
     require('__stdlib__/stdlib/data/developer/developer').make_test_entities()
-    data:extend{
+    data:extend {
         {
             type = 'custom-input',
             name = 'picker-reload-mods',
@@ -12,6 +14,17 @@ if require('__PickerAtheneum__/config').DEBUG then
             name = 'picker-dump-data',
             key_sequence = 'SHIFT + PAGEUP',
             localised_name = 'Dump Data'
+        }
+    }
+end
+
+if mods['debugadapter'] then
+    data:extend {
+        {
+            type = 'custom-input',
+            name = 'picker-trigger-breakpoint',
+            key_sequence = 'CONTROL + SHIFT + END',
+            localised_name = 'Trigger Breakpoint'
         }
     }
 end
