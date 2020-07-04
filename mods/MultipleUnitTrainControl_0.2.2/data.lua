06d8f4b69feb7f45a441fe9ba28384d6ecc7d2ba
drd-avel <avelinadmitrieva@gmail.com>
2020-07-04 10:13:23 +0300

    Удалена устаревшая копия мода MultipleUnitTrainControl_0.2.2

--- mods/MultipleUnitTrainControl_0.2.2/data.lua
+++
@@ -1,7 +0,0 @@
---[[ Copyright (c) 2019 robot256 (MIT License)
- * Project: Multiple Unit Train Control
- * File: data.lua
- * Description: Add the MU technology
---]]
-
-require ("prototypes.technology")
