0f9ad052606a544436969f8b510e35b17a569c74
sbeljakov <sbelyakov@mail.ru>
2020-07-08 08:01:29 +0300

    Количество рыбы Santa Ray в озёрах уменьшено в 20 раз, а другие виды рыб Ангела выключены

---
+++ mods/zzzparanoidal_0.18.30/prototypes/fish.lua
@@ -0,0 +1,6 @@
+--рыбы Santa Ray будет меньше в 20 раз, не поймаешь ни одну
+--две другие ангельские рыбы отключаются совсем
+data.raw["fish"]["alien-fish-3"].autoplace = {influence=0.0005}
+data.raw["fish"]["alien-fish-2"].autoplace.peaks[1].influence=0
+data.raw["fish"]["alien-fish-1"].autoplace.peaks[1].influence=0
+--data.raw["fish"]["fish"].autoplace = {influence=0} -- отключается по желанию в другом моде
