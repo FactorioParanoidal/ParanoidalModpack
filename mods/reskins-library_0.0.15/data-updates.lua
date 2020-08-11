9b22b30e81f2956b664b78166afae7f49bc5b9c7
drd-avel <avelinadmitrieva@gmail.com>
2020-08-11 19:03:38 +0300

    Обновление до reskins-library_0.0.15

---
+++ mods/reskins-library_0.0.15/data-updates.lua
@@ -0,0 +1,12 @@
+-- Copyright (c) 2020 Kirazy
+-- Part of Artisanal Reskins: Library
+--     
+-- See LICENSE.md in the project directory for license information.
+
+-- Compatibility with ShinyBobGFX/ShinyAngelGFX
+if mods["ShinyBobGFX"] or mods["ShinyAngelGFX"] then
+    require("shiny-compatibility")
+end
+
+-- Assign deferred icons
+reskins.lib.assign_deferred_icons("lib", "data-updates")
