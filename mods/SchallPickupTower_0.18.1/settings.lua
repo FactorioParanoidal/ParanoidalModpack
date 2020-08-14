fffd3561bf23601e97038b24d20a6d5cd49381a5
drd-avel <avelinadmitrieva@gmail.com>
2020-08-14 16:21:47 +0300

    Откат до SchallPickupTower_0.18.1

--- mods/SchallPickupTower_0.18.1/settings.lua
+++ mods/SchallPickupTower_0.18.1/settings.lua
@@ -2,22 +2,22 @@ data:extend{
   -- Pickup Tower
   {
     type = "int-setting",
-    name = "pickuptower-tier-max",
+    name = "Schall-pickup-tower-tier-max",
     order = "pt-t",
     setting_type = "startup",
-    allowed_values = { 1, 2, 3, 4 },
-    default_value = 2
+    default_value = 2,
+    allowed_values = { 1, 2, 3, 4 }
   },
   {
     type = "bool-setting",
-    name = "pickuptower-range-force-disable",
+    name = "Schall-pickup-tower-range-force-disable",
     order = "pt-b-a",
     setting_type = "runtime-global",
     default_value = false
   },
   {
     type = "bool-setting",
-    name = "pickuptower-range-show",
+    name = "Schall-pickup-tower-range-show",
     order = "pt-b-b",
     setting_type = "runtime-per-user",
     default_value = true
