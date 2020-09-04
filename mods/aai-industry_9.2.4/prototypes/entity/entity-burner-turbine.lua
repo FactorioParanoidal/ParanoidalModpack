f5968e238ea2f952fd14cb88a93aba7903044ebe
sovigod <CrashDGC@gmail.com>
2020-09-05 00:06:23 +0200

    фиксы загрязнений построек из  aai

--- mods/aai-industry_9.2.4/prototypes/entity/entity-burner-turbine.lua
+++ mods/aai-industry_9.2.4/prototypes/entity/entity-burner-turbine.lua
@@ -5,6 +5,169 @@ local blank = {
   height = 1,
   frame_count = 1
 }
+local effectivity = settings.startup["aai-burner-turbine-efficiency"].value / 100
+local layers = {
+  layers =
+  {
+    {
+      filename = "__aai-industry__/graphics/entity/burner-turbine/burner-turbine.png",
+      priority = "extra-high",
+      line_length = 4,
+      frame_count = 8,
+      animation_speed = 0.5,
+      width = 540/4,
+      height = 300/2,
+      shift = util.by_pixel(0, -10),
+      hr_version = {
+        filename = "__aai-industry__/graphics/entity/burner-turbine/hr-burner-turbine.png",
+        priority = "extra-high",
+        line_length = 4,
+        frame_count = 8,
+        animation_speed = 0.5,
+        width = 540/2,
+        height = 300,
+        shift = util.by_pixel(0, -10),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__aai-industry__/graphics/entity/burner-turbine/burner-turbine-shadow.png",
+      priority = "extra-high",
+      width = 165,
+      height = 75,
+      shift = util.by_pixel(30, 42),
+      draw_as_shadow = true,
+      repeat_count = 8,
+      hr_version = {
+        filename = "__aai-industry__/graphics/entity/burner-turbine/hr-burner-turbine-shadow.png",
+        priority = "extra-high",
+        width = 165*2,
+        height = 75*2,
+        scale = 0.5,
+        shift = util.by_pixel(30, 42),
+        draw_as_shadow = true,
+        repeat_count = 8,
+      }
+    }
+  }
+}
+local fire = {
+  filename = "__aai-industry__/graphics/entity/burner-turbine/burner-turbine-glow.png",
+  priority = "extra-high",
+  line_length = 4,
+  frame_count = 8,
+  animation_speed = 1,
+  width = 540/4,
+  height = 300/2,
+  shift = util.by_pixel(0, -10),
+  blend_mode = "additive",
+  hr_version = {
+    filename = "__aai-industry__/graphics/entity/burner-turbine/hr-burner-turbine-glow.png",
+    priority = "extra-high",
+    line_length = 4,
+    frame_count = 8,
+    animation_speed = 1,
+    width = 540/2,
+    height = 300,
+    shift = util.by_pixel(0, -10),
+    scale = 0.5,
+    blend_mode = "additive",
+  }
+}
+local fire_glow = {
+  filename = "__aai-industry__/graphics/entity/burner-turbine/burner-turbine.png",
+  priority = "extra-high",
+  line_length = 4,
+  frame_count = 8,
+  animation_speed = 1,
+  width = 540/4,
+  height = 300/2,
+  shift = util.by_pixel(0, -10),
+  hr_version = {
+    filename = "__aai-industry__/graphics/entity/burner-turbine/hr-burner-turbine.png",
+    priority = "extra-high",
+    line_length = 4,
+    frame_count = 8,
+    animation_speed = 1,
+    width = 540/2,
+    height = 300,
+    shift = util.by_pixel(0, -10),
+    scale = 0.5
+  }
+}
+local fire_and_glow = { layers = {fire, glow} }
+local burner_generator = {
+  type = "burner-generator",
+  name = "burner-turbine",
+  icon = "__aai-industry__/graphics/icons/burner-turbine.png",
+  icon_size = 32,
+  flags = {"placeable-neutral", "player-creation"},
+  minable = {hardness = 0.2, mining_time = 0.5, result = "burner-turbine"},
+  max_health = 250,
+  corpse = "small-remnants",
+  vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+  resistances =
+  {
+    { type = "fire", percent = 100 },
+    { type = "explosion", percent = 30 },
+    { type = "impact", percent = 50 }
+  },
+  selection_box = {{-2,-2}, {2,2}},
+  collision_box = {{-1.6, -1.6}, {1.6, 1.6}},
+  max_power_output = "2MW",
+  burner =
+  {
+    type = "burner",
+    fuel_category = "chemical",
+    effectivity = effectivity,
+    fuel_inventory_size = 4,
+    emissions_per_minute = 34,
+    smoke =
+    {
+      {
+        name = "smoke",
+        north_position = util.by_pixel(20, -85),
+        south_position = util.by_pixel(20, -85),
+        east_position = util.by_pixel(20, -85),
+        west_position = util.by_pixel(20, -85),
+        frequency = 30,
+        starting_vertical_speed = 0.0,
+        starting_frame_deviation = 60,
+        deviation = {-1, 1},
+      }
+    }
+  },
+  energy_source = {
+    type = "electric",
+    usage_priority = "primary-output",
+  },
+  working_sound =
+  {
+    sound =
+    {
+      filename = "__base__/sound/furnace.ogg",
+      volume = 1.6
+    },
+    max_sounds_per_type = 3
+  },
+  min_perceived_performance = 0.25,
+  performance_to_sound_speedup = 0.5,
+  idle_animation = {
+    north = layers,
+    south = layers,
+    east = layers,
+    west = layers,
+  },
+  always_draw_idle_animation = true,
+  animation = {
+    north = fire_and_glow,
+    south = fire_and_glow,
+    east = fire_and_glow,
+    west = fire_and_glow,
+  }
+}
+data:extend({burner_generator})
+--[[
 local burner_turbine = {
   type = "boiler",
   name = "burner-turbine",
@@ -29,7 +192,7 @@ local burner_turbine = {
   fluid_output = { name = "steam", amount = 10.0 },
   fluid_box =
   {
-    base_area = 0.5,
+    base_area = 1,
     height = 2,
     base_level = -1,
     production_type = "input-output",
@@ -38,20 +201,20 @@ local burner_turbine = {
   },
   output_fluid_box =
   {
-    base_area = 0.5,
+    base_area = 1,
     height = 2,
     production_type = "output",
     pipe_connections = {},
     filter = "steam"
   },
-  energy_consumption = "1MW",
+  energy_consumption = (2 / effectivity) .. "MW",
   energy_source =
   {
     type = "burner",
     fuel_category = "chemical",
-    effectivity = 0.25, --0.35
+    effectivity = effectivity,
     fuel_inventory_size = 4,
-    emissions_per_second_per_watt = 6 * 1e-05, -- emissions = 0.5 / 3,
+    emissions_per_minute = 5,
     smoke =
     {
       {
@@ -81,108 +244,17 @@ local burner_turbine = {
   burning_cooldown = 20
 }
 
-local layers = {
-  layers =
-  {
-    {
-      filename = "__aai-industry__/graphics/entity/burner-turbine/burner-turbine.png",
-      priority = "extra-high",
-      line_length = 4,
-      frame_count = 8,
-      animation_speed = 0.5,
-      width = 540/4,
-      height = 300/2,
-      shift = util.by_pixel(0, -10),
-      hr_version = {
-        filename = "__aai-industry__/graphics/entity/burner-turbine/hr-burner-turbine.png",
-        priority = "extra-high",
-        line_length = 4,
-        frame_count = 8,
-        animation_speed = 0.5,
-        width = 540/2,
-        height = 300,
-        shift = util.by_pixel(0, -10),
-        scale = 0.5
-      }
-    }
-	--[[,
-    {
-      filename = "__aai-industry__/graphics/entity/burner-turbine/burner-turbine-shadow.png",
-      priority = "extra-high",
-      width = 165,
-      height = 75,
-      shift = util.by_pixel(30, 42),
-      draw_as_shadow = true,
-      hr_version = {
-        filename = "__aai-industry__/graphics/entity/burner-turbine/hr-burner-turbine-shadow.png",
-        priority = "extra-high",
-        width = 165*2,
-        height = 75*2,
-        scale = 0.5,
-        shift = util.by_pixel(30, 42),
-        draw_as_shadow = true,
-      }
-    }
-	
-	]]
-	
-  }
-}
 burner_turbine.structure = {}
 burner_turbine.structure.north = layers
 burner_turbine.structure.south = layers
 burner_turbine.structure.east = layers
 burner_turbine.structure.west = layers
-local fire = {
-  filename = "__aai-industry__/graphics/entity/burner-turbine/burner-turbine-glow.png",
-  priority = "extra-high",
-  line_length = 4,
-  frame_count = 8,
-  animation_speed = 0.5,
-  width = 540/4,
-  height = 300/2,
-  shift = util.by_pixel(0, -10),
-  blend_mode = "additive",
-  hr_version = {
-    filename = "__aai-industry__/graphics/entity/burner-turbine/hr-burner-turbine-glow.png",
-    priority = "extra-high",
-    line_length = 4,
-    frame_count = 8,
-    animation_speed = 0.5,
-    width = 540/2,
-    height = 300,
-    shift = util.by_pixel(0, -10),
-    scale = 0.5,
-    blend_mode = "additive",
-  }
-}
 burner_turbine.fire = {}
 burner_turbine.fire.north = fire
 burner_turbine.fire.south = fire
 burner_turbine.fire.east = fire
 burner_turbine.fire.west = fire
 
-local fire_glow = {
-  filename = "__aai-industry__/graphics/entity/burner-turbine/burner-turbine.png",
-  priority = "extra-high",
-  line_length = 4,
-  frame_count = 8,
-  animation_speed = 0.5,
-  width = 540/4,
-  height = 300/2,
-  shift = util.by_pixel(0, -10),
-  hr_version = {
-    filename = "__aai-industry__/graphics/entity/burner-turbine/hr-burner-turbine.png",
-    priority = "extra-high",
-    line_length = 4,
-    frame_count = 8,
-    animation_speed = 0.5,
-    width = 540/2,
-    height = 300,
-    shift = util.by_pixel(0, -10),
-    scale = 0.5
-  }
-}
 burner_turbine.fire_glow = {}
 burner_turbine.fire_glow.north = fire_glow
 burner_turbine.fire_glow.south = fire_glow
@@ -201,7 +273,7 @@ local burner_turbine_generator = {
   healing_per_tick = 10000,
   corpse = "big-remnants",
   dying_explosion = "medium-explosion",
-  effectivity = 1, -- fix for exactly 1 fluid per tick
+  effectivity = 1,
   order="z",
   fluid_usage_per_tick = 1,
   maximum_temperature = 165,
@@ -211,7 +283,7 @@ local burner_turbine_generator = {
   collision_mask = {"not-colliding-with-itself"},
   fluid_box =
   {
-    base_area = 0.5,
+    base_area = 1,
     height = 2,
     base_level = -1,
     production_type = "input-output",
@@ -224,7 +296,7 @@ local burner_turbine_generator = {
     --usage_priority = "terciary", -- bugs when accumilators are linked
     usage_priority = "secondary-output",
     render_no_network_icon = false,
-	emissions_per_second_per_watt = 0,
+    emissions_per_minute = 0
   },
   horizontal_animation = { layers = { blank }, },
   vertical_animation = { layers = { blank }, },
@@ -242,3 +314,4 @@ local burner_turbine_generator = {
   },
 }
 data:extend({burner_turbine_generator})
+]]--
--- mods/aai-industry_9.2.4/settings.lua
+++ mods/aai-industry_9.2.4/settings.lua
@@ -4,5 +4,15 @@ data:extend{
         name = "crash-sequence",
         setting_type = "startup",
         default_value = true,
+        order = "a"
+    },
+    {
+        type = "int-setting",
+        name = "aai-burner-turbine-efficiency",
+        setting_type = "startup",
+        default_value = 85,
+        minimum_value = 10,
+        maximum_value = 100,
+        order = "e"
     }
 }
