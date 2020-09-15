338b2bcc8625bd6c0998f109a607201396a0aef2
drd-avel <avelinadmitrieva@gmail.com>
2020-09-15 20:44:08 +0300

    Обновление до scattergun_turret_6.0.0

---
+++ mods/scattergun_turret_6.0.0/data.lua
@@ -0,0 +1,35 @@
+-- Base Modular Turrets
+require("prototypes.entities.scattergun-turret")
+require("prototypes.entities.w93-hmg-turret")
+require("prototypes.entities.w93-gatling-turret")
+require("prototypes.entities.w93-lcannon-turret")
+require("prototypes.entities.w93-dcannon-turret")
+require("prototypes.entities.w93-hcannon-turret")
+require("prototypes.entities.w93-rocket-turret")
+require("prototypes.entities.w93-tlaser-turret")
+require("prototypes.entities.w93-beam-turret")
+require("prototypes.projectiles.projectiles")
+require("prototypes.items.equipment")
+require("prototypes.items.items")
+require("prototypes.recipes.recipes-entity")
+require("prototypes.technology.technology")
+
+-- Modular Radars
+require("prototypes.entities.w93-radar-turret")
+require("prototypes.items.items-radar")
+require("prototypes.recipes.recipes-radar")
+require("prototypes.technology.technology-radar")
+
+-- Hardened Inserters
+require("prototypes.entities.w93-hardened-inserter")
+require("prototypes.items.items-inserter")
+require("prototypes.recipes.recipes-inserter")
+
+-- Extra ammo types
+require("prototypes.projectiles.projectiles-ammo")
+require("prototypes.items.items-ammo")
+require("prototypes.recipes.recipes-ammo")
+
+-- Compatibility
+require("prototypes.recipes.recipes-krastorio2")
+require("prototypes.technology.technology-krastorio2")
