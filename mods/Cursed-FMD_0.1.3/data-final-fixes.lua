f37b9bffb95fd00eb6473411b3b6e6743603cab3
drd-avel <avelinadmitrieva@gmail.com>
2020-09-15 20:35:21 +0300

    Обновление до Cursed-FMD_0.1.3

--- mods/Cursed-FMD_0.1.3/data-final-fixes.lua
+++ mods/Cursed-FMD_0.1.3/data-final-fixes.lua
@@ -71,7 +71,6 @@ local newRes = {}
 for resName, oldCat in pairs(oldRes) do table.insert(newRes, resName) end
 
 
--- local tbl = {}
 for i, arrResource in ipairs(newRes) do
     for drillName, drill in pairs(data.raw["mining-drill"]) do
         if oldMinCat[drillName] then
@@ -90,7 +89,6 @@ for i, arrResource in ipairs(newRes) do
                 else
                     newDrill.localised_name = {"",{"entity-name." .. drillName}," {",localeRes[arrResource],"}"}
                 end
-                -- tbl[newName] = newDrill.localised_name
                 local checkDrill = ""
                 local handleFluids = (newDrill.input_fluid_box ~= nil)
                 for _, entCat in ipairs(oldMinCat[drillName]) do
@@ -106,57 +104,3 @@ for i, arrResource in ipairs(newRes) do
         end
     end
 end
-
--- error(serpent.block(tbl))
-
-
-local obj = util.table.deepcopy(data.raw["selection-tool"]["selection-tool"])
-obj.name = "filter-drills"
-obj.icon = "__Cursed-FMD__/icon_64.png"
-obj.icon_size = 64
-obj.selection_color = {r=0.66, g=0.93, b=0.30}
-obj.alt_selection_color = obj.selection_color
-obj.selection_mode = {"any-entity"}
-obj.alt_selection_mode = obj.selection_mode
-obj.entity_filters = nil
-obj.alt_entity_filters = obj.entity_filters
-obj.entity_type_filters = {"mining-drill"}
-obj.entity_type_filters = obj.entity_type_filters
-obj.show_in_library = true
-obj.localised_name = {"gui-control-behavior-modes.set-filters"}
-data.raw[obj.type][obj.name] = obj
-
-data.raw["sprite"]["Cursed-FMD"] =
-{
-    type = "sprite",
-    name = "Cursed-FMD",
-    filename = "__Cursed-FMD__/icon_64.png",
-    width = 64,
-    height = 64
-}
-
-data.raw["shortcut"]["Cursed-FMD"] = {
-    type = "shortcut",
-    name = "Cursed-FMD",
-    action = "create-blueprint-item",
-    item_to_create = "filter-drills",
-    order = "zy",
-    localised_name = {"gui-control-behavior-modes.set-filters"},
-    icon = {
-      filename = "__Cursed-FMD__/icon_64.png",
-      size = 64,
-      scale = 0.25,
-      flags = {"gui-icon"}
-    }
-  }
-
-
--- description.logistic-chest-filters=Filtros
--- gui.select-filter=Seleccionar filtro
--- gui.set-filter=Configurar el filtro
--- gui-blueprint-library.shelf-choice=Filtro:
--- gui-browse-games.filters=filtros
--- gui-control-behavior-modes.set-filters=Definir filtros
--- gui-inserter.filter=Filtro
--- gui-logistic.title-filter-short=Filtro
--- entity-name.can-filter-items=Puede filtrar objetos.
