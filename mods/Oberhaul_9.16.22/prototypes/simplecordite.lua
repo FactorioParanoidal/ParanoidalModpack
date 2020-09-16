17c480588dedfba2d7e44504ae475be3b5bf3506
drd-avel <avelinadmitrieva@gmail.com>
2020-09-16 20:58:52 +0300

    Фикс названий глицерола и других жидкостей в модах после обновления модов ангела (gas-xxx --> liquid-xxx)

--- mods/Oberhaul_9.16.22/prototypes/simplecordite.lua
+++ mods/Oberhaul_9.16.22/prototypes/simplecordite.lua
@@ -1,6 +1,6 @@
 --Changing Bullet Complexity Back
 data.raw.recipe["sulfuric-nitric-acid"].hidden = true
-data.raw.recipe["gas-glycerol"].ingredients = {{type="fluid", name="liquid-fuel-oil", amount=100}}
+data.raw.recipe["liquid-glycerol"].ingredients = {{type="fluid", name="liquid-fuel-oil", amount=100}}
 data.raw.recipe["gun-cotton"].ingredients = {{type="item", name="wood", amount=1}}
 data:extend(
 { 
@@ -13,7 +13,7 @@ data:extend(
     energy_required = 1.5,
     ingredients =
     {
-      {type="fluid", name="gas-glycerol", amount=10},
+      {type="fluid", name="liquid-glycerol", amount=10},
       {type="fluid", name="liquid-sulfuric-acid", amount=30},
     },
     results=
