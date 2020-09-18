a741ad930af5964e39badf51eba86122aba9358c
Andrey <ash@mom.life>
2020-09-18 15:08:38 +0200

    Установка минимального времени на рецепты с продуктивностью

--- mods/zzzparanoidal_1.0.0/settings.lua
+++ mods/zzzparanoidal_1.0.0/settings.lua
@@ -8,6 +8,16 @@ data:extend(
     minimum_value = 0.01,
     maximum_value = 100,
     order = "a"
+  },
+  {
+    type = "double-setting",
+    name = "paranoidal-flowfix-min-time",
+    setting_type = "startup",
+    default_value = 15,
+    minimum_value = 1,
+    maximum_value = 100,
+    order = "a"
   }
+
 }
 )
