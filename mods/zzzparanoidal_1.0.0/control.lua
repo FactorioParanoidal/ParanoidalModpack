ef84ac564b5716e7b297ed77aa470754961a1231
sbeljakov <sbelyakov@mail.ru>
2020-09-20 23:06:58 +0300

    В zzzparanoidal вшит код от мода some-corpsemarker

---
+++ mods/zzzparanoidal_1.0.0/control.lua
@@ -0,0 +1,5 @@
+-- from some corpse marker
+script.on_event(defines.events.on_pre_player_died, function(event)
+    local player = game.players[event.player_index]
+    player.force.add_chart_tag(player.surface, {position=player.position, text='Corpse: '..player.name..'; Time: '..math.floor(game.tick/60/60/60)..':'..(math.floor(game.tick/60/60)%60), icon={type="virtual",name="signal-info"}})
+end)
