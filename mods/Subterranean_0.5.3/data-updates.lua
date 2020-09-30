78243db1ff32878d68eacfd549dd9ecd4ac22a80
sbeljakov <sbelyakov@mail.ru>
2020-09-30 18:40:28 +0300

    Роботы теперь смогут снимать пластиковые, нитиноловые и медно-вольфрамовые трубы

--- mods/Subterranean_0.5.3/data-updates.lua
+++ mods/Subterranean_0.5.3/data-updates.lua
@@ -14,18 +14,18 @@ end
 entity = data.raw["pipe-to-ground"]["nitinol-pipe-to-ground"]
 if entity ~= nil then --bob's logistics is installed
 	entity.fluid_box.pipe_connections = {{position = {0, -1}}, {position = {0, 1}, max_underground_distance = 250}}
-	table.insert(entity.flags, "not-deconstructable")
+	--table.insert(entity.flags, "not-deconstructable")   --drd
 end
 
 entity = data.raw["pipe-to-ground"]["copper-tungsten-pipe-to-ground"]
 if entity ~= nil then --bob's logistics is installed
 	entity.fluid_box.pipe_connections = {{position = {0, -1}}, {position = {0, 1}, max_underground_distance = 250}}
-	table.insert(entity.flags, "not-deconstructable")
+	--table.insert(entity.flags, "not-deconstructable")   --drd
 end
 
 entity = data.raw["pipe-to-ground"]["plastic-pipe-to-ground"]
 if entity ~= nil then --bob's logistics is installed
 	entity.fluid_box.pipe_connections = {{position = {0, -1}}, {position = {0, 1}, max_underground_distance = 100}}
-	table.insert(entity.flags, "not-deconstructable")
+	--table.insert(entity.flags, "not-deconstructable")   --drd
 
 end
