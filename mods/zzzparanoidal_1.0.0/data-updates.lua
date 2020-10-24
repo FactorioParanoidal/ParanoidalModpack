1e0cba42be5aae0c321c8195dc81cf16bc99b4fb
sbeljakov <sbelyakov@mail.ru>
2020-10-24 14:55:20 +0300

    Персональные дронстанции mk3-mk4 перенесены в раздел "снаряжение", чтобы были вместе с mk1-mk2

--- mods/zzzparanoidal_1.0.0/data-updates.lua
+++ mods/zzzparanoidal_1.0.0/data-updates.lua
@@ -1,2 +1,7 @@
 require("prototypes.miniloaders")
 require("prototypes.generators")
+
+if mods.bobequipment then
+	data.raw.item["personal-roboport-mk3-equipment"].subgroup = "misc1"
+	data.raw.item["personal-roboport-mk4-equipment"].subgroup = "misc1"
+end
