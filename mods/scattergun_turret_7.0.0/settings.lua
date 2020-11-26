78c54ff642d4955c157b9176cc561c029fdbf5c1
drd-avel <avelinadmitrieva@gmail.com>
2020-11-26 03:00:38 +0300

    Обновление до scattergun_turret_7.0.0

---
+++ mods/scattergun_turret_7.0.0/settings.lua
@@ -0,0 +1,36 @@
+data:extend({
+{
+	type = "bool-setting",
+	name = "enable-radars",
+	setting_type = "startup",
+	default_value = true,
+	order = "aa"
+},
+{
+	type = "bool-setting",
+	name = "enable-hardened-inserter",
+	setting_type = "startup",
+	default_value = true,
+	order = "ab"
+},
+{
+	type = "bool-setting",
+	name = "enable-extra-ammo",
+	setting_type = "startup",
+	default_value = true,
+	order = "ac"
+},
+{
+	type = "bool-setting",
+	name = "cannon-ammo-range-disable",
+	setting_type = "startup",
+	default_value = true,
+	order = "ad"
+},
+{
+	type = "bool-setting",
+	name = "enable-pretargeting",
+	setting_type = "startup",
+	default_value = true,
+	order = "ae"
+}})
