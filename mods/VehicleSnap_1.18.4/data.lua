b6eda1bc943fe764f8a31c939ecace830a21d30f
drd-avel <avelinadmitrieva@gmail.com>
2020-11-28 01:16:35 +0300

    Обновление до VehicleSnap_1.18.4

---
+++ mods/VehicleSnap_1.18.4/data.lua
@@ -0,0 +1,10 @@
+require("prototypes.shortcuts")
+
+data:extend({
+  {
+    type = "custom-input",
+    name = "VehicleSnap-toggle",
+    key_sequence = "SHIFT + V",
+    consuming = "none"
+  }
+})
---
+++ mods/VehicleSnap_1.18.4/prototypes/shortcuts.lua
@@ -0,0 +1,33 @@
+data:extend({
+  {
+    type = "shortcut",
+    name = "VehicleSnap-shortcut",
+    order = "v[vehiclesnap]",
+    action = "lua",
+    toggleable = true,
+    icon =
+    {
+      filename = "__VehicleSnap__/graphics/icons/vehsnap-x32.png",
+      priority = "extra-high-no-scale",
+      size = 32,
+      scale = 1,
+      flags = { "icon" }
+    },
+    small_icon =
+    {
+      filename = "__VehicleSnap__/graphics/icons/vehsnap-x32.png",
+      priority = "extra-high-no-scale",
+      size = 32,
+      scale = 1,
+      flags = { "icon" }
+    },
+    disabled_small_icon =
+    {
+      filename = "__VehicleSnap__/graphics/icons/vehsnap-x32.png",
+      priority = "extra-high-no-scale",
+      size = 32,
+      scale = 1,
+      flags = { "icon" }
+    }
+  }
+})
