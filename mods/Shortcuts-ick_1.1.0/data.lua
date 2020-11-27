549b600ed962539e6ecc7cefb2ace4729288ac92
drd-avel <avelinadmitrieva@gmail.com>
2020-11-27 19:06:50 +0300

    Временно уберём несовместимые моды

--- mods/Shortcuts-ick_1.1.0/data.lua
+++
@@ -1,5 +0,0 @@
-require("prototypes.shortcuts-basic")
-require("prototypes.shortcuts-blueprint")
-require("prototypes.shortcuts-equipment")
-require("prototypes.shortcuts-artillery")
-require("prototypes.shortcuts-vehicle")
