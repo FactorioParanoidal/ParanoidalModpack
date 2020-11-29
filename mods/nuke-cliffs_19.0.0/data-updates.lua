db73f04712598303637cf3e7a9731e5c065a88c7
drd-avel <avelinadmitrieva@gmail.com>
2020-11-29 16:49:07 +0300

    Обновление до nuke-cliffs_19.0.0

--- mods/nuke-cliffs_19.0.0/data-updates.lua
+++ mods/nuke-cliffs_19.0.0/data-updates.lua
@@ -17,6 +17,26 @@ table.insert(data.raw.projectile["atomic-bomb-wave"].action,
     }
   })
 
+
+--[[
+local target_effects = data.raw.projectile["atomic-bomb-wave"].action[1].action_delivery.target_effects
+if not target_effects[1] then
+  target_effects[1] = {}
+  for k,v in pairs(target_effects) do
+    if type(k) == "string" then
+      target_effects[1][k] = v
+      target_effects[k] = nil
+    end
+  end
+end
+target_effects[#target_effects+1] = {
+  type = "destroy-cliffs",
+  radius = 3,
+  explosion = "explosion"
+}
+]]
+
+
 if (data.raw.projectile["atomic-artillery-wave"]) then
   table.insert(data.raw.projectile["atomic-artillery-wave"].action,
   {
