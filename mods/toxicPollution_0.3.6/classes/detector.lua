34a0da84097468d00558a4cb482fc6011af77197
sbeljakov <sbelyakov@mail.ru>
2020-12-11 08:48:07 +0300

    toxicPollution: исправление кода для совместимости с 1.1(ошибка при открытии константного комбинатора)

--- mods/toxicPollution_0.3.6/classes/detector.lua
+++ mods/toxicPollution_0.3.6/classes/detector.lua
@@ -15,8 +15,8 @@ end
 function Detector:CheckEntity(entity)
     if entity.type == "constant-combinator" then
         local behavior = entity.get_control_behavior()
-        for i = 1, #behavior.parameters.parameters do
-            local signal = behavior.parameters.parameters[i].signal
+        for i = 1, #behavior.parameters do
+            local signal = behavior.parameters[i].signal
             if signal ~= nil and signal.name == "signal-yellow-more-toxin" then
                 signal.count = math.floor(entity.surface.get_pollution(entity.position))
                 if global.combinators[entity.unit_number] == nil then
