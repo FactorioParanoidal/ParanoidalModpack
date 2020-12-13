309dfc933cf888a69be5d7712449325493843718
drd-avel <avelinadmitrieva@gmail.com>
2020-12-13 21:10:36 +0300

    Обновление до blueprint_flip_and_turn_101.8.6

--- mods/blueprint_flip_and_turn_101.8.6/prototypes/hotkeys.lua
+++ mods/blueprint_flip_and_turn_101.8.6/prototypes/hotkeys.lua
@@ -8,5 +8,10 @@ data:extend({
     type = "custom-input",
     name = "blueprint_hotkey_flip_vertical",
     key_sequence = "SHIFT + X",
-  }
+  },
+  {
+    type = "custom-input",
+    name = "blueprint_hotkey_reverse_inserters",
+    key_sequence = "SHIFT + C",
+  },
 })
