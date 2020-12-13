36988473063b0075bcaf0d7686205ee1a5ee5dc7
drd-avel <avelinadmitrieva@gmail.com>
2020-12-13 23:42:36 +0300

    Обновление до Factorissimo2_2.5.1 (4 файла с хардкодом --drd)

--- mods/Factorissimo2_2.5.1/control.lua
+++ mods/Factorissimo2_2.5.1/control.lua
@@ -41,7 +41,7 @@ factory = {
 
 	+inside_energy_sender = *,
 	+inside_energy_receiver = *,
-	+inside_overlay_controllers = {*},
+	+inside_overlay_controller = *,
 	+inside_fluid_dummy_connectors = {*},
 	(+)inside_other_entities = {*},
 	+energy_indicator = *,
@@ -305,16 +305,6 @@ end
 
 -- FACTORY UPGRADES --
 
---[[
-local function build_power_upgrade(factory)
-	if factory.upgrades.power then return end
-	factory.upgrades.power = true
-	local iet = factory.inside_surface.create_entity{name = "factory-power-pole", position = {factory.inside_x + factory.layout.inside_energy_x, factory.inside_y + factory.layout.inside_energy_y}, force = factory.force}
-	iet.destructible = false
-	table.insert(factory.inside_other_entities, iet)
-end
-]]--
-
 local function build_lights_upgrade(factory)
 	if factory.upgrades.lights then return end
 	factory.upgrades.lights = true
@@ -327,34 +317,122 @@ local function build_lights_upgrade(factory)
 	end
 end
 
-local function build_display_upgrade(factory)
-	if factory.upgrades.display then return end
-	factory.upgrades.display = true
-	for id, pos in pairs(factory.layout.overlays) do
-		local controller = factory.inside_surface.create_entity{name = "factory-overlay-controller", position = {factory.inside_x + pos.inside_x, factory.inside_y + pos.inside_y}, force = factory.force}
-		controller.destructible = false
-		controller.rotatable = false
-		factory.inside_overlay_controllers[id] = controller
-	end
+function build_display_upgrade(factory)
+	if not factory.force.technologies["factory-interior-upgrade-display"].researched then return end
+	if factory.inside_overlay_controller and factory.inside_overlay_controller.valid then return end
+
+	pos = factory.layout.overlays
+	local controller = factory.inside_surface.create_entity{
+		name = "factory-overlay-controller",
+		position = {
+			factory.inside_x + pos.inside_x,
+			factory.inside_y + pos.inside_y
+		},
+		force = factory.force
+	}
+	controller.minable = false
+	controller.destructible = false
+	controller.rotatable = false
+	factory.inside_overlay_controller = controller
 end
 
 -- OVERLAY MANAGEMENT --
 
-local function update_overlay(factory)
-	if factory.built then
-		-- Do it this way because the controllers might not exist yet
-		for id, controller in pairs(factory.inside_overlay_controllers) do
-			local display = factory.outside_overlay_displays[id]
-			if controller.valid and display and display.valid then
-				local controller_inv = controller.get_inventory(defines.inventory.chest)
-				local display_inv = display.get_inventory(defines.inventory.chest)
-				display_inv.clear()
-				local slot_size = math.min(4, math.min(#controller_inv, #display_inv))
-				for i = 1, slot_size do
-					local slot = controller_inv[i]
-					if slot.valid_for_read then display_inv.insert(slot) end
-				end
+local sprite_path_translation = {
+	item = "item",
+	fluid = "fluid",
+	virtual = "virtual-signal",
+}
+local function draw_overlay_sprite(signal, target_entity, offset, scale, id_table)
+
+	local sprite_name = sprite_path_translation[signal.type] .. "/" .. signal.name
+	if target_entity.valid then
+		local sprite_data = {
+			sprite = sprite_name,
+			x_scale = scale,
+			y_scale = scale,
+			target = target_entity,
+			surface = target_entity.surface,
+			only_in_alt_mode = true,
+			render_layer = "entity-info-icon",
+		}
+		-- Fake shadows
+		local shadow_radius = 0.07 * scale
+		for _, shadow_offset in pairs({{0,shadow_radius}, {0, -shadow_radius}, {shadow_radius, 0}, {-shadow_radius, 0}}) do
+			sprite_data.tint = {0, 0, 0, 0.5} -- Transparent black
+			sprite_data.target_offset = {offset[1] + shadow_offset[1], offset[2] + shadow_offset[2]}
+			table.insert(id_table, rendering.draw_sprite(sprite_data))
+		end
+		-- Proper sprite
+		sprite_data.tint = nil
+		sprite_data.target_offset = offset
+		table.insert(id_table, rendering.draw_sprite(sprite_data))
+	end
+end
+
+local function get_nice_overlay_arrangement(width, height, amount)
+	-- Computes a nice arrangement of square sprites within a rectangle of given size
+	-- Returned coordinates are relative to the center of the rectangle
+	if amount <= 0 then return {} end
+	local opt_rows = 1
+	local opt_cols = 1
+	local opt_scale = 0
+	-- Determine the optimal number of rows to use
+	-- This assumes width >= height
+	for rows = 1, math.ceil(math.sqrt(amount)) do
+		local cols = math.ceil(amount/rows)
+		local scale = math.min(width/cols, height/rows)
+		if scale > opt_scale then
+			opt_rows = rows
+			opt_cols = cols
+			opt_scale = scale
+		end
+	end
+	-- Adjust scale to ensure that sprites do not become too big
+	opt_scale = math.pow(opt_scale, 0.8) * math.pow(1.5, 0.8 - 1)
+	-- Create evenly spaced coordinates
+	local result = {}
+	for i = 0, amount-1 do
+		local col = i % opt_cols
+		local row = math.floor(i / opt_cols)
+		local cols_in_row = (row < opt_rows - 1 and opt_cols or (amount - 1) % opt_cols + 1)
+		table.insert(result, {
+			x = (2 * col + 1 - cols_in_row) * width / (2 * opt_cols),
+			y = (2 * row + 1 - opt_rows) * height / (2 * opt_rows),
+			scale = opt_scale
+		})
+	end
+	return result
+end
+
+function update_overlay(factory)
+	for _, id in pairs(factory.outside_overlay_displays) do
+		rendering.destroy(id)
+	end
+	factory.outside_overlay_displays = {}
+	if factory.built and factory.inside_overlay_controller and factory.inside_overlay_controller.valid then
+		local params = factory.inside_overlay_controller.get_or_create_control_behavior().parameters
+		local nonempty_params = {}
+		for _, param in pairs(params) do
+			if param and param.signal and param.signal.name then
+				table.insert(nonempty_params, param)
 			end
+		end
+		local sprite_positions = get_nice_overlay_arrangement(
+			factory.layout.overlays.outside_w,
+			factory.layout.overlays.outside_h,
+			#nonempty_params
+		)
+		local i = 0
+		for _, param in pairs(nonempty_params) do
+			i = i + 1
+			draw_overlay_sprite(param.signal, factory.building,
+				{
+					sprite_positions[i].x + factory.layout.overlays.outside_x,
+					sprite_positions[i].y + factory.layout.overlays.outside_y,
+				},
+				sprite_positions[i].scale,
+			factory.outside_overlay_displays)
 		end
 	end
 end
@@ -548,14 +626,6 @@ local function create_factory_exterior(factory, building)
 
 	factory.outside_overlay_displays = {}
 
-	for id, pos in pairs(layout.overlays) do
-		local display = factory.outside_surface.create_entity{name = "factory-overlay-display", position = {factory.outside_x + pos.outside_x, factory.outside_y + pos.outside_y}, force = force}
-		display.destructible = false
-		display.operable = false
-		display.rotatable = false
-		factory.outside_overlay_displays[id] = display
-	end
-
 	factory.outside_fluid_dummy_connectors = {}
 
 	for id, cpos in pairs(layout.connections) do
@@ -591,16 +661,19 @@ local function toggle_port_markers(factory)
 	if not factory.built then return end
 	if #(factory.outside_port_markers) == 0 then
 		for id, cpos in pairs(factory.layout.connections) do
-			local marker = factory.outside_surface.create_entity{name = "factory-port-marker", position = {
-			factory.outside_x + cpos.outside_x-cpos.indicator_dx, factory.outside_y + cpos.outside_y-cpos.indicator_dy}, force = factory.force, direction = cpos.direction_out}
-			marker.destructible = false
-			marker.operable = false
-			marker.rotatable = false
-			marker.active = false
-			table.insert(factory.outside_port_markers, marker)
+			local sprite_data = {
+				sprite = "utility/indication_arrow",
+				orientation = cpos.direction_out/8,
+				target = factory.building,
+				surface = factory.building.surface,
+				target_offset = {cpos.outside_x - 0.5 * cpos.indicator_dx, cpos.outside_y - 0.5 * cpos.indicator_dy},
+				only_in_alt_mode = true,
+				render_layer = "entity-info-icon",
+			}
+			table.insert(factory.outside_port_markers, rendering.draw_sprite(sprite_data))
 		end
 	else
-		for _, entity in pairs(factory.outside_port_markers) do entity.destroy() end
+		for _, sprite in pairs(factory.outside_port_markers) do rendering.destroy(sprite) end
 		factory.outside_port_markers = {}
 	end
 end
@@ -609,11 +682,11 @@ local function cleanup_factory_exterior(factory, building)
 	Connections.disconnect_factory(factory)
 	factory.outside_energy_sender.destroy()
 	factory.outside_energy_receiver.destroy()
-	for _, entity in pairs(factory.outside_overlay_displays) do entity.destroy() end
+	for _, render_id in pairs(factory.outside_overlay_displays) do rendering.destroy(render_id) end
 	factory.outside_overlay_displays = {}
 	for _, entity in pairs(factory.outside_fluid_dummy_connectors) do entity.destroy() end
 	factory.outside_fluid_dummy_connectors = {}
-	for _, entity in pairs(factory.outside_port_markers) do entity.destroy() end
+	for _, render_id in pairs(factory.outside_port_markers) do rendering.destroy(render_id) end
 	factory.outside_port_markers = {}
 	for _, entity in pairs(factory.outside_other_entities) do entity.destroy() end
 	factory.outside_other_entities = {}
