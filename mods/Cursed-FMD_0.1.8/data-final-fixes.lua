eb8f573b4ab0ec7662acee37f15491704a5f39e7
drd-avel <avelinadmitrieva@gmail.com>
2020-12-13 23:28:08 +0300

    Обновление до Cursed-FMD_0.1.8

--- mods/Cursed-FMD_0.1.8/data-final-fixes.lua
+++ mods/Cursed-FMD_0.1.8/data-final-fixes.lua
@@ -61,8 +61,10 @@ end
 
 
 for n, item in pairs(data.raw["item"]) do
-    if item.place_result and oldMinCat[item.place_result] then
-        placeable[item.place_result] = { item=item.name, count=1 }
+    if n:sub(1, 7) ~= "cursed-" then
+        if item.place_result and oldMinCat[item.place_result] then
+            placeable[item.place_result] = { item=item.name, count=1 }
+        end
     end
 end
 
@@ -73,32 +75,34 @@ for resName, oldCat in pairs(oldRes) do table.insert(newRes, resName) end
 
 for i, arrResource in ipairs(newRes) do
     for drillName, drill in pairs(data.raw["mining-drill"]) do
-        if oldMinCat[drillName] then
-            local newName = drillName .. ";" .. arrResource
-            if not data.raw["mining-drill"][newName] and drill.resource_categories then
-                local newDrill = util.table.deepcopy(drill)
-                newDrill.name = newName
-                newDrill.resource_categories = {}
-                newDrill.localised_name = drill.localised_name or {"entity-name." .. drillName}
-                newDrill.localised_description = drill.localised_description or {"entity-description.drillName"}
-                newDrill.flags = {"placeable-neutral", "player-creation", "hidden"}
-                newDrill.order = newName
-                newDrill.placeable_by = util.table.deepcopy(placeable[drill.name])
-                if newDrill.localised_name then
-                    newDrill.localised_name = {"",newDrill.localised_name," {",localeRes[arrResource],"}"}
-                else
-                    newDrill.localised_name = {"",{"entity-name." .. drillName}," {",localeRes[arrResource],"}"}
-                end
-                local checkDrill = ""
-                local handleFluids = (newDrill.input_fluid_box ~= nil)
-                for _, entCat in ipairs(oldMinCat[drillName]) do
-                    if (not needFluid[arrResource] or handleFluids) and oldRes[arrResource] == entCat then
-                        table.insert(newDrill.resource_categories, arrResource)
-                        checkDrill = arrResource
+        if drillName:sub(1, 7) ~= "cursed-" then
+            if oldMinCat[drillName] then
+                local newName = drillName .. ";" .. arrResource
+                if not data.raw["mining-drill"][newName] and drill.resource_categories then
+                    local newDrill = util.table.deepcopy(drill)
+                    newDrill.name = newName
+                    newDrill.resource_categories = {}
+                    newDrill.localised_name = drill.localised_name or {"entity-name." .. drillName}
+                    newDrill.localised_description = drill.localised_description or {"entity-description.drillName"}
+                    newDrill.flags = {"placeable-neutral", "player-creation", "hidden"}
+                    newDrill.order = newName
+                    newDrill.placeable_by = util.table.deepcopy(placeable[drill.name])
+                    if newDrill.localised_name then
+                        newDrill.localised_name = {"",newDrill.localised_name," {",localeRes[arrResource],"}"}
+                    else
+                        newDrill.localised_name = {"",{"entity-name." .. drillName}," {",localeRes[arrResource],"}"}
                     end
-                end
-                if newName == drillName .. ";" .. checkDrill then
-                    data.raw["mining-drill"][newName] = newDrill
+                    local checkDrill = ""
+                    local handleFluids = (newDrill.input_fluid_box ~= nil)
+                    for _, entCat in ipairs(oldMinCat[drillName]) do
+                        if (not needFluid[arrResource] or handleFluids) and oldRes[arrResource] == entCat then
+                            table.insert(newDrill.resource_categories, arrResource)
+                            checkDrill = arrResource
+                        end
+                    end
+                    if newName == drillName .. ";" .. checkDrill then
+                        data.raw["mining-drill"][newName] = newDrill
+                    end
                 end
             end
         end
