5c6c4153a69d48e587b08272a0fc909c780e3ab6
drd-avel <avelinadmitrieva@gmail.com>
2020-12-14 20:20:25 +0300

    Новый мод LootingSpaceshipWrecks_1.0.1 - для управления лутом на старте и размером инвентаря разибившегося корабля

---
+++ mods/LootingSpaceshipWrecks_1.0.1/data.lua
@@ -0,0 +1,12 @@
+
+-- big hull
+    data.raw.container["crash-site-spaceship"].inventory_size = 80
+
+
+    data.raw["simple-entity-with-owner"]["crash-site-spaceship-wreck-small-1"].minable = {mining_time = 0.75, result = "concrete", count = 50}
+	
+    data.raw["simple-entity-with-owner"]["crash-site-spaceship-wreck-small-2"].minable = {mining_time = 0.75, result = "copper-plate", count = 20}
+	
+    data.raw["simple-entity-with-owner"]["crash-site-spaceship-wreck-small-3"].minable = {mining_time = 0.75, result = "iron-plate", count = 20}
+	
+    data.raw["simple-entity-with-owner"]["crash-site-spaceship-wreck-small-4"].minable = {mining_time = 0.75, result = "steel-plate", count = 35}
