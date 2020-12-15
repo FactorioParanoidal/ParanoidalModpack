db159f8e1ac3acd396d2c251f4d0908cc8b54513
sbeljakov <sbelyakov@mail.ru>
2020-12-15 07:59:42 +0300

    LootingSpaceshipWrecks: Добавление настройки на получение бонусных предметов

---
+++ mods/LootingSpaceshipWrecks_1.1.0/settings.lua
@@ -0,0 +1,10 @@
+data:extend(
+{
+  {
+    type = "bool-setting",
+    name = "lootingspaceshipwrecks-bonus-start-items",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "a"
+  }
+})
