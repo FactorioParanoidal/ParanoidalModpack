e4629b1e38f700f50422674ebd8e42f215d4bd19
drd-avel <avelinadmitrieva@gmail.com>
2020-12-19 00:51:30 +0300

    Добавление теплотворности и интересных коэффициентов загрязнения для всех alien-artifact и solid-oil-residual

---
+++ mods/marathon_9.1.17/prototypes/item-fuelvalue.lua
@@ -0,0 +1,65 @@
+--FUEL VALUES
+
+--data.raw["item"]["wood"].fuel_value = "4MJ""2MJ"
+--data.raw["item"]["coal"].fuel_value = "4MJ"0.8
+--data.raw["item"]["solid-fuel"].fuel_value = "4MJ""25MJ"
+
+--data.raw.item["coal"].fuel_emissions_multiplier = 3
+--data.raw.item["solid-fuel"].fuel_emissions_multiplier = 0.8
+
+
+data.raw.item["alien-artifact"].fuel_value = "90MJ"
+data.raw.item["alien-artifact"].fuel_emissions_multiplier = 0.8
+data.raw.item["alien-artifact"].fuel_category = "chemical"
+ 
+data.raw.item["alien-artifact-red"].fuel_value = "80MJ"
+data.raw.item["alien-artifact-red"].fuel_emissions_multiplier = 0.5
+data.raw.item["alien-artifact-red"].fuel_category = "chemical"
+data.raw.item["alien-artifact-orange"].fuel_value = "70MJ"
+data.raw.item["alien-artifact-orange"].fuel_emissions_multiplier = 0
+data.raw.item["alien-artifact-orange"].fuel_category = "chemical"
+data.raw.item["alien-artifact-yellow"].fuel_value = "60MJ"
+data.raw.item["alien-artifact-yellow"].fuel_emissions_multiplier = -0.5
+data.raw.item["alien-artifact-yellow"].fuel_category = "chemical"
+data.raw.item["alien-artifact-green"].fuel_value = "50MJ"
+data.raw.item["alien-artifact-green"].fuel_emissions_multiplier = -1
+data.raw.item["alien-artifact-green"].fuel_category = "chemical"
+data.raw.item["alien-artifact-blue"].fuel_value = "40MJ"
+data.raw.item["alien-artifact-blue"].fuel_emissions_multiplier = -1.5
+data.raw.item["alien-artifact-blue"].fuel_category = "chemical"
+data.raw.item["alien-artifact-purple"].fuel_value = "30MJ"
+data.raw.item["alien-artifact-purple"].fuel_emissions_multiplier = -1.9
+data.raw.item["alien-artifact-purple"].fuel_category = "chemical"
+
+
+data.raw.item["small-alien-artifact"].fuel_value = "4MJ"
+data.raw.item["small-alien-artifact"].fuel_emissions_multiplier = 0.8
+data.raw.item["small-alien-artifact"].fuel_category = "chemical"
+
+data.raw.item["small-alien-artifact-red"].fuel_value = "4MJ"
+data.raw.item["small-alien-artifact-red"].fuel_emissions_multiplier = 0.5
+data.raw.item["small-alien-artifact-red"].fuel_category = "chemical"
+data.raw.item["small-alien-artifact-orange"].fuel_value = "4MJ"
+data.raw.item["small-alien-artifact-orange"].fuel_emissions_multiplier = 0
+data.raw.item["small-alien-artifact-orange"].fuel_category = "chemical"
+data.raw.item["small-alien-artifact-yellow"].fuel_value = "4MJ"
+data.raw.item["small-alien-artifact-yellow"].fuel_emissions_multiplier = -0.5
+data.raw.item["small-alien-artifact-yellow"].fuel_category = "chemical"
+data.raw.item["small-alien-artifact-green"].fuel_value = "4MJ"
+data.raw.item["small-alien-artifact-green"].fuel_emissions_multiplier = -1
+data.raw.item["small-alien-artifact-green"].fuel_category = "chemical"
+data.raw.item["small-alien-artifact-blue"].fuel_value = "3MJ"
+data.raw.item["small-alien-artifact-blue"].fuel_emissions_multiplier = -1.5
+data.raw.item["small-alien-artifact-blue"].fuel_category = "chemical"
+data.raw.item["small-alien-artifact-purple"].fuel_value = "2.5MJ"
+data.raw.item["small-alien-artifact-purple"].fuel_emissions_multiplier = -1.9
+data.raw.item["small-alien-artifact-purple"].fuel_category = "chemical"
+
+
+data.raw["item"]["small-electric-pole"].fuel_category = "chemical"
+data.raw["item"]["small-electric-pole"].fuel_emissions_multiplier = 1.25
+data.raw["item"]["small-electric-pole"].fuel_value = "2MJ"
+
+data.raw.item["solid-oil-residual"].fuel_value = "8MJ"
+data.raw.item["solid-oil-residual"].fuel_emissions_multiplier = 9
+data.raw.item["solid-oil-residual"].fuel_category = "chemical"
