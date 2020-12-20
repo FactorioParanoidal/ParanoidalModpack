00bd65b077961e6edb1614faf3ed683004b8014a
drd-avel <avelinadmitrieva@gmail.com>
2020-12-20 20:28:47 +0300

    Revert "Обновление до Sandros-fixes_0.6.2"
    This reverts commit 073c156914730b37e2b977277ee34066258c0ca2.

--- mods/Sandros-fixes_0.6.0/data-final-fixes.lua
+++ mods/Sandros-fixes_0.6.0/data-final-fixes.lua
@@ -7,14 +7,6 @@ local lab = data.raw.lab
 local recipe = data.raw.recipe
 local subgroup = data.raw["item-subgroup"]
 
-local function sort_recipe(recipe_sort, subgroup)
-  if recipe[recipe_sort] then
-    recipe[recipe_sort].subgroup = subgroup
-  else
-    log("Recipe " .. recipe_sort .. " does not exist.")
-  end
-end
-
 local function sort_item_recipe(item_sort, subgroup)
   if item[item_sort] then
     item[item_sort].subgroup = subgroup
@@ -43,11 +35,6 @@ local function sort_item_recipe_order(item_sort, subgroup, order)
     log("Item " .. item_sort .. " does not exist.")
   end
 
-  if any[item_sort] then
-    any[item_sort].subgroup = subgroup
-    any[item_sort].order = order
-  end
-
   if recipe[item_sort] then
     recipe[item_sort].subgroup = subgroup
     recipe[item_sort].order = order
@@ -56,32 +43,25 @@ local function sort_item_recipe_order(item_sort, subgroup, order)
   end
 end
 
-if mods["PCPRedux"] then
-  if mods["angelsexploration"] then
-    sort_item_recipe_order("plaswall", "angels-exploration-walls", "b[plastic]-a[wall]")
+local function sort_recipe_order(recipe_sort, subgroup)
+  if item[recipe_sort] then
+    item[recipe_sort].subgroup = subgroup
+  else
+    log("Item " .. recipe_sort .. " does not exist.")
   end
-
-  if mods["angelspetrochem"] then
-    sort_recipe("carbon-dioxide", "petrochem-basics", "d[carbon-separation-3]")
+  if recipe[recipe_sort] then
+    recipe[recipe_sort].subgroup = subgroup
+  else
+    log("Recipe " .. recipe_sort .. " does not exist.")
   end
 end
 
-if mods["angelsindustries"] then
-  if mods["bobvehicleequipment"] then
-    sort_item_recipe_order("vehicle-belt-immunity-equipment", "angels-vehicle-equipment", "a[vehicle-belt-immunity-equipment]")
-  end
+if mods["angelsexploration"] and mods["PCPRedux"] then
+  sort_item_recipe_order("plaswall", "angels-exploration-walls", "b[plastic]-a[wall]")
+end
 
-  if mods["bobequipment"] and mods["Nanobots"] then
-    sort_item_recipe_order("equipment-bot-chip-items", "angels-personal-equipment-power-d", "aa")
-    sort_item_recipe_order("equipment-bot-chip-launcher", "angels-personal-equipment-power-d", "ab")
-    sort_item_recipe_order("equipment-bot-chip-trees", "angels-personal-equipment-power-d", "ac")
-    sort_item_recipe_order("equipment-bot-chip-feeder", "angels-personal-equipment-power-d", "ad")
-    sort_item_recipe_order("equipment-bot-chip-nanointerface", "angels-personal-equipment-power-d", "ae")
-  end
-
-  if mods["Portable_power"] then
-    sort_item_recipe_order("portable-generator-equipment", "angels-personal-equipment-power-a", "z")
-  end
+if mods["angelspetrochem"] and mods["PCPRedux"] then
+  sort_recipe_order("carbon-dioxide", "petrochem-basics", "d[carbon-separation-3]")
 end
 
 --DrD
