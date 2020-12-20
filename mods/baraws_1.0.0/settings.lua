8bf2c87d0cf02298a4af2d635a9054e72c538525
drd-avel <avelinadmitrieva@gmail.com>
2020-12-21 01:45:16 +0300

    Новый мод baraws_1.0.0 -- теперь должен правильно отображаться список необходимого сырья

---
+++ mods/baraws_1.0.0/settings.lua
@@ -0,0 +1,17 @@
+data:extend
+({
+	{
+		type = "bool-setting",
+		name = "baraws-all-made-in",
+		order = "aa",
+		setting_type = "startup",
+		default_value = false,
+	},
+	{
+		type = "bool-setting",
+		name = "baraws-all-products",
+		order = "aa",
+		setting_type = "startup",
+		default_value = false,
+	},
+})
