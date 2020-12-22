eb10fdfd7ae4eaaedb651343bc0adf6b6d3f8400
drd-avel <avelinadmitrieva@gmail.com>
2020-12-22 14:17:25 +0300

    Попытка убрать бесплатный насос из рецептов

--- mods/zzzparanoidal_1.0.0/prototypes/recipefixes.lua
+++ mods/zzzparanoidal_1.0.0/prototypes/recipefixes.lua
@@ -1,2 +1,4 @@
 --замена обычной рыбы на луч санты в рецепте механического паука
 bobmods.lib.recipe.replace_ingredient("spidertron", "raw-fish", "alien-fish-3-raw")
+--вывод из эксплуатации бесплатного насоса на воду	
+data.raw.recipe["ground-water-pump"].hidden = true
