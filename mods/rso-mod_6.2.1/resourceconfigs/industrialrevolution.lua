9002788310905671d2ae54300a89284b09cce135
drd-avel <avelinadmitrieva@gmail.com>
2021-01-14 00:50:12 +0300

    Обновление до rso-mod_6.2.1

--- mods/rso-mod_6.2.1/resourceconfigs/industrialrevolution.lua
+++ mods/rso-mod_6.2.1/resourceconfigs/industrialrevolution.lua
@@ -8,7 +8,7 @@ function fillIndustrialRevolutionConfig(config)
 		-- general spawn params
 		allotment=100, -- how common resource is
 		spawns_per_region={min=1, max=1}, --number of chunks
-		richness=15000,        -- resource_ore has only one richness value - resource-liquid has min/max
+		richness=14000,        -- resource_ore has only one richness value - resource-liquid has min/max
 		
 		size={min=15, max=25}, -- rough radius of area, too high value can produce square shaped areas
 		min_amount=300,
@@ -19,9 +19,9 @@ function fillIndustrialRevolutionConfig(config)
 	config["gold-ore"] = {
 		type="resource-ore",
 		
-		allotment=60,
+		allotment=50,
 		spawns_per_region={min=1, max=1},
-		richness=10000,
+		richness=7000,
 		size={min=10, max=15},
 		min_amount=250,
 	}
