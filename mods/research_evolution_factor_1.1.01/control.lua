15f5f190171d80b524ba26ad5c46b40df60cac04
sbeljakov <sbelyakov@mail.ru>
2021-01-17 15:53:28 +0300

    research_evolution_factor: рефакторинг и изменение алгоритма вычиссления стоимости бесконечных исследований

--- mods/research_evolution_factor_1.1.01/control.lua
+++ mods/research_evolution_factor_1.1.01/control.lua
@@ -1,4 +1,54 @@
+require "parameters"
+global.infinite_research_units={}
+local function tech_cost(tech)
+  local sum = 0
+  if tech.research_unit_ingredients then
+    for _, item in pairs(tech.research_unit_ingredients) do
+      local name, amount
+      if item.type then
+        if item.type=="item" then
+          name = item.name
+          amount = item.amount
+        end
+      else
+        name = item[1]
+        amount = item[2]
+      end
+      if name then 
+        --log(name..'----'..amount) 
+        sum  = sum + amount * (science_packet_cost[name] or 1)
+      end
+    end
+  end
+  local unit_count = tech.research_unit_count
+  if tech.research_unit_count_formula then
+    unit_count = global.infinite_research_units[tech.name]
+    global.infinite_research_units[tech.name] = nil       --erasing unusial record
+    --local level = tech.level
+    --if formula and level ~= nil then
+    --  level = math.max(0, (tech.level or 1)-1)
+    --  unit_count = game.evaluate_expression(formula, {L=level, l=level})
+    --end
+    --log("calculate unit count by formula "..formula.." with level "..(level or "NAN").." and result value "..unit_count)
+  end    
+  --log("new tech "..tech.name.." researched for "..(unit_count or 0))
+  sum = sum * (unit_count or 0)
+  return sum*0.00001*linear_factor+constant_factor*0.01
+end
 
-require 'evolution-control'
-
-script.on_event(defines.events.on_research_finished, researchEvolutionFactor_on_research_finished)
+script.on_event(defines.events.on_research_finished, function(event)
+  local inc = tech_cost(event.research)
+  --log("adding "..inc.." to evolution factor")
+  for _,force in pairs(game.forces) do
+    if force.ai_controllable or force == game.forces.enemy then
+      force.evolution_factor = 1-(1-force.evolution_factor)*math.exp(-inc)
+    end
+  end
+end)
+script.on_event(defines.events.on_research_started, function(event)
+  local tech = event.research
+  if tech.research_unit_count_formula  then  --is infinite tech?
+    global.infinite_research_units[tech.name] = tech.research_unit_count -- save researched technology units count for later use
+    --log("saving "..tech.name.." units count "..tech.research_unit_count)
+  end   
+end)
--- mods/research_evolution_factor_1.1.01/data-final-fixes.lua
+++ mods/research_evolution_factor_1.1.01/data-final-fixes.lua
@@ -1,3 +1,40 @@
-require "evolution-control"
+require "parameters"
+local function proto_tech_cost(tech)
+  --log("new tech "..tech.name.." researched for "..(tech.unit.count or 0))
+  local sum = 0
+  if tech.unit.ingredients then
+    for _, item in pairs(tech.unit.ingredients) do
+      local name, amount
+      if item.type then
+        if item.type=="item" then
+          name = item.name
+          amount = item.amount
+        end
+      else
+        name = item[1]
+        amount = item[2]
+      end
+      if name then 
+        --log(name..'----'..amount) 
+        sum  = sum + amount * (science_packet_cost[name] or 1)
+      end
+    end
+  end
+  if not tech.unit.count then return nil end
+  return tech.unit.count*sum*0.00001*linear_factor+constant_factor*0.01
+end
 
-add_tech_effects()
+for _,tech in pairs(data.raw.technology) do
+  local inc = proto_tech_cost(tech)
+  if inc then 
+    local effect = {
+      type  = "nothing",
+      effect_description = {"research-evolution-factor-effect", math.floor((1-math.exp(-inc))*100000+0.5)*0.001}
+    }
+    if tech.effects then
+      table.insert(tech.effects, effect)
+    else
+      tech.effects = {effect}
+    end
+  end  
+end  
