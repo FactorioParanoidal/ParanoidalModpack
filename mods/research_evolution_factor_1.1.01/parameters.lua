3053ac0b110e6af3d7ff81ae7e38bee618275efb
sbeljakov <sbelyakov@mail.ru>
2021-01-17 16:03:50 +0300

    research_evolution_factor:добавлен один пропущенный файл

---
+++ mods/research_evolution_factor_1.1.01/parameters.lua
@@ -0,0 +1,6 @@
+science_packet_cost = { --evolution cost coefficients for specific science packs
+  ["planetary-data"] = 1400,
+  ["station-science"] = 3000
+}
+constant_factor = 0.15  --percent of evolution per each researched technology
+linear_factor   = 0.35  --percent of evolution per 1000 total researched science packs
