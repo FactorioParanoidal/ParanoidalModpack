5003e12558bbe907e060555d0530f59e316f8c91
drd-avel <avelinadmitrieva@gmail.com>
2021-01-24 22:33:10 +0300

    Новая графика для титанового сундука, чтобы больше не путать со стальным.

---
+++ mods/KaoExtended_9.16.12/prototypes/chest-gfx.lua
@@ -0,0 +1,26 @@
+
+data.raw["container"]["titanium-chest"].picture =
+    {
+      layers =
+      {
+        {
+          filename = "__KaoExtended__/graphics/chest/hr-titanium-chest.png",
+          priority = "extra-high",
+          width = 66,
+          height = 86,
+          shift = util.by_pixel(0, -3),
+          scale = 0.5,
+        },
+        {
+          filename = "__KaoExtended__/graphics/chest/hr-titanium-chest-shadow.png",
+          priority = "extra-high",
+          width = 116,
+          height = 48,
+          shift = util.by_pixel(14, 6),
+          draw_as_shadow = true,
+          scale = 0.5,
+        }
+      }
+    }
+data.raw["item"]["titanium-chest"].icon = "__KaoExtended__/graphics/chest/hr-titanium-chest-icon.png"
+data.raw["item"]["titanium-chest"].icon_size = 64
