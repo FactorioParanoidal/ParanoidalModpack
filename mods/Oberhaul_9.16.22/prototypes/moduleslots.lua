18a2964d1a18ddb07bbd531928cd3d8015c9f753
sbeljakov <sbelyakov@mail.ru>
2021-01-25 23:10:21 +0300

    Oberhaul: не будет вылетать, когда отсутствуют химзаводы Баба

--- mods/Oberhaul_9.16.22/prototypes/moduleslots.lua
+++ mods/Oberhaul_9.16.22/prototypes/moduleslots.lua
@@ -49,30 +49,39 @@ data.raw["assembling-machine"]["assembling-machine-6"].module_specification =
 }
 
 --Nerf Bob Assembly OIL
-data.raw["assembling-machine"]["chemical-plant"].module_specification = 
-{
+local assembling_machine = data.raw["assembling-machine"]
+if assembling_machine["chemical-plant"] then 
+  assembling_machine["chemical-plant"].module_specification = 
+  {
       module_slots = 1,
       module_info_icon_shift = {0, 0.5},
       module_info_multi_row_initial_height_modifier = -0.3
-}
-data.raw["assembling-machine"]["chemical-plant-2"].module_specification = 
-{
+  }
+end
+if assembling_machine["chemical-plant-2"] then 
+  assembling_machine["chemical-plant-2"].module_specification = 
+  {
       module_slots = 2,
       module_info_icon_shift = {0, 0.5},
       module_info_multi_row_initial_height_modifier = -0.3
-}
-data.raw["assembling-machine"]["chemical-plant-3"].module_specification = 
-{
+  }
+end
+if assembling_machine["chemical-plant-3"] then 
+  assembling_machine["chemical-plant-3"].module_specification = 
+  {
       module_slots = 3,
       module_info_icon_shift = {0, 0.5},
       module_info_multi_row_initial_height_modifier = -0.3
-}
-data.raw["assembling-machine"]["chemical-plant-4"].module_specification = 
-{
+  }
+end
+if assembling_machine["chemical-plant-4"] then 
+  assembling_machine["chemical-plant-4"].module_specification = 
+  {
       module_slots = 4,
       module_info_icon_shift = {0, 0.5},
       module_info_multi_row_initial_height_modifier = -0.3
-}
+  }
+end
 data.raw["assembling-machine"]["oil-refinery"].module_specification = 
 {
       module_slots = 1,
