9d620a06e6ca1e61ec8b1f92f7b0a4d981a3aacd
drd-avel <avelinadmitrieva@gmail.com>
2021-01-26 02:52:13 +0300

    Обновление графики технологий, модулей и некоторых металлов и сплавов

---
+++ mods/bobicons_1.0.0/prototypes/technology-icons.lua
@@ -0,0 +1,250 @@
+--METALS--
+
+data.raw.technology["aluminium-processing"].icon = "__bobicons__/graphics/technology/aluminium-processing.png"
+data.raw.technology["aluminium-processing"].icon_size = 128
+data.raw.technology["cobalt-processing"].icon = "__bobicons__/graphics/technology/cobalt-processing.png"
+data.raw.technology["cobalt-processing"].icon_size = 128
+data.raw.technology["gold-processing"].icon = "__bobicons__/graphics/technology/gold-processing.png"
+data.raw.technology["gold-processing"].icon_size = 128
+data.raw.technology["invar-processing"].icon = "__bobicons__/graphics/technology/invar-processing.png"
+data.raw.technology["invar-processing"].icon_size = 128
+data.raw.technology["lead-processing"].icon = "__bobicons__/graphics/technology/lead-processing.png"
+data.raw.technology["lead-processing"].icon_size = 128
+data.raw.technology["lithium-processing"].icon = "__bobicons__/graphics/technology/lithium-processing.png"
+data.raw.technology["lithium-processing"].icon_size = 128
+data.raw.technology["nickel-processing"].icon = "__bobicons__/graphics/technology/nickel-processing.png"
+data.raw.technology["nickel-processing"].icon_size = 128
+data.raw.technology["nitinol-processing"].icon = "__bobicons__/graphics/technology/nitinol-processing.png"
+data.raw.technology["nitinol-processing"].icon_size = 128
+data.raw.technology["silicon-processing"].icon = "__bobicons__/graphics/technology/silicon-processing.png"
+data.raw.technology["silicon-processing"].icon_size = 128
+data.raw.technology["sodium-processing"].icon = "__bobicons__/graphics/technology/sodium-processing.png"
+data.raw.technology["sodium-processing"].icon_size = 128
+data.raw.technology["titanium-processing"].icon = "__bobicons__/graphics/technology/titanium-processing.png"
+data.raw.technology["titanium-processing"].icon_size = 128
+data.raw.technology["tungsten-processing"].icon = "__bobicons__/graphics/technology/tungsten-processing.png"
+data.raw.technology["tungsten-processing"].icon_size = 128
+data.raw.technology["tungsten-alloy-processing"].icon = "__bobicons__/graphics/technology/tungsten-alloy-processing.png"
+data.raw.technology["tungsten-alloy-processing"].icon_size = 128
+data.raw.technology["zinc-processing"].icon = "__bobicons__/graphics/technology/zinc-processing.png"
+data.raw.technology["zinc-processing"].icon_size = 128
+
+
+--CHEMISTRY--
+
+data.raw.technology["basic-chemistry"].icons = nil
+data.raw.technology["basic-chemistry"].icon = "__bobicons__/graphics/technology/basic-chemistry.png"
+data.raw.technology["basic-chemistry"].icon_size = 128
+data.raw.technology["basic-chemistry-2"].icons = nil
+data.raw.technology["basic-chemistry-2"].icon = "__bobicons__/graphics/technology/basic-chemistry.png"
+data.raw.technology["basic-chemistry-2"].icon_size = 128
+data.raw.technology["basic-chemistry-3"].icons = nil
+data.raw.technology["basic-chemistry-3"].icon = "__bobicons__/graphics/technology/basic-chemistry.png"
+data.raw.technology["basic-chemistry-3"].icon_size = 128
+
+
+data.raw.technology["angels-coal-processing"].icons = nil
+data.raw.technology["angels-coal-processing"].icon = "__bobicons__/graphics/technology/coal-tech.png"
+data.raw.technology["angels-coal-processing"].icon_size = 128
+data.raw.technology["angels-coal-processing-2"].icons = nil
+data.raw.technology["angels-coal-processing-2"].icon = "__bobicons__/graphics/technology/coal-tech.png"
+data.raw.technology["angels-coal-processing-2"].icon_size = 128
+data.raw.technology["angels-coal-cracking"].icons = nil
+data.raw.technology["angels-coal-cracking"].icon = "__bobicons__/graphics/technology/coal-tech.png"
+data.raw.technology["angels-coal-cracking"].icon_size = 128
+
+data.raw.technology["angels-sulfur-processing-1"].icons = nil
+data.raw.technology["angels-sulfur-processing-1"].icon = "__bobicons__/graphics/technology/sulfur-tech.png"
+data.raw.technology["angels-sulfur-processing-1"].icon_size = 128
+data.raw.technology["angels-sulfur-processing-2"].icons = nil
+data.raw.technology["angels-sulfur-processing-2"].icon = "__bobicons__/graphics/technology/sulfur-tech.png"
+data.raw.technology["angels-sulfur-processing-2"].icon_size = 128
+data.raw.technology["angels-sulfur-processing-3"].icons = nil
+data.raw.technology["angels-sulfur-processing-3"].icon = "__bobicons__/graphics/technology/sulfur-tech.png"
+data.raw.technology["angels-sulfur-processing-3"].icon_size = 128
+data.raw.technology["angels-sulfur-processing-4"].icons = nil
+data.raw.technology["angels-sulfur-processing-4"].icon = "__bobicons__/graphics/technology/sulfur-tech.png"
+data.raw.technology["angels-sulfur-processing-4"].icon_size = 128
+
+data.raw.technology["angels-nitrogen-processing-1"].icons = nil
+data.raw.technology["angels-nitrogen-processing-1"].icon = "__bobicons__/graphics/technology/nitrogen-tech.png"
+data.raw.technology["angels-nitrogen-processing-1"].icon_size = 128
+data.raw.technology["angels-nitrogen-processing-2"].icons = nil
+data.raw.technology["angels-nitrogen-processing-2"].icon = "__bobicons__/graphics/technology/nitrogen-tech.png"
+data.raw.technology["angels-nitrogen-processing-2"].icon_size = 128
+data.raw.technology["angels-nitrogen-processing-3"].icons = nil
+data.raw.technology["angels-nitrogen-processing-3"].icon = "__bobicons__/graphics/technology/nitrogen-tech.png"
+data.raw.technology["angels-nitrogen-processing-3"].icon_size = 128
+data.raw.technology["angels-nitrogen-processing-4"].icons = nil
+data.raw.technology["angels-nitrogen-processing-4"].icon = "__bobicons__/graphics/technology/nitrogen-tech.png"
+data.raw.technology["angels-nitrogen-processing-4"].icon_size = 128
+
+data.raw.technology["chlorine-processing-1"].icons = nil
+data.raw.technology["chlorine-processing-1"].icon = "__bobicons__/graphics/technology/chlorine-tech.png"
+data.raw.technology["chlorine-processing-1"].icon_size = 128
+data.raw.technology["chlorine-processing-2"].icons = nil
+data.raw.technology["chlorine-processing-2"].icon = "__bobicons__/graphics/technology/chlorine-tech.png"
+data.raw.technology["chlorine-processing-2"].icon_size = 128
+data.raw.technology["chlorine-processing-3"].icons = nil
+data.raw.technology["chlorine-processing-3"].icon = "__bobicons__/graphics/technology/chlorine-tech.png"
+data.raw.technology["chlorine-processing-3"].icon_size = 128
+
+
+--MODULES--
+
+data.raw.technology["speed-module"].icon_size = 128
+data.raw.technology["speed-module"].icon_mipmaps = nil
+data.raw.technology["speed-module"].icon = "__bobicons__/graphics/technology/modules/speed-module-1.png"
+data.raw.technology["speed-module-2"].icon_size = 128
+data.raw.technology["speed-module-2"].icon_mipmaps = nil
+data.raw.technology["speed-module-2"].icon = "__bobicons__/graphics/technology/modules/speed-module-2.png"
+data.raw.technology["speed-module-3"].icon_size = 128
+data.raw.technology["speed-module-3"].icon_mipmaps = nil
+data.raw.technology["speed-module-3"].icon = "__bobicons__/graphics/technology/modules/speed-module-3.png"
+data.raw.technology["speed-module-4"].icon_size = 128
+data.raw.technology["speed-module-4"].icon = "__bobicons__/graphics/technology/modules/speed-module-4.png"
+data.raw.technology["speed-module-5"].icon_size = 128
+data.raw.technology["speed-module-5"].icon = "__bobicons__/graphics/technology/modules/speed-module-5.png"
+data.raw.technology["speed-module-6"].icon_size = 128
+data.raw.technology["speed-module-6"].icon = "__bobicons__/graphics/technology/modules/speed-module-6.png"
+data.raw.technology["speed-module-7"].icon_size = 128
+data.raw.technology["speed-module-7"].icon = "__bobicons__/graphics/technology/modules/speed-module-7.png"
+data.raw.technology["speed-module-8"].icon_size = 128
+data.raw.technology["speed-module-8"].icon = "__bobicons__/graphics/technology/modules/speed-module-8.png"
+
+data.raw.technology["productivity-module"].icon_size = 128
+data.raw.technology["productivity-module"].icon_mipmaps = nil
+data.raw.technology["productivity-module"].icon = "__bobicons__/graphics/technology/modules/red-module-1.png"
+data.raw.technology["productivity-module-2"].icon_size = 128
+data.raw.technology["productivity-module-2"].icon_mipmaps = nil
+data.raw.technology["productivity-module-2"].icon = "__bobicons__/graphics/technology/modules/red-module-2.png"
+data.raw.technology["productivity-module-3"].icon_size = 128
+data.raw.technology["productivity-module-3"].icon_mipmaps = nil
+data.raw.technology["productivity-module-3"].icon = "__bobicons__/graphics/technology/modules/red-module-3.png"
+data.raw.technology["productivity-module-4"].icon_size = 128
+data.raw.technology["productivity-module-4"].icon = "__bobicons__/graphics/technology/modules/red-module-4.png"
+data.raw.technology["productivity-module-5"].icon_size = 128
+data.raw.technology["productivity-module-5"].icon = "__bobicons__/graphics/technology/modules/red-module-5.png"
+data.raw.technology["productivity-module-6"].icon_size = 128
+data.raw.technology["productivity-module-6"].icon = "__bobicons__/graphics/technology/modules/red-module-6.png"
+data.raw.technology["productivity-module-7"].icon_size = 128
+data.raw.technology["productivity-module-7"].icon = "__bobicons__/graphics/technology/modules/red-module-7.png"
+data.raw.technology["productivity-module-8"].icon_size = 128
+data.raw.technology["productivity-module-8"].icon = "__bobicons__/graphics/technology/modules/red-module-8.png"
+
+data.raw.technology["effectivity-module"].icon_size = 128
+data.raw.technology["effectivity-module"].icon_mipmaps = nil
+data.raw.technology["effectivity-module"].icon = "__bobicons__/graphics/technology/modules/yellow-module-1.png"
+data.raw.technology["effectivity-module-2"].icon_size = 128
+data.raw.technology["effectivity-module-2"].icon_mipmaps = nil
+data.raw.technology["effectivity-module-2"].icon = "__bobicons__/graphics/technology/modules/yellow-module-2.png"
+data.raw.technology["effectivity-module-3"].icon_size = 128
+data.raw.technology["effectivity-module-3"].icon_mipmaps = nil
+data.raw.technology["effectivity-module-3"].icon = "__bobicons__/graphics/technology/modules/yellow-module-3.png"
+data.raw.technology["effectivity-module-4"].icon_size = 128
+data.raw.technology["effectivity-module-4"].icon = "__bobicons__/graphics/technology/modules/yellow-module-4.png"
+data.raw.technology["effectivity-module-5"].icon_size = 128
+data.raw.technology["effectivity-module-5"].icon = "__bobicons__/graphics/technology/modules/yellow-module-5.png"
+data.raw.technology["effectivity-module-6"].icon_size = 128
+data.raw.technology["effectivity-module-6"].icon = "__bobicons__/graphics/technology/modules/yellow-module-6.png"
+data.raw.technology["effectivity-module-7"].icon_size = 128
+data.raw.technology["effectivity-module-7"].icon = "__bobicons__/graphics/technology/modules/yellow-module-7.png"
+data.raw.technology["effectivity-module-8"].icon_size = 128
+data.raw.technology["effectivity-module-8"].icon = "__bobicons__/graphics/technology/modules/yellow-module-8.png"
+
+data.raw.technology["pollution-clean-module-1"].icon_size = 128
+data.raw.technology["pollution-clean-module-1"].icon = "__bobicons__/graphics/technology/modules/pollution-clean-module-1.png"
+data.raw.technology["pollution-clean-module-2"].icon_size = 128
+data.raw.technology["pollution-clean-module-2"].icon = "__bobicons__/graphics/technology/modules/pollution-clean-module-2.png"
+data.raw.technology["pollution-clean-module-3"].icon_size = 128
+data.raw.technology["pollution-clean-module-3"].icon = "__bobicons__/graphics/technology/modules/pollution-clean-module-3.png"
+data.raw.technology["pollution-clean-module-4"].icon_size = 128
+data.raw.technology["pollution-clean-module-4"].icon = "__bobicons__/graphics/technology/modules/pollution-clean-module-4.png"
+data.raw.technology["pollution-clean-module-5"].icon_size = 128
+data.raw.technology["pollution-clean-module-5"].icon = "__bobicons__/graphics/technology/modules/pollution-clean-module-5.png"
+data.raw.technology["pollution-clean-module-6"].icon_size = 128
+data.raw.technology["pollution-clean-module-6"].icon = "__bobicons__/graphics/technology/modules/pollution-clean-module-6.png"
+data.raw.technology["pollution-clean-module-7"].icon_size = 128
+data.raw.technology["pollution-clean-module-7"].icon = "__bobicons__/graphics/technology/modules/pollution-clean-module-7.png"
+data.raw.technology["pollution-clean-module-8"].icon_size = 128
+data.raw.technology["pollution-clean-module-8"].icon = "__bobicons__/graphics/technology/modules/pollution-clean-module-8.png"
+
+data.raw.technology["pollution-create-module-1"].icon_size = 128
+data.raw.technology["pollution-create-module-1"].icon = "__bobicons__/graphics/technology/modules/pollution-create-module-1.png"
+data.raw.technology["pollution-create-module-2"].icon_size = 128
+data.raw.technology["pollution-create-module-2"].icon = "__bobicons__/graphics/technology/modules/pollution-create-module-2.png"
+data.raw.technology["pollution-create-module-3"].icon_size = 128
+data.raw.technology["pollution-create-module-3"].icon = "__bobicons__/graphics/technology/modules/pollution-create-module-3.png"
+data.raw.technology["pollution-create-module-4"].icon_size = 128
+data.raw.technology["pollution-create-module-4"].icon = "__bobicons__/graphics/technology/modules/pollution-create-module-4.png"
+data.raw.technology["pollution-create-module-5"].icon_size = 128
+data.raw.technology["pollution-create-module-5"].icon = "__bobicons__/graphics/technology/modules/pollution-create-module-5.png"
+data.raw.technology["pollution-create-module-6"].icon_size = 128
+data.raw.technology["pollution-create-module-6"].icon = "__bobicons__/graphics/technology/modules/pollution-create-module-6.png"
+data.raw.technology["pollution-create-module-7"].icon_size = 128
+data.raw.technology["pollution-create-module-7"].icon = "__bobicons__/graphics/technology/modules/pollution-create-module-7.png"
+data.raw.technology["pollution-create-module-8"].icon_size = 128
+data.raw.technology["pollution-create-module-8"].icon = "__bobicons__/graphics/technology/modules/pollution-create-module-8.png"
+
+data.raw.technology["raw-speed-module-1"].icon_size = 128
+data.raw.technology["raw-speed-module-1"].icon = "__bobicons__/graphics/technology/modules/cyan-module-1.png"
+data.raw.technology["raw-speed-module-2"].icon_size = 128
+data.raw.technology["raw-speed-module-2"].icon = "__bobicons__/graphics/technology/modules/cyan-module-2.png"
+data.raw.technology["raw-speed-module-3"].icon_size = 128
+data.raw.technology["raw-speed-module-3"].icon = "__bobicons__/graphics/technology/modules/cyan-module-3.png"
+data.raw.technology["raw-speed-module-4"].icon_size = 128
+data.raw.technology["raw-speed-module-4"].icon = "__bobicons__/graphics/technology/modules/cyan-module-4.png"
+data.raw.technology["raw-speed-module-5"].icon_size = 128
+data.raw.technology["raw-speed-module-5"].icon = "__bobicons__/graphics/technology/modules/cyan-module-5.png"
+data.raw.technology["raw-speed-module-6"].icon_size = 128
+data.raw.technology["raw-speed-module-6"].icon = "__bobicons__/graphics/technology/modules/cyan-module-6.png"
+data.raw.technology["raw-speed-module-7"].icon_size = 128
+data.raw.technology["raw-speed-module-7"].icon = "__bobicons__/graphics/technology/modules/cyan-module-7.png"
+data.raw.technology["raw-speed-module-8"].icon_size = 128
+data.raw.technology["raw-speed-module-8"].icon = "__bobicons__/graphics/technology/modules/cyan-module-8.png"
+
+data.raw.technology["green-module-1"].icon_size = 128
+data.raw.technology["green-module-1"].icon = "__bobicons__/graphics/technology/modules/green-module-1.png"
+data.raw.technology["green-module-2"].icon_size = 128
+data.raw.technology["green-module-2"].icon = "__bobicons__/graphics/technology/modules/green-module-2.png"
+data.raw.technology["green-module-3"].icon_size = 128
+data.raw.technology["green-module-3"].icon = "__bobicons__/graphics/technology/modules/green-module-3.png"
+data.raw.technology["green-module-4"].icon_size = 128
+data.raw.technology["green-module-4"].icon = "__bobicons__/graphics/technology/modules/green-module-4.png"
+data.raw.technology["green-module-5"].icon_size = 128
+data.raw.technology["green-module-5"].icon = "__bobicons__/graphics/technology/modules/green-module-5.png"
+data.raw.technology["green-module-6"].icon_size = 128
+data.raw.technology["green-module-6"].icon = "__bobicons__/graphics/technology/modules/green-module-6.png"
+data.raw.technology["green-module-7"].icon_size = 128
+data.raw.technology["green-module-7"].icon = "__bobicons__/graphics/technology/modules/green-module-7.png"
+data.raw.technology["green-module-8"].icon_size = 128
+data.raw.technology["green-module-8"].icon = "__bobicons__/graphics/technology/modules/green-module-8.png"
+
+data.raw.technology["raw-productivity-module-1"].icon_size = 128
+data.raw.technology["raw-productivity-module-1"].icon = "__bobicons__/graphics/technology/modules/productivity-module-1.png"
+data.raw.technology["raw-productivity-module-2"].icon_size = 128
+data.raw.technology["raw-productivity-module-2"].icon = "__bobicons__/graphics/technology/modules/productivity-module-2.png"
+data.raw.technology["raw-productivity-module-3"].icon_size = 128
+data.raw.technology["raw-productivity-module-3"].icon = "__bobicons__/graphics/technology/modules/productivity-module-3.png"
+data.raw.technology["raw-productivity-module-4"].icon_size = 128
+data.raw.technology["raw-productivity-module-4"].icon = "__bobicons__/graphics/technology/modules/productivity-module-4.png"
+data.raw.technology["raw-productivity-module-5"].icon_size = 128
+data.raw.technology["raw-productivity-module-5"].icon = "__bobicons__/graphics/technology/modules/productivity-module-5.png"
+data.raw.technology["raw-productivity-module-6"].icon_size = 128
+data.raw.technology["raw-productivity-module-6"].icon = "__bobicons__/graphics/technology/modules/productivity-module-6.png"
+data.raw.technology["raw-productivity-module-7"].icon_size = 128
+data.raw.technology["raw-productivity-module-7"].icon = "__bobicons__/graphics/technology/modules/productivity-module-7.png"
+data.raw.technology["raw-productivity-module-8"].icon_size = 128
+data.raw.technology["raw-productivity-module-8"].icon = "__bobicons__/graphics/technology/modules/productivity-module-8.png"
+
+data.raw.technology["god-module-1"].icon_size = 128
+data.raw.technology["god-module-1"].icon = "__bobicons__/graphics/technology/modules/god-module-1.png"
+data.raw.technology["god-module-2"].icon_size = 128
+data.raw.technology["god-module-2"].icon = "__bobicons__/graphics/technology/modules/god-module-2.png"
+data.raw.technology["god-module-3"].icon_size = 128
+data.raw.technology["god-module-3"].icon = "__bobicons__/graphics/technology/modules/god-module-3.png"
+data.raw.technology["god-module-4"].icon_size = 128
+data.raw.technology["god-module-4"].icon = "__bobicons__/graphics/technology/modules/god-module-4.png"
+data.raw.technology["god-module-5"].icon_size = 128
+data.raw.technology["god-module-5"].icon = "__bobicons__/graphics/technology/modules/god-module-5.png"
+
