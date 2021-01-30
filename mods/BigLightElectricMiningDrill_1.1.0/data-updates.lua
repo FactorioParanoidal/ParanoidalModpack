3464325ca10c2512e1204277ca3bf3c5fa13ff29
drd-avel <avelinadmitrieva@gmail.com>
2021-01-30 16:01:19 +0300

    Обновление до BigLightElectricMiningDrill_1.1.0

--- mods/BigLightElectricMiningDrill_1.1.0/data-updates.lua
+++ mods/BigLightElectricMiningDrill_1.1.0/data-updates.lua
@@ -10,6 +10,15 @@ if mods["ev-mining-drills"] then
     electric_mining_drill("electric-mining-drill-mk5")
 end
 
+-- Hiladdar Mining
+if mods["Hiladdar_Mining"] then 
+    electric_mining_drill("hsmd-electric-mining-drill-mk2")
+    electric_mining_drill("hsmd-electric-mining-drill-mk3")
+    electric_mining_drill("hsmd-electric-mining-drill-mk4")
+    electric_mining_drill("hsmd-electric-mining-drill-mk5")
+    electric_mining_drill("hsmd-electric-mining-drill-mk6")
+end
+
 -- Bob Mining
 if mods["bobmining"] then 
     electric_mining_drill("bob-mining-drill-1")
