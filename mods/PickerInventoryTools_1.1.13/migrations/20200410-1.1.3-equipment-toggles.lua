bf5793077299bd2177fe26927cc8cc9f77ed284c
drd-avel <avelinadmitrieva@gmail.com>
2021-01-30 16:23:54 +0300

    Обновление до PickerInventoryTools_1.1.13

--- mods/PickerInventoryTools_1.1.13/migrations/20200410-1.1.3-equipment-toggles.lua
+++ mods/PickerInventoryTools_1.1.13/migrations/20200410-1.1.3-equipment-toggles.lua
@@ -2,7 +2,7 @@
 for _, player in pairs(game.players) do
     local force = player.force
     for _, shortcut in pairs(game.shortcut_prototypes) do
-        if shortcut.name:find('^toggle%-') and not player.is_shortcut_available(shortcut.name) then
+        if shortcut.name:find('^toggle%-') and shortcut.action == 'lua' and not player.is_shortcut_available(shortcut.name) then
             local tech = shortcut.technology_to_unlock
             if tech and force.technologies[tech.name].researched then
                 player.set_shortcut_available(shortcut.name, true)
--- mods/PickerInventoryTools_1.1.13/settings.lua
+++ mods/PickerInventoryTools_1.1.13/settings.lua
@@ -3,7 +3,7 @@ data:extend {
         type = 'bool-setting',
         name = 'picker-auto-sort-inventory',
         setting_type = 'runtime-per-user',
-        default_value = true,
+        default_value = false,
         order = 'a'
     },
     {
