c3e9696dfb5e7fa56655dc8dc4bb678de7648408
drd-avel <avelinadmitrieva@gmail.com>
2021-01-31 23:33:01 +0300

    Убираем возможность крафта подземной помпы даже без рецепта.

--- mods/zzzparanoidal_1.0.0/prototypes/recipefixes.lua
+++ mods/zzzparanoidal_1.0.0/prototypes/recipefixes.lua
@@ -2,3 +2,4 @@
 bobmods.lib.recipe.replace_ingredient("spidertron", "raw-fish", "alien-fish-3-raw")
 --вывод из эксплуатации бесплатного насоса на воду	
 data.raw.recipe["ground-water-pump"].hidden = true
+data.raw.recipe["ground-water-pump"].enabled = false
