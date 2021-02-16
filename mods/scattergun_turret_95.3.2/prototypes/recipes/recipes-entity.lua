c94fa8a0eacb0f1b0e945b869deac2361a55f90c
drd-avel <avelinadmitrieva@gmail.com>
2021-02-16 23:57:06 +0300

    Чуть посложнее рецепты модульных турелей

--- mods/scattergun_turret_95.3.2/prototypes/recipes/recipes-entity.lua
+++ mods/scattergun_turret_95.3.2/prototypes/recipes/recipes-entity.lua
@@ -1,10 +1,12 @@
+local MP = 5 --drd
+
 data:extend({
 	{
 		type = "recipe",
 		name = "scattergun-turret",
 		enabled = false,
 		energy_required = 20,
-		ingredients = {{"stone-brick", 10},{"iron-plate", 10},{"copper-plate", 15},{"iron-gear-wheel", 10}},
+		ingredients = {{"stone-brick", 10*MP},{"iron-plate", 10*MP},{"copper-plate", 15*MP},{"iron-gear-wheel", 10*MP}},
     		results = {{type="item", name="scattergun-turret", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/scattergun-turret.png",
     		icon_size = 32,
@@ -174,7 +176,9 @@ data:extend({
 		name = "w93-modular-turret-base",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"stone-brick", 15},{"steel-plate", 8},{"engine-unit", 1}},
+		ingredients = 	{{"stone-brick", 10*MP},{"steel-plate", 8*MP},{"engine-unit", 1*MP},
+						{"gun-turret", 1} --drd
+						},
     		results = {{type="item", name="w93-modular-turret-base", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-turret-base.png",
     		icon_size = 64,
@@ -184,7 +188,8 @@ data:extend({
 		name = "w93-modular-turret2-base",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"concrete", 25},{"plastic-bar", 20},{"electric-engine-unit", 1},{"processing-unit", 2}},
+		ingredients = {{"concrete", 15*MP},{"plastic-bar", 16*MP},{"electric-engine-unit", 2*MP},
+						{"w93-modular-turret-base", 1},{"electronic-circuit", 2*MP}}, --drd
     		results = {{type="item", name="w93-modular-turret2-base", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-turret2-base.png",
     		icon_size = 64,
@@ -194,7 +199,9 @@ data:extend({
 		name = "w93-modular-gun-hmg",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"iron-plate", 6},{"electronic-circuit", 1},{"iron-gear-wheel", 2}},
+		ingredients = {{"iron-plate", 6*MP},{"electronic-circuit", 1*MP},{"iron-gear-wheel", 2*MP}
+		,{"submachine-gun", 2} --drd
+		},
     		results = {{type="item", name="w93-modular-gun-hmg", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-gun-hmg.png",
     		icon_size = 64,
@@ -204,7 +211,7 @@ data:extend({
 		name = "w93-modular-gun-gatling",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"iron-stick", 12},{"copper-plate", 8},{"advanced-circuit", 2},{"electric-engine-unit", 1}},
+		ingredients = {{"iron-stick", 12*MP},{"copper-plate", 8*MP},{"advanced-circuit", 2*MP},{"electric-engine-unit", 1*MP}},
     		results = {{type="item", name="w93-modular-gun-gatling", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-gun-gatling.png",
     		icon_size = 64,
@@ -214,7 +221,7 @@ data:extend({
 		name = "w93-modular-gun-lcannon",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"copper-plate", 5},{"steel-plate", 4}, {"advanced-circuit", 1},{"iron-gear-wheel", 4}},
+		ingredients = {{"copper-plate", 5*MP},{"steel-plate", 4*MP}, {"advanced-circuit", 1*MP},{"iron-gear-wheel", 4*MP}},
     		results = {{type="item", name="w93-modular-gun-lcannon", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-gun-lcannon.png",
     		icon_size = 64,
@@ -235,8 +242,8 @@ data:extend({
 		category = "crafting-with-fluid",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"copper-plate", 10},{"steel-plate", 12}, {"processing-unit", 1}, {type="fluid", name="lubricant", amount=50}
-},
+		ingredients = {{"copper-plate", 10*MP},{"steel-plate", 12*MP}, {"processing-unit", 1*MP}, {type="fluid", name="lubricant", amount=50*MP}
+						},
     		results = {{type="item", name="w93-modular-gun-hcannon", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-gun-hcannon.png",
     		icon_size = 64,
@@ -246,7 +253,7 @@ data:extend({
 		name = "w93-modular-gun-rocket",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"steel-plate", 5},{"advanced-circuit", 2},{"rocket-launcher", 2}},
+		ingredients = {{"steel-plate", 5*MP},{"advanced-circuit", 2*MP},{"rocket-launcher", 2}},
     		results = {{type="item", name="w93-modular-gun-rocket", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-gun-rocket.png",
     		icon_size = 64,
@@ -256,7 +263,9 @@ data:extend({
 		name = "w93-modular-gun-tlaser",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"plastic-bar", 8},{"steel-plate", 2}, {"processing-unit", 2},{"battery", 10}},
+		ingredients = {{"plastic-bar", 8*MP},{"steel-plate", 2*MP}, {"advanced-circuit", 2*MP},{"battery", 10*MP},
+						{"laser-turret", 1},{"ruby-5", 2*MP}
+						},
     		results = {{type="item", name="w93-modular-gun-tlaser", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-gun-tlaser.png",
     		icon_size = 64,
@@ -266,7 +275,7 @@ data:extend({
 		name = "w93-modular-gun-beam",
 		enabled = false,
 		energy_required = 10,
-		ingredients = {{"uranium-fuel-cell", 1},{"small-lamp",1},{"low-density-structure", 1},{"poison-capsule", 1}, {"processing-unit", 2},{"battery", 8}},
+		ingredients = {{"uranium-fuel-cell", 1*MP},{"w93-modular-gun-tlaser",1},{"low-density-structure", 1*MP},{"poison-capsule", 1*MP}, {"processing-unit", 2*MP},{"lithium-ion-battery", 8*MP}},
     		results = {{type="item", name="w93-modular-gun-beam", amount=1}},
 		icon = "__scattergun_turret__/graphics/icons/modular-gun-beam.png",
     		icon_size = 64,
@@ -276,7 +285,7 @@ data:extend({
 		name = "w93-turret-light-cannon-shells",
 		enabled = false,
 		energy_required = 8,
-		ingredients = {{"copper-plate", 6}, {"steel-plate", 6}},
+		ingredients = {{"nickel-plate", 6}, {"steel-plate", 6}}, --drd
 		result = "w93-turret-light-cannon-shells",
 		icon = "__scattergun_turret__/graphics/icons/turret-light-cannon-shells.png",
     		icon_size = 64,
