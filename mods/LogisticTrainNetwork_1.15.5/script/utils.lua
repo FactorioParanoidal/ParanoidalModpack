4432d9b4872b3c903ff385302978acf895941946
drd-avel <avelinadmitrieva@gmail.com>
2021-03-13 16:22:38 +0300

    Обновление до LogisticTrainNetwork_1.15.5

---
+++ mods/LogisticTrainNetwork_1.15.5/script/utils.lua
@@ -0,0 +1,51 @@
+--[[ Copyright (c) 2017 Optera
+ * Part of Logistics Train Network
+ * Control stage utility functions
+ *
+ * See LICENSE.md in the project directory for license information.
+--]]
+
+--GetTrainCapacity(train)
+local function getCargoWagonCapacity(entity)
+  local capacity = entity.prototype.get_inventory_size(defines.inventory.cargo_wagon)
+  -- log("(getCargoWagonCapacity) capacity for "..entity.name.." = "..capacity)
+  global.WagonCapacity[entity.name] = capacity
+  return capacity
+end
+
+local function getFluidWagonCapacity(entity)
+  local capacity = 0
+  for n=1, #entity.fluidbox do
+    capacity = capacity + entity.fluidbox.get_capacity(n)
+  end
+  -- log("(getFluidWagonCapacity) capacity for "..entity.name.." = "..capacity)
+  global.WagonCapacity[entity.name] = capacity
+  return capacity
+end
+
+-- returns inventory and fluid capacity of a given train
+function GetTrainCapacity(train)
+  local inventorySize = 0
+  local fluidCapacity = 0
+  if train and train.valid then
+    for _,wagon in pairs(train.cargo_wagons) do
+      local capacity = global.WagonCapacity[wagon.name] or getCargoWagonCapacity(wagon)
+      inventorySize = inventorySize + capacity
+    end
+    for _,wagon in pairs(train.fluid_wagons) do
+      local capacity = global.WagonCapacity[wagon.name] or getFluidWagonCapacity(wagon)
+      fluidCapacity = fluidCapacity + capacity
+    end
+  end
+  return inventorySize, fluidCapacity
+end
+
+-- returns gps string from entity or just string if entity is invalid
+function MakeGpsString(entity, name)
+  if message_include_gps and entity and entity.valid then
+    return format("%s [gps=%s,%s,%s]", name, entity.position["x"], entity.position["y"], entity.surface.name)
+  else
+    return name
+  end
+end
+
