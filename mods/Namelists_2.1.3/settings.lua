9ac8810634537c819382cf2fe05e332258c7b3b6
drd-avel <avelinadmitrieva@gmail.com>
2021-03-13 19:44:55 +0300

    Новый мод Namelists_2.1.3 позволяет добавлять имена например станциям.

---
+++ mods/Namelists_2.1.3/settings.lua
@@ -0,0 +1,44 @@
+data:extend({
+  {
+    type = "bool-setting",
+    name = "namelists-backer-roboports",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "a"
+  },
+  {
+    type = "bool-setting",
+    name = "namelists-backer-labs",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "b"
+  },
+  {
+    type = "bool-setting",
+    name = "namelists-backer-stations",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "c"
+  },
+  {
+    type = "bool-setting",
+    name = "namelists-backer-locomotives",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "d"
+  },
+  {
+    type = "bool-setting",
+    name = "namelists-backer-radars",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "e"
+  },
+  {
+    type = "bool-setting",
+    name = "namelists-backer-default",
+    setting_type = "runtime-global",
+    default_value = false,
+    order = "f"
+  }
+})
