083cc2710f0f602ac71e7e76d3f273b1badc3fbc
drd-avel <avelinadmitrieva@gmail.com>
2021-03-13 16:27:06 +0300

    Переезд на LTN_Combinator_Fix_1.1.1 (были вылеты сервера с LTN_Combinator_Modernized)

---
+++ mods/LTN_Combinator_Fix_1.1.1/data.lua
@@ -0,0 +1,11 @@
+require "prototypes.ltnc-prototype"
+require "prototypes.styles"
+
+-- register tab key event
+--local tab_event = {
+--    type = "custom-input",
+--    name = "ltnc-tab-key-event",
+--    key_sequence = "TAB",
+--    consuming = "none"
+--}
+--data:extend({tab_event})
--- mods/LTN_Combinator_Modernized_0.9.8/data.lua
+++
@@ -1,5 +0,0 @@
-flib = require("__flib__.data-util")
-require ("prototypes.entity")
-require ("prototypes.event")
-require ("prototypes.styles")
-flib = nil
