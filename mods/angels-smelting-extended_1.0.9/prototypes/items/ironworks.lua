b8c767a644708a1f79acdecb165dd7a54be5cf4c
drd-avel <avelinadmitrieva@gmail.com>
2021-06-27 09:14:38 +0300

    Обновление до angels-smelting-extended_1.0.9

--- mods/angels-smelting-extended_1.0.9/prototypes/items/ironworks.lua
+++ mods/angels-smelting-extended_1.0.9/prototypes/items/ironworks.lua
@@ -4,7 +4,7 @@ data:extend({
     type = "item",
     name = "ASE-spent-metal-die",
     icons={
-      {icon= "__angelssmelting__/graphics/icons/non-expendable-mold.png", icon_size = 32},
+      {icon= "__angelssmelting__/graphics/icons/non-expendable-mold.png", icon_size = 32, icon_mipmaps = 1},
       {icon = "__angelsrefining__/graphics/icons/slag.png",icon_size=32,scale=0.6}--leave in the middle
     },
     icon_size = 32,
@@ -16,8 +16,7 @@ data:extend({
     type = "item",
     name = "ASE-metal-die",
     icons={
-      {icon="__angelssmelting__/graphics/icons/non-expendable-mold.png",icon_size = 32},
-      {icon="__angelssmelting__/graphics/icons/powder-steel.png",icon_size=32,scale = 0.32,shift = {-12, -12}},
+      {icon="__angelssmelting__/graphics/icons/non-expendable-mold.png",icon_size = 32, icon_mipmaps = 1},
     },
     icon_size=32,
     subgroup = "angels-mold-casting",
@@ -28,8 +27,7 @@ data:extend({
     type = "item",
     name = "ASE-sand-die",
     icons={
-      {icon= "__angelssmelting__/graphics/icons/expendable-mold.png",icon_size = 32,},
-      {icon = "__angelsrefining__/graphics/icons/solid-sand.png",icon_size=32,scale = 0.32,shift = {-12, -12}},
+      {icon= "__angelssmelting__/graphics/icons/expendable-mold.png",icon_size = 32, icon_mipmaps = 1},
     },
     icon_size = 32,
     subgroup = "angels-mold-casting",
