e8950d01070c0a38b537904c0fa32ea786e1cfb0
drd-avel <avelinadmitrieva@gmail.com>
2021-06-28 06:57:00 +0300

    Обновление до UnPollock_1.1.0

--- mods/UnPollock_1.1.0/settings.lua
+++ mods/UnPollock_1.1.0/settings.lua
@@ -3,6 +3,9 @@ data:extend{
 		setting_type = "startup",
 		default_value = .4,
 		minimum_value = 0, maximum_value = 1,
-		allowed_values = {0,.2,.4,.6,.8,1},
+	},
+	{ type = "bool-setting", name = "unpollock-no-fake-rocks",
+		setting_type = "startup",
+		default_value = false,
 	},
 }
