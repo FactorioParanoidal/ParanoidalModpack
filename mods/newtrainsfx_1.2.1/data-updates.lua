5ca521cb5b3dc0a36dc33220b269a034ca2b8b04
drd-avel <avelinadmitrieva@gmail.com>
2021-06-28 06:49:52 +0300

    Обновление до newtrainsfx_1.2.1

---
+++ mods/newtrainsfx_1.2.1/data-updates.lua
@@ -0,0 +1,6 @@
+if mods["Armored-train"] then
+	require("sfx.armored_train.at")
+end
+if mods["VehicleWagon2"] then
+	require("sfx.vehicle_wagon.vehicle-wagon")
+end
---
+++ mods/newtrainsfx_1.2.1/sfx/vehicle_wagon/vehicle-wagon.lua
@@ -0,0 +1,33 @@
+require("sfx.defines")
+data.raw["cargo-wagon"]["vehicle-wagon"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["vehicle-wagon"].drive_over_tie_trigger.sound = stuk_sounds
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-tarp"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-tarp"].drive_over_tie_trigger.sound = stuk_sounds
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-car"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-car"].drive_over_tie_trigger.sound = stuk_sounds
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-tank"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-tank"].drive_over_tie_trigger.sound = stuk_sounds
+if mods["bigtruck"] then
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-truck"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-truck"].drive_over_tie_trigger.sound = stuk_sounds
+end
+if mods["Aircraft"] then
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-cargoplane"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-cargoplane"].drive_over_tie_trigger.sound = stuk_sounds
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-jet"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-jet"].drive_over_tie_trigger.sound = stuk_sounds
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-gunship"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-gunship"].drive_over_tie_trigger.sound = stuk_sounds
+end
+if mods["SchallTankPlatoon"] then
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-tank-L"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-tank-L"].drive_over_tie_trigger.sound = stuk_sounds
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-tank-H"].working_sound.sound = working_sound
+data.raw["cargo-wagon"]["loaded-vehicle-wagon-tank-H"].drive_over_tie_trigger.sound = stuk_sounds
+-- data.raw["cargo-wagon"]["loaded-vehicle-wagon-tank-SH"].working_sound.sound = working_sound
+-- data.raw["cargo-wagon"]["loaded-vehicle-wagon-tank-SH"].drive_over_tie_trigger.sound = stuk_sounds
+end
+if mods["Krastorio2"] then
+-- data.raw["cargo-wagon"]["loaded-vehicle-wagon-kr-advanced-tank"].working_sound.sound = working_sound
+-- data.raw["cargo-wagon"]["loaded-vehicle-wagon-kr-advanced-tank"].drive_over_tie_trigger.sound = stuk_sounds
+end
