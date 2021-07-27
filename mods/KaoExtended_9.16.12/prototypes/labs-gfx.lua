960dffa554f8b9af2730674d6c9086dc47642849
drd-avel <avelinadmitrieva@gmail.com>
2021-07-27 18:00:02 +0300

    Новая графика для зданий Лаборатории МК2 и Лаборатории модулей

---
+++ mods/KaoExtended_9.16.12/prototypes/labs-gfx.lua
@@ -0,0 +1,63 @@
+data.raw["lab"]["lab-2"].on_animation =
+    {
+      layers =
+      {
+        {
+          filename = "__KaoExtended__/graphics/lab/lab-2-working.png",
+          width = 224,
+          height = 224,
+          frame_count = 32,
+          line_length = 8,
+		  scale = 0.6,
+          --animation_speed = 0.5,
+          shift = util.by_pixel(7, -10)
+        }
+      }
+    }
+
+data.raw["lab"]["lab-2"].off_animation =
+    {
+      layers =
+      {
+        {
+          filename = "__KaoExtended__/graphics/lab/lab-2-base.png",
+          width = 224,
+          height = 224,
+          frame_count = 1,
+		  scale = 0.6,
+          shift = util.by_pixel(7, -10)
+        }
+      }
+    }
+	
+data.raw["lab"]["lab-module"].on_animation =
+    {
+      layers =
+      {
+        {
+          filename = "__KaoExtended__/graphics/lab/lab-module-working.png",
+          width = 224,
+          height = 224,
+          frame_count = 32,
+          line_length = 8,
+		  scale = 0.6,
+          --animation_speed = 0.5,
+          shift = util.by_pixel(7, -10)
+        }
+      }
+    }
+
+data.raw["lab"]["lab-module"].off_animation =
+    {
+      layers =
+      {
+        {
+          filename = "__KaoExtended__/graphics/lab/lab-module-base.png",
+          width = 224,
+          height = 224,
+          frame_count = 1,
+		  scale = 0.6,
+          shift = util.by_pixel(7, -10)
+        }
+      }
+    }
