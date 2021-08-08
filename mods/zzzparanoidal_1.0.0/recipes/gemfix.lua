f48fba12ba3fea876c97e28a055cadc8275519cf
drd-avel <avelinadmitrieva@gmail.com>
2021-08-08 10:29:41 +0300

    Фикс ненужной доступности рецептов с драг.камнями

---
+++ mods/zzzparanoidal_1.0.0/recipes/gemfix.lua
@@ -0,0 +1,6 @@
+data.raw.recipe["bob-ruby-3"].enabled = false
+data.raw.recipe["bob-sapphire-3"].enabled = false
+data.raw.recipe["bob-emerald-3"].enabled = false
+data.raw.recipe["bob-amethyst-3"].enabled = false
+data.raw.recipe["bob-topaz-3"].enabled = false
+data.raw.recipe["bob-diamond-3"].enabled = false
