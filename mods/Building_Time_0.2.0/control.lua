f258f9644278b5f62b17edc6c796989d473f0883
drd-avel <avelinadmitrieva@gmail.com>
2021-08-08 09:39:01 +0300

    НОВЫЙ мод Building_Time_0.2.0 на немгновенную постройку зданий (в частности - турелей)

---
+++ mods/Building_Time_0.2.0/control.lua
@@ -0,0 +1,2 @@
+local handler = require("event_handler")
+handler.add_lib(require("script/building_time"))
---
+++ mods/Building_Time_0.2.0/shared.lua
@@ -0,0 +1,7 @@
+--Shared data interface between data and script, notably prototype names.
+
+local data = {}
+
+data.repair_rate = 25 -- In health per second
+
+return data
