c6d686020b886a5063c2de0f935a38fba7148d59
sovigod <CrashDGC@gmail.com>
2021-08-13 12:31:34 +0200

    Добавил рецепты стали, которых не хватало

--- mods/angelsextended-remelting-fixed_9.1.3/prototypes/recipes/angelssmelting/remelting-steel.lua
+++ mods/angelsextended-remelting-fixed_9.1.3/prototypes/recipes/angelssmelting/remelting-steel.lua
@@ -64,5 +64,87 @@ data:extend(
 		icon_size = 64,
 		order = "aa",
 	},
+	{
+		type = "recipe",
+		name = "molten-steel-alloy-mixing-2",
+		category = "molten-alloy-mixing",
+		subgroup = "aragas-steel-alloy-mixing",
+		enabled = "false",
+		energy_required = 4,
+		ingredients ={
+			{type="fluid", name="liquid-molten-iron", amount=480},
+			{type="fluid", name="liquid-molten-silicon", amount=120},
+			{type="fluid", name="gas-oxygen", amount=120},
+		},
+		results={{type="fluid", name="liquid-molten-steel", amount=240}},
+		icons = {
+			{
+				icon = "__angelssmelting__/graphics/icons/molten-steel.png",
+			},
+			{
+				icon = "__angelsextended-remelting-fixed__/graphics/icons/remelting.png",
+				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
+				scale = 0.32,
+				shift = {-12, -12},
+			}
+		},
+		icon_size = 64,
+		order = "ab",
+	},
+	{
+		type = "recipe",
+		name = "molten-steel-alloy-mixing-3",
+		category = "molten-alloy-mixing",
+		subgroup = "aragas-steel-alloy-mixing",
+		enabled = "false",
+		energy_required = 4,
+		ingredients ={
+			{type="fluid", name="liquid-molten-iron", amount=480},
+			{type="fluid", name="liquid-molten-manganese", amount=120},
+			{type="fluid", name="gas-oxygen", amount=120},
+		},
+		results={{type="fluid", name="liquid-molten-steel", amount=240}},
+		icons = {
+			{
+				icon = "__angelssmelting__/graphics/icons/molten-steel.png",
+			},
+			{
+				icon = "__angelsextended-remelting-fixed__/graphics/icons/remelting.png",
+				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
+				scale = 0.32,
+				shift = {-12, -12},
+			}
+		},
+		icon_size = 64,
+		order = "ac",
+	},
+	{
+		type = "recipe",
+		name = "molten-steel-alloy-mixing-4",
+		category = "molten-alloy-mixing",
+		subgroup = "aragas-steel-alloy-mixing",
+		enabled = "false",
+		energy_required = 4,
+		ingredients ={
+			{type="fluid", name="liquid-molten-iron", amount=240},
+			{type="fluid", name="liquid-molten-chrome", amount=120},
+			{type="fluid", name="gas-oxygen", amount=60},
+			{type="item", name="powder-tungsten", amount=12},
+		},
+		results={{type="fluid", name="liquid-molten-steel", amount=440}},
+		icons = {
+			{
+				icon = "__angelssmelting__/graphics/icons/molten-steel.png",
+			},
+			{
+				icon = "__angelsextended-remelting-fixed__/graphics/icons/remelting.png",
+				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
+				scale = 0.32,
+				shift = {-12, -12},
+			}
+		},
+		icon_size = 64,
+		order = "ad",
+	},
 }
 )
