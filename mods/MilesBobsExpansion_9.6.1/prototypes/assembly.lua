4ca691037e96341bb739f78b5e91f34c9f594f73
drd-avel <avelinadmitrieva@gmail.com>
2021-08-17 22:20:29 +0300

    Фоикс - переписывание кода на сборщики МК8 и МК9, теперь все они идут в списке по порядку

--- mods/MilesBobsExpansion_9.6.1/prototypes/assembly.lua
+++ mods/MilesBobsExpansion_9.6.1/prototypes/assembly.lua
@@ -18,7 +18,7 @@ data:extend(
     icon = "__MilesBobsExpansion__/graphics/icons/assembling-machine-7.png",
     icon_size = 64,
     flags = {"placeable-neutral","placeable-player","player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "assembling-machine-7"},
+    minable = {hardness = 0.2, mining_time = 1, result = "assembling-machine-7"},
     max_health = 800,
     corpse = "big-remnants",
     dying_explosion = "medium-explosion",
@@ -273,8 +273,223 @@ data:extend(
 	upgrade = true,
   },
   ]]--
+ 
+ {
+    type = "item",
+    name = "assembling-machine-8",
+    icon = "__MilesBobsExpansion__/graphics/icons/assembling-machine-8.png",
+    icon_size = 64,
+    subgroup = "bob-assembly-machine",
+    order = "c[assembling-machine-8]",
+    place_result = "assembling-machine-8",
+    stack_size = 10
+  },
+ 
+    {
+    type = "assembling-machine",
+    name = "assembling-machine-8",
+    icon = "__MilesBobsExpansion__/graphics/icons/assembling-machine-8.png",
+    icon_size = 64,
+    flags = {"placeable-neutral","placeable-player","player-creation"},
+    minable = {hardness = 0.2, mining_time = 1, result = "assembling-machine-8"},
+    max_health = 900,
+    corpse = "big-remnants",
+    dying_explosion = "medium-explosion",
+    resistances = 
+    {
+      {
+        type = "fire",
+        percent = 70
+      }
+    },
+    fluid_boxes =
+    {
+      {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {0, -2} }}
+      },
+      {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {0, 2} }}
+      },
+      off_when_no_fluid_recipe = true
+    },
+    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+    fast_replaceable_group = "assembling-machine",
+    next_upgrade = "assembling-machine-9",
+    animation =
+    {
+      layers =
+      {
+        {
+		filename = "__MilesBobsExpansion__/graphics/assembling-machine/assembling-machine-8.png",
+          priority = "high",
+          width = 192,
+          height = 256,
+          frame_count = 60,
+          line_length = 10,
+		  animation_speed = 0.125,
+          --shift = util.by_pixel(0, -0.5),
+          shift = util.by_pixel(0, -15),
+		  scale = 0.55
+        },
+      }
+    },
+    open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
+    close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
+    working_sound =
+    {
+      sound = {
+        {
+          filename = "__base__/sound/assembling-machine-t2-1.ogg",
+          volume = 0.8
+        },
+        {
+          filename = "__base__/sound/assembling-machine-t2-2.ogg",
+          volume = 0.8
+        },
+      },
+      idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
+      apparent_volume = 1.5,
+    },
+    crafting_categories = {"crafting", "advanced-crafting", "crafting-with-fluid"},
+    crafting_speed = 4.5,
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      emissions_per_minute = 0.2
+    },
+    energy_usage = "1500kW",
+	ingredient_count = 20,
+    module_specification =
+    {
+      module_slots = 8,
+      module_info_icon_shift = {0, 0.5},
+      module_info_multi_row_initial_height_modifier = -0.3
+    },
+    allowed_effects = {"consumption", "speed", "productivity", "pollution"}
+  },
+  
+ 
+ {
+    type = "item",
+    name = "assembling-machine-9",
+    icon = "__MilesBobsExpansion__/graphics/icons/assembling-machine-9.png",
+    icon_size = 64,
+    subgroup = "bob-assembly-machine",
+    order = "c[assembling-machine-9]",
+    place_result = "assembling-machine-9",
+    stack_size = 10
+  },
+ 
+    {
+    type = "assembling-machine",
+    name = "assembling-machine-9",
+    icon = "__MilesBobsExpansion__/graphics/icons/assembling-machine-9.png",
+    icon_size = 64,
+    flags = {"placeable-neutral","placeable-player","player-creation"},
+    minable = {hardness = 0.2, mining_time = 1, result = "assembling-machine-9"},
+    max_health = 1000,
+    corpse = "big-remnants",
+    dying_explosion = "medium-explosion",
+    resistances = 
+    {
+      {
+        type = "fire",
+        percent = 70
+      }
+    },
+    fluid_boxes =
+    {
+      {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {0, -2} }}
+      },
+      {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {0, 2} }}
+      },
+      off_when_no_fluid_recipe = true
+    },
+    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+    fast_replaceable_group = "assembling-machine",
+    --next_upgrade = "assembling-machine-9",
+    animation =
+    {
+      layers =
+      {
+        {
+		filename = "__MilesBobsExpansion__/graphics/assembling-machine/assembling-machine-9.png",
+          priority = "high",
+          width = 192,
+          height = 256,
+          frame_count = 60,
+          line_length = 10,
+		  animation_speed = 0.125,
+          --shift = util.by_pixel(0, -0.5),
+          shift = util.by_pixel(0, -15),
+		  scale = 0.55
+        },
+      }
+    },
+    open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
+    close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
+    working_sound =
+    {
+      sound = {
+        {
+          filename = "__base__/sound/assembling-machine-t2-1.ogg",
+          volume = 0.8
+        },
+        {
+          filename = "__base__/sound/assembling-machine-t2-2.ogg",
+          volume = 0.8
+        },
+      },
+      idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
+      apparent_volume = 1.5,
+    },
+    crafting_categories = {"crafting", "advanced-crafting", "crafting-with-fluid"},
+    crafting_speed = 5,
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      emissions_per_minute = 0.2
+    },
+    energy_usage = "2000kW",
+	ingredient_count = 20,
+    module_specification =
+    {
+      module_slots = 9,
+      module_info_icon_shift = {0, 0.5},
+      module_info_multi_row_initial_height_modifier = -0.3
+    },
+    allowed_effects = {"consumption", "speed", "productivity", "pollution"}
+  },
+  
 }
 )
+--[[
 		local new_assembler=table.deepcopy(data.raw["assembling-machine"]["assembling-machine-7"])
 		new_assembler.name="assembling-machine-8"
 		new_assembler.icon="__MilesBobsExpansion__/graphics/icons/assembling-machine-8.png"
@@ -293,6 +508,8 @@ data:extend(
         new_assembler.order="c[assembling-machine-8]"		
 		new_assembleritem.icon="__MilesBobsExpansion__/graphics/icons/assembling-machine-8.png"
 		data:extend{new_assembleritem}
+
+
 		
 		local new_assembler=table.deepcopy(data.raw["assembling-machine"]["assembling-machine-7"])
 		new_assembler.name="assembling-machine-9"
@@ -312,9 +529,9 @@ data:extend(
 		new_assembler.order="c[assembling-machine-9]"
 		new_assembleritem.icon="__MilesBobsExpansion__/graphics/icons/assembling-machine-9.png"
 		data:extend{new_assembleritem}
+]]--	
 		
-		
-		--[[
+--[[
 		
 		local new_assembler=table.deepcopy(data.raw["assembling-machine"]["assembling-machine-7"])
 		new_assembler.name="assembling-machine-10"
@@ -333,8 +550,8 @@ data:extend(
     	new_assembleritem.order="c[assembling-machine-9+1]"
 		new_assembleritem.icon="__MilesBobsExpansion__/graphics/icons/assembling-machine-10.png"
 		data:extend{new_assembleritem}
-		]]--
-	
+
+
  data.raw["assembling-machine"]["assembling-machine-8"]["animation"] = {
             layers =
 	{
@@ -369,3 +586,5 @@ data:extend(
         }
     }
 }
+
+		]]--
