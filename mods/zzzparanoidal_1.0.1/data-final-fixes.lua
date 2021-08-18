4eed71d83a12f8fa6ee092bf4f50fefb804aaac3
sovigod <CrashDGC@gmail.com>
2021-08-18 09:40:57 +0200

    фикс проверок на наличие модов

--- mods/zzzparanoidal_1.0.1/data-final-fixes.lua
+++ mods/zzzparanoidal_1.0.1/data-final-fixes.lua
@@ -8,17 +8,16 @@ require("prototypes.pipes")
 require("prototypes.map-gen-presets")
 require("prototypes.boiler-effectivity")
 
-if mods="angelsindustries" then
-require("prototypes.techfixes-angelsIndustries")
+if mods["angelsindustries"] then
+    require("prototypes.techfixes-angelsIndustries")
 end -- при наличии angelsIndustries
 
 require("prototypes.tile-concrete-brick-fix") --фикс tile для бетонного кирпича
 require("prototypes.artillery-prototype.artillery-turret-prototype-final-fix") --фикс добавляющий прототип арты в обычную арту
 
 require("recipes.gemfix")
-
-if mods="yuoki" then
-require("prototypes.yuoki")
+if mods["yuoki"] then
+    require("prototypes.yuoki")
 end -- при наличии yuoki
 
 --должно быть последним. После всех рецептов.
