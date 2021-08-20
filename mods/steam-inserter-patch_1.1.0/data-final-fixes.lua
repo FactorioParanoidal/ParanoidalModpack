30972d2566882a8896e873a9ba19bd55985f8a0f
drd-avel <avelinadmitrieva@gmail.com>
2021-08-20 22:24:27 +0300

    НОВЫЕ 2 мода на паровые манипуляторы - steam-filter-inserter_1.1.0  и  steam-inserter-patch_1.1.0

---
+++ mods/steam-inserter-patch_1.1.0/data-final-fixes.lua
@@ -0,0 +1,4 @@
+require("prototypes/entity/inserter-update")
+require("prototypes/item/inserter-update")
+require("prototypes/recipe/inserter-recipe-update")
+require("prototypes/technology/technology-update")
---
+++ mods/steam-inserter-patch_1.1.0/prototypes/technology/technology-update.lua
@@ -0,0 +1,6 @@
+local util = require("data-util")
+
+util.tech_lock_recipes("steam-power", {"steam-inserter"})
+util.tech_lock_recipes("steam-power", {"corner-steam-inserter"})
+util.tech_lock_recipes("steam-power", {"straight-corner-steam-inserter"})
+util.tech_lock_recipes("steam-power", {"corner-straight-steam-inserter"})
