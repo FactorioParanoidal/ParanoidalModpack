fc42b4958d82eac1f1992e7a71e5de0bd1ffbfed
drd-avel <avelinadmitrieva@gmail.com>
2021-08-21 15:40:13 +0300

    ВШИТ НОВЫЙ мод BobLogisticZoneExpanderRadius_0.1.0 - изменение размеров зон контроля робопортов, соответственно размерам чанков.

---
+++ mods/KaoExtended_9.16.12/prototypes/BobLogisticZoneExpanderRadius010.lua
@@ -0,0 +1,24 @@
+data.raw["roboport"]["bob-logistic-zone-expander"].logistics_radius = 16
+data.raw["roboport"]["bob-logistic-zone-expander"].construction_radius = 32
+
+data.raw["roboport"]["bob-logistic-zone-expander-2"].logistics_radius = 32
+data.raw["roboport"]["bob-logistic-zone-expander-2"].construction_radius = 64
+
+data.raw["roboport"]["bob-logistic-zone-expander-3"].logistics_radius = 48
+data.raw["roboport"]["bob-logistic-zone-expander-3"].construction_radius = 96
+
+data.raw["roboport"]["bob-logistic-zone-expander-4"].logistics_radius = 64
+data.raw["roboport"]["bob-logistic-zone-expander-4"].construction_radius = 128
+
+
+data.raw["roboport"]["roboport"].logistics_radius = 32
+data.raw["roboport"]["roboport"].construction_radius = 64
+
+data.raw["roboport"]["bob-roboport-2"].logistics_radius = 64
+data.raw["roboport"]["bob-roboport-2"].construction_radius = 128
+
+data.raw["roboport"]["bob-roboport-3"].logistics_radius = 96
+data.raw["roboport"]["bob-roboport-3"].construction_radius = 192
+
+data.raw["roboport"]["bob-roboport-4"].logistics_radius = 128
+data.raw["roboport"]["bob-roboport-4"].construction_radius = 256
