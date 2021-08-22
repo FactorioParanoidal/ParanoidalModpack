d35842e4cd3054d4de5179524ceaec546b3de89b
drd-avel <avelinadmitrieva@gmail.com>
2021-08-23 00:17:48 +0300

    Обновление до bobicons_1.1.1 - теперь добытые руды вновь будут иметь уникальный вид и не только.

---
+++ mods/bobicons_1.1.1/prototypes/update-to-hr.lua
@@ -0,0 +1,21 @@
+
+
+data.raw.item["electric-engine-unit"].icon = "__base__/graphics/technology/electric-engine.png"
+data.raw.item["electric-engine-unit"].icon_size = 256
+data.raw.item["electric-engine-unit"].icon_mipmaps = 4
+
+data.raw.car["car"].icon = "__base__/graphics/technology/automobilism.png" --item-with-entity-data?
+data.raw.car["car"].icon_size = 256
+data.raw.car["car"].icon_mipmaps = 4
+
+data.raw.item["rocket-control-unit"].icon = "__base__/graphics/technology/rocket-control-unit.png"
+data.raw.item["rocket-control-unit"].icon_size = 256
+data.raw.item["rocket-control-unit"].icon_mipmaps = 4
+
+data.raw.item["rocket-fuel"].icon = "__base__/graphics/technology/rocket-fuel.png"
+data.raw.item["rocket-fuel"].icon_size = 256
+data.raw.item["rocket-fuel"].icon_mipmaps = 4
+
+data.raw.item["sulfur"].icon = "__base__/graphics/technology/sulfur-processing.png"
+data.raw.item["sulfur"].icon_size = 256
+data.raw.item["sulfur"].icon_mipmaps = 4
