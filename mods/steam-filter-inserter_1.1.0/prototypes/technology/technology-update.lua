e1c8afaa014e84f109236eb6a93292cc6a3b8682
drd-avel <avelinadmitrieva@gmail.com>
2021-08-29 20:26:19 +0300

    Паровые манипуляторы покачто уберём, кому нужно - поставит самостоятельно (будут также добавлены в допы).

--- mods/steam-filter-inserter_1.1.0/prototypes/technology/technology-update.lua
+++
@@ -1,6 +0,0 @@
-local util = require("data-util")
-
-util.tech_lock_recipes("steam-power", {"steam-filter-inserter"})
-util.tech_lock_recipes("steam-power", {"corner-steam-filter-inserter"})
-util.tech_lock_recipes("steam-power", {"straight-corner-steam-filter-inserter"})
-util.tech_lock_recipes("steam-power", {"corner-straight-steam-filter-inserter"})
--- mods/steam-inserter-patch_1.1.0/data-final-fixes.lua
+++
@@ -1,4 +0,0 @@
-require("prototypes/entity/inserter-update")
-require("prototypes/item/inserter-update")
-require("prototypes/recipe/inserter-recipe-update")
-require("prototypes/technology/technology-update")
