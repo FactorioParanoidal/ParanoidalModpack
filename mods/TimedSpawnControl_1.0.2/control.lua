424ff9dca752e64c63641cae3eb0d6a672fc8173
drd-avel <avelinadmitrieva@gmail.com>
2021-08-29 20:26:41 +0300

    Обновление до TimedSpawnControl_1.0.2

--- mods/TimedSpawnControl_1.0.2/control.lua
+++ mods/TimedSpawnControl_1.0.2/control.lua
@@ -8,6 +8,11 @@ function ui()
 			player.gui.top.add{name = "spawn", type = "button", caption = "Set Spawn", style="spc_fentus_button"}
 		end
     end
+	for playerIndex, player in pairs(game.players) do
+		if player.gui.top.random == nil then
+			player.gui.top.add{name = "random", type = "button", caption = "Random spawn (Kill me!/I'm stuck)", description = "this will kill you!", style="spc_fentus_button"}
+		end
+    end
 end
 
 script.on_configuration_changed(function(_)
@@ -21,6 +26,27 @@ function handle_event(defines,eventou)
 	
 	-- on_gui_click
 	if eventou.name == defines.on_gui_click then
+		if eventou.element.name == "random" then
+            local player = game.players[eventou.player_index]
+
+            -- I don't think timing out random spawn is necessary.
+			--if (global.spawns[player.name]~=nil) then
+			--	local nextChangeTick = global.spawns[player.name].changeTick+(10*60)
+			--	if (nextChangeTick>game.tick) then
+			--		game.print("can't change spawn yet! (try again after : " .. string.format("%.1f", (nextChangeTick-game.tick)/60) .. " seconds" )
+			--		return
+			--	end
+			--end
+
+			-- added changeTick and surface
+			local p_x = player.position.x;
+			local p_y = player.position.y;
+			global.spawns[player.name] = {player = player.name, x = math.random(p_x-4000,p_x+4000), y = math.random(p_y-4000,p_y+4000), lock = true, tries = 0, changeTick = game.tick, surface = player.surface}
+			game.print("Setting " .. player.name .. "'s spawn point to random location.")
+			if (player.character ~= nil) then
+				player.character.die()
+			end
+		end
 		if eventou.element.name == "spawn" then
             local player = game.players[eventou.player_index]
 
