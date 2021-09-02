dc3391f815c9f2812b92ea86255309f6f897ea5d
drd-avel <avelinadmitrieva@gmail.com>
2021-09-02 06:53:13 +0300

    ВШИТ имевшийся мод nuke-cliffs_19.0.0 в KaoExtended

--- mods/KaoExtended_9.16.12/prototypes/nuke_cliffs_1900.lua
+++ mods/KaoExtended_9.16.12/prototypes/nuke_cliffs_1900.lua
@@ -56,7 +56,8 @@ if (data.raw.projectile["atomic-artillery-wave"]) then
   })
 end
 
-
+--[[
 if settings.startup["nukes-require-cliff-explosives"].value and data.raw.technology["atomic-bomb"] and data.raw.technology["cliff-explosives"] then
   table.insert(data.raw.technology["atomic-bomb"].prerequisites,"cliff-explosives")
 end
+]]--
--- mods/nuke-cliffs_19.0.0/settings.lua
+++
@@ -1,8 +0,0 @@
-data:extend({
-    {
-        type = "bool-setting",
-        name = "nukes-require-cliff-explosives",
-        setting_type = "startup",
-        default_value = true,
-    },
-})
