76d087fc22c476ae8dcf7a434a4c134adf1af42f
drd-avel <avelinadmitrieva@gmail.com>
2021-09-04 00:46:39 +0300

    Обновление до GUI_Unifyer_1.10.0

---
+++ mods/GUI_Unifyer_1.10.0/README.md
@@ -0,0 +1,96 @@
+# GUI Unifyer
+Factorio Mod forcing mod buttons to look uniform and aligned, harmonizes most icons.
+(Continuation of Better_Modbuttons by Dr_Pepper)
+
+# Current advancement (before/after)
+![before](https://i.imgur.com/yudkRLa.png)
+![before](https://i.imgur.com/PRWSeGo.png)
+-----------------------------
+![after](https://i.imgur.com/D3vMpmR.png)
+![after](https://i.imgur.com/5OUYZgh.png)
+
+# Extra features
+- Many buttons which didn't have hover text now do.
+![tooltips](https://i.imgur.com/IXbqUlR.png)
+- Per-player setting to switch to grey square buttons.
+![greybuttons](https://i.imgur.com/kcSHyzX.png)
+- Blueprint related buttons only show when one is selected:
+![blueprint](https://i.imgur.com/jUbwLoX.png)
+
+# Supports (so far)
+- Factory Planner
+- Helmod
+- Module Inserter
+- Placeables
+- Todo List
+- What Is It Really Used For
+- Creative Mod
+- BeastFinder
+- Blueprint request
+- Bob's mods
+- CleanMap / DeleteEmptyChunks
+- Death Counter
+- Ingame Technology Browser
+- Outpost Planner
+- Quickbar Import/Export
+- Rocket Silo Stats
+- Schall Satellite Controller
+- Actual Craft Times Remade
+- Attila Zoom Mod
+- Auto Trash
+- Better Bots Technologies
+- Change Map Settings
+- Doing Things By Hand
+- Kill Lost Bots
+- Markers
+- Screenshot Toolkit
+- Factorissimo2
+- Kraskaska's Technology Total Raw Resources Calc
+- Crafting Tools
+- Orbital Ion Cannon (SE Edition)
+- Landfill Everything
+- Not Enough Todo
+- OSHA Hot Swap
+- Picker Inventory Tools
+- Powered Entities
+- Research Counter
+- Rich Text Helper
+- RitnTeleportation
+- Solar Calculator
+- Solar Ratio Calculator
+- Space Extension Mod (SpaceX)
+- The Fat Controller
+- Toggle Peaceful Mode
+- Train Log
+- Train Supply Manager (TSM)
+- Upgrade Builder and Planner
+- What's Missing?
+- Schall Railway Controller
+- Picks Rocket Stats
+- Black Market 2
+- Commu Guide Mod
+- Blueprint Flip And Turn
+- FJEI
+- 248k Mod
+- Blueprint Alignment
+- Cargo Train Manager
+- Clusterio
+- Cursed Upgradable Buildings
+- Default Wait Conditions
+- Diplomacy
+- Electronic Locomotives
+- Forces
+- Hive Mind
+- Hive Mind Remastered
+- How Far Has it Been?
+- Kuxynator's Blueprint Editor
+- Handcraft Ghosts
+- Logistic Machines
+- Logistic Request Manager (continued)
+- Region Cloner
+- Reset Evolution and Pollution
+- Schall Ore Conversion
+- Shuttle Train Continued
+- Simple Circuit Trains
+- Smart chest
+- TeamCoop
---
+++ mods/GUI_Unifyer_1.10.0/data.lua
@@ -0,0 +1,109 @@
+local ICONPATH = "__GUI_Unifyer__/graphics/icons/"
+
+local sprites = {"placeables_button", "todolist_button", "helmod_button", "factoryplanner_button", "moduleinserter_button", "wiiuf_button", "creativemod_button", "beastfinder_button",
+"blueprintrequest_button", "bobclasses_button", "bobinserters_button", "cleanmap_button", "deathcounter_button", "ingteb_button", "outpostplanner_button",
+"quickbarimportexport_button", "quickbarimport_button", "quickbarexport_button", "rocketsilostats_button", "schall_sc_button",
+
+"actr_button", "attachnotes_button", "attilazoommod_button", "avatars_button", "betterbotsfixed_button", "blackmarket1_button", "blackmarket2_button",
+"changemapsettings_button", "dana_button", "deleteadjacentchunk_button", "doingthingsbyhand_button",
+"facautoscreenshot_button", "factorissimo2_button", "factorissimo2_inspect_button", "killlostbots_button",
+"kttrrc_button", "kuxcraftingtools_button", "kuxorbitalioncannon_button", "landfilleverythingu_button",
+"markers_button", "modmashsplinterboom_button", "modmashsplinternewworlds_button", "notenoughtodo_button", "nullius_button",
+"oshahotswap_button", "picksrocketstats_button", "poweredentities_button", "researchcounter_button",
+"richtexthelper_button", "ritnteleportation_button", "schallendgameevolution_button", "solarcalc_button",
+"spacemod_button",
+"thefatcontroller_button", "trainlog_button", "trainpubsub_button", "upgradeplannernext_button", "whatsmissing_button", "schall_rc_button",
+"commuguidemod_guide_button", "commuguidemod_pupil_button", "blueprint_flip_horizontal_button", "blueprint_flip_vertical_button", "fjei_button",
+
+"248k_button", "blueprintalignment_button", "cargotrainmanager_button", "clusterio_button", "cursedexp_button", "defaultwaitconditions_button",
+"diplomacy_button", "electronic_locomotives_button", "forces_button", "hive_mind_button1", "hive_mind_button2", "howfardiditgo_button", "kuxblueprinteditor_button",
+"logisticmachines_button", "logisticrequestmanager_button", "regioncloner_button", "resetevolutionpollution_button",
+"schalloreconversion_button", "shuttle_train_continued_button", "simple_circuit_trains_button", "smartchest_button", "teamcoop_button1", "teamcoop_button2", "trainschedulesignals_button",
+}
+
+for _, i in pairs(sprites) do
+	local p = {}
+	p.type = "sprite"
+	p.name = i
+	p.filename = ICONPATH .. i .. ".png"
+	p.flags = { "gui-icon" }
+	p.width = 64
+	p.height = 64
+	p.scale = 0.5
+	p.priority = "extra-high-no-scale"
+	data:extend({p})
+end
+
+if data.raw["sprite"]["autotrash_trash"] and data.raw["sprite"]["autotrash_trash_paused"] and data.raw["sprite"]["autotrash_requests_paused"] and data.raw["sprite"]["autotrash_both_paused"] then
+	data.raw["sprite"]["autotrash_trash"].filename = ICONPATH .. "autotrash_button.png"
+	data.raw["sprite"]["autotrash_trash_paused"].layers[1].filename = ICONPATH .. "autotrash_button.png"
+	data.raw["sprite"]["autotrash_requests_paused"].layers[1].filename = ICONPATH .. "autotrash_button.png"
+	data.raw["sprite"]["autotrash_both_paused"].layers[1].filename = ICONPATH .. "autotrash_button.png"
+end
+
+if data.raw["sprite"]["tpm_button_sprite_peace"] and data.raw["sprite"]["tpm_button_sprite_war"] then
+	data.raw["sprite"]["tpm_button_sprite_peace"].filename = ICONPATH .. "tpm_button_sprite_peace.png"
+	data.raw["sprite"]["tpm_button_sprite_peace"].size = {64, 64}
+	data.raw["sprite"]["tpm_button_sprite_war"].filename = ICONPATH .. "tpm_button_sprite_war.png"
+	data.raw["sprite"]["tpm_button_sprite_war"].size = {64, 64}
+end
+
+local nothing = {0, 0, 0, 0}
+local white = {1, 1, 1, 0.9}
+local black = {0, 0, 0, 0.9}
+local slot_button_notext = {
+	type = "button_style",
+	parent = "slot_button",
+	default_font_color = nothing,
+	hovered_font_color = nothing,
+	clicked_font_color = nothing,
+	disabled_font_color = nothing,
+	selected_font_color = nothing,
+	selected_hovered_font_color = nothing,
+	selected_clicked_font_color = nothing,
+	strikethrough_color = nothing,
+}
+local slot_button_whitetext = {
+	type = "button_style",
+	parent = "slot_button",
+	default_font_color = white,
+	hovered_font_color = white,
+	clicked_font_color = white,
+	disabled_font_color = white,
+	selected_font_color = white,
+	selected_hovered_font_color = white,
+	selected_clicked_font_color = white,
+	strikethrough_color = white,
+}
+local slot_sized_button_notext = {
+	type = "button_style",
+	parent = "slot_sized_button",
+	default_font_color = nothing,
+	hovered_font_color = nothing,
+	clicked_font_color = nothing,
+	disabled_font_color = nothing,
+	selected_font_color = nothing,
+	selected_hovered_font_color = nothing,
+	selected_clicked_font_color = nothing,
+	strikethrough_color = nothing,
+}
+local slot_sized_button_blacktext = {
+	type = "button_style",
+	parent = "slot_sized_button",
+	default_font_color = black,
+	hovered_font_color = black,
+	clicked_font_color = black,
+	disabled_font_color = black,
+	selected_font_color = black,
+	selected_hovered_font_color = black,
+	selected_clicked_font_color = black,
+	strikethrough_color = black,
+}
+
+data.raw["gui-style"].default["slot_button_notext"] = slot_button_notext
+data.raw["gui-style"].default["slot_button_whitetext"] = slot_button_whitetext
+data.raw["gui-style"].default["slot_sized_button_notext"] = slot_sized_button_notext
+data.raw["gui-style"].default["slot_sized_button_blacktext"] = slot_sized_button_blacktext
+--data.raw["gui-style"].default["attach-notes-add-button"]
+--data.raw["gui-style"].default["attach-notes-edit-button"]
+--data.raw["gui-style"].default["attach-notes-view-button"]
