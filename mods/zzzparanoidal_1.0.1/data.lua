c7197f6ae475a804e7aad5f633d0d33aa9e7a3c5
drd-avel <avelinadmitrieva@gmail.com>
2021-09-19 21:04:49 +0300

    Переезд paranoidal-tweaks в zzzparanoidal

--- mods/zzzparanoidal_1.0.1/data.lua
+++ mods/zzzparanoidal_1.0.1/data.lua
@@ -5,3 +5,5 @@ require("prototypes.seed-extractor")
 angelsmods.trigger.smelting_products["cobalt"].plate=true
 
 require("prototypes.artillery-prototype.artillery-turret-prototype") --фикс убирающий био-арту и добовляющий новую на базе обычной
+
+require("prototypes.beltentities") --переехало из paranoidal-tweaks_0.18.34 (sbelyakov)
