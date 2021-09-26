d31c85f1944a08cea19a1272477ac68c8d02782d
drd-avel <avelinadmitrieva@gmail.com>
2021-09-26 18:13:06 +0300

    Попытка добавляения фосфорной кислоты в список сливаемых утилизируемых жидкостей.

--- mods/zzzparanoidal_1.0.1/data-updates.lua
+++ mods/zzzparanoidal_1.0.1/data-updates.lua
@@ -1,7 +1,9 @@
 require("prototypes.miniloaders")
 require("prototypes.generators")
+require("prototypes.fluid-void")
 
 if mods.bobequipment then
 	data.raw.item["personal-roboport-mk3-equipment"].subgroup = "misc1"
 	data.raw.item["personal-roboport-mk4-equipment"].subgroup = "misc1"
 end
+
---
+++ mods/zzzparanoidal_1.0.1/prototypes/fluid-void.lua
@@ -0,0 +1,4 @@
+-- добавление жидкостям возможность быть отправленными на слив
+-- пример из mods\angelsrefining_0.12.0\prototypes\refining-generate.lua
+
+angelsmods.functions.make_void("liquid-phosphoric-acid", "water")
