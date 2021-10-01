f475a8bb2018bb3cca929728c6888b0f02ef6868
drd-avel <avelinadmitrieva@gmail.com>
2021-10-01 21:55:48 +0300

    Фикс каменных труб (спс KiRiK)

---
+++ mods/zzzparanoidal_1.0.1/prototypes/stone-pipe-fix.lua
@@ -0,0 +1,5 @@
+data.raw["item"]["stone-pipe"].flags = {"hidden"}
+data.raw["recipe"]["angels-stone-pipe-casting"].hidden = true
+
+data.raw["item"]["stone-pipe-to-ground"].flags = {"hidden"}
+data.raw["recipe"]["angels-stone-pipe-to-ground-casting"].hidden = true
