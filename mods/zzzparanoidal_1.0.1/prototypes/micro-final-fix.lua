9ab3775c177de822a51f19ddae025d9120b8aaf2
drd-avel <avelinadmitrieva@gmail.com>
2021-10-05 09:40:34 +0300

    Множество ФИКСОВ от KiRiK'а

---
+++ mods/zzzparanoidal_1.0.1/prototypes/micro-final-fix.lua
@@ -0,0 +1,123 @@
+--data.raw["technology"]["xxx"].prerequisites = {"xxx", "xxx"}
+--bobmods.lib.recipe.add_ingredient("recipe", {"ingredient", 2})
+--bobmods.lib.tech.add_prerequisite ("technology", "prerequisite")
+
+-------------------------------------------------------------------------------------------------
+-- добавляем зависимости в техологии для последовательности развития
+bobmods.lib.tech.add_prerequisite ("electronics-machine-1", "steel-processing")
+bobmods.lib.tech.add_prerequisite ("flat-lamp-t", "electronics")
+bobmods.lib.tech.add_prerequisite ("radar", "electronics")
+bobmods.lib.tech.add_prerequisite ("electric-lab", "electronics")
+bobmods.lib.tech.add_prerequisite ("bi-tech-bio-farming", "ore-crushing")
+bobmods.lib.tech.add_prerequisite ("logistic-science-pack", "electronics")
+bobmods.lib.tech.add_prerequisite ("logistic-science-pack", "angels-bronze-smelting-1")
+bobmods.lib.tech.add_prerequisite ("bio-wood-processing", "bi-tech-bio-farming")
+-------------------------------------------------------------------------------------------------
+--целюлозное волокно в переработку древесины
+data.raw["recipe"]["cellulose-fiber-raw-wood"].normal.enabled = false
+data.raw["recipe"]["cellulose-fiber-raw-wood"].expensive.enabled = false
+bobmods.lib.tech.add_recipe_unlock("bio-wood-processing", "cellulose-fiber-raw-wood") 
+-------------------------------------------------------------------------------------------------
+--ремкомплект в электричество
+data.raw["recipe"]["repair-pack"].enabled = false
+bobmods.lib.tech.add_recipe_unlock("electricity", "repair-pack") 
+-------------------------------------------------------------------------------------------------
+--конденсатор в электричество
+data.raw["recipe"]["condensator"].enabled = false
+bobmods.lib.tech.add_recipe_unlock("electricity", "condensator") 
+-------------------------------------------------------------------------------------------------
+--паровой манипулятор в автоматику
+data.raw["recipe"]["steam-inserter"].enabled = false
+bobmods.lib.tech.add_recipe_unlock("basic-automation", "steam-inserter") 
+-------------------------------------------------------------------------------------------------
+--скрываем рецепты шариков. технология в prototypes/micro-fix.lua
+data.raw["recipe"]["alien-artifact-red-from-basic"].enabled = false
+data.raw["recipe"]["alien-artifact-orange-from-basic"].enabled = false
+data.raw["recipe"]["alien-artifact-yellow-from-basic"].enabled = false
+data.raw["recipe"]["alien-artifact-green-from-basic"].enabled = false
+data.raw["recipe"]["alien-artifact-blue-from-basic"].enabled = false
+data.raw["recipe"]["alien-artifact-purple-from-basic"].enabled = false
+
+data.raw["recipe"]["alien-artifact-from-small"].enabled = false
+data.raw["recipe"]["alien-artifact-red-from-small"].enabled = false
+data.raw["recipe"]["alien-artifact-orange-from-small"].enabled = false
+data.raw["recipe"]["alien-artifact-yellow-from-small"].enabled = false
+data.raw["recipe"]["alien-artifact-green-from-small"].enabled = false
+data.raw["recipe"]["alien-artifact-blue-from-small"].enabled = false
+data.raw["recipe"]["alien-artifact-purple-from-small"].enabled = false
+-------------------------------------------------------------------------------------------------
+--перемещаем рецепты и предметы больших шариков куда следует
+data.raw["recipe"]["alien-artifact-red-from-basic"].group = "bio-processing-alien"
+data.raw["recipe"]["alien-artifact-orange-from-basic"].group = "bio-processing-alien"
+data.raw["recipe"]["alien-artifact-yellow-from-basic"].group = "bio-processing-alien"
+data.raw["recipe"]["alien-artifact-green-from-basic"].group = "bio-processing-alien"
+data.raw["recipe"]["alien-artifact-blue-from-basic"].group = "bio-processing-alien"
+data.raw["recipe"]["alien-artifact-purple-from-basic"].group = "bio-processing-alien"
+
+data.raw["recipe"]["alien-artifact-red-from-basic"].subgroup = "bio-processing-alien-large"
+data.raw["recipe"]["alien-artifact-orange-from-basic"].subgroup = "bio-processing-alien-large"
+data.raw["recipe"]["alien-artifact-yellow-from-basic"].subgroup = "bio-processing-alien-large"
+data.raw["recipe"]["alien-artifact-green-from-basic"].subgroup = "bio-processing-alien-large"
+data.raw["recipe"]["alien-artifact-blue-from-basic"].subgroup = "bio-processing-alien-large"
+data.raw["recipe"]["alien-artifact-purple-from-basic"].subgroup = "bio-processing-alien-large"
+
+data.raw["item"]["alien-artifact"].group = "bio-processing-alien"
+data.raw["item"]["alien-artifact-red"].group = "bio-processing-alien"
+data.raw["item"]["alien-artifact-orange"].group = "bio-processing-alien"
+data.raw["item"]["alien-artifact-yellow"].group = "bio-processing-alien"
+data.raw["item"]["alien-artifact-green"].group = "bio-processing-alien"
+data.raw["item"]["alien-artifact-blue"].group = "bio-processing-alien"
+data.raw["item"]["alien-artifact-purple"].group = "bio-processing-alien"
+
+data.raw["item"]["alien-artifact"].subgroup = "bio-processing-alien-large"
+data.raw["item"]["alien-artifact-red"].subgroup = "bio-processing-alien-large"
+data.raw["item"]["alien-artifact-orange"].subgroup = "bio-processing-alien-large"
+data.raw["item"]["alien-artifact-yellow"].subgroup = "bio-processing-alien-large"
+data.raw["item"]["alien-artifact-green"].subgroup = "bio-processing-alien-large"
+data.raw["item"]["alien-artifact-blue"].subgroup = "bio-processing-alien-large"
+data.raw["item"]["alien-artifact-purple"].subgroup = "bio-processing-alien-large"
+-------------------------------------------------------------------------------------------------
+-- замена иконки на технологии припоя
+data.raw["technology"]["angels-solder-smelting-basic"].icon = "__reskins-library__/graphics/icons/shared/items/solder.png"
+data.raw["technology"]["angels-solder-smelting-basic"].icon_size = 64
+data.raw["technology"]["angels-solder-smelting-basic"].icon_mipmaps = 4
+-------------------------------------------------------------------------------------------------
+-- подкрутка чтобы сборщик1 мог собирать сам себя
+data.raw["assembling-machine"]["assembling-machine-1"].ingredient_count = 5
+
+-------------------------------------------------------------------------------------------------
+-- создаем технологию для переработки шариков
+data:extend({
+    {
+    type = "technology",
+    name = "alien-artifact",
+    icon = "__reskins-bobs__/graphics/icons/enemies/artifacts/alien-artifact.png",
+    icon_size = 64, icon_mipmaps = 4,
+    effects =
+    {
+        {type = "unlock-recipe", recipe = "alien-artifact-red-from-basic"},
+        {type = "unlock-recipe", recipe = "alien-artifact-orange-from-basic"},
+        {type = "unlock-recipe", recipe = "alien-artifact-yellow-from-basic"},
+        {type = "unlock-recipe", recipe = "alien-artifact-green-from-basic"},
+        {type = "unlock-recipe", recipe = "alien-artifact-blue-from-basic"},
+        {type = "unlock-recipe", recipe = "alien-artifact-purple-from-basic"},
+
+        {type = "unlock-recipe", recipe = "alien-artifact-from-small"},
+        {type = "unlock-recipe", recipe = "alien-artifact-red-from-small"},
+        {type = "unlock-recipe", recipe = "alien-artifact-orange-from-small"},
+        {type = "unlock-recipe", recipe = "alien-artifact-yellow-from-small"},
+        {type = "unlock-recipe", recipe = "alien-artifact-green-from-small"},
+        {type = "unlock-recipe", recipe = "alien-artifact-blue-from-small"},
+        {type = "unlock-recipe", recipe = "alien-artifact-purple-from-small"},
+    },
+    prerequisites = { "gardens" },
+    unit =
+    {
+        count = 20,
+        ingredients = {{ "automation-science-pack", 1 }},
+        time = 30
+    },
+    order = "c-a" 
+    }
+})
+-------------------------------------------------------------------------------------------------
