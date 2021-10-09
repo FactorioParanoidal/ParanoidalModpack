f60c954845a92beb78cdc58f1f1900c6bbda425c
drd-avel <avelinadmitrieva@gmail.com>
2021-10-09 22:14:37 +0300

    ВШИТ мод Angels_RBOS (Re-enabled Basic Ore Smelting) - плавка руды для железа, меди, олова, свинца.

---
+++ mods/zzzparanoidal_1.0.1/prototypes/Angels_RBOS.lua
@@ -0,0 +1,25 @@
+data.raw["recipe"]["iron-plate"]["normal"]["enabled"] = true
+data.raw["recipe"]["iron-plate"]["expensive"]["enabled"] = true
+data.raw["recipe"]["iron-plate"]["normal"]["hidden"] = false
+data.raw["recipe"]["iron-plate"]["expensive"]["hidden"] = false
+
+--copper--
+data.raw["recipe"]["copper-plate"]["normal"]["enabled"] = true
+data.raw["recipe"]["copper-plate"]["expensive"]["enabled"] = true
+data.raw["recipe"]["copper-plate"]["normal"]["hidden"] = false
+data.raw["recipe"]["copper-plate"]["expensive"]["hidden"] = false
+
+  if mods["bobplates"] then
+--tin--
+data.raw["recipe"]["tin-plate"]["normal"]["enabled"] = true
+data.raw["recipe"]["tin-plate"]["expensive"]["enabled"] = true
+data.raw["recipe"]["tin-plate"]["normal"]["hidden"] = false
+data.raw["recipe"]["tin-plate"]["expensive"]["hidden"] = false
+
+--lead--
+data.raw["recipe"]["lead-plate"]["normal"]["enabled"] = true
+data.raw["recipe"]["lead-plate"]["expensive"]["enabled"] = true
+data.raw["recipe"]["lead-plate"]["normal"]["hidden"] = false
+data.raw["recipe"]["lead-plate"]["expensive"]["hidden"] = false
+
+end
