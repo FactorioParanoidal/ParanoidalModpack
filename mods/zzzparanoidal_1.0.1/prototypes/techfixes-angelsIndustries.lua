a89b60d5ad75359abdccf649900692c5b98ac497
sovigod <CrashDGC@gmail.com>
2021-10-11 22:29:36 +0200

    upgrade nuclear-art

--- mods/zzzparanoidal_1.0.1/prototypes/techfixes-angelsIndustries.lua
+++ mods/zzzparanoidal_1.0.1/prototypes/techfixes-angelsIndustries.lua
@@ -22,3 +22,15 @@ angelsmods.functions.OV.disable_recipe("condensator")
 angelsmods.functions.OV.execute()
 bobmods.lib.recipe.replace_ingredient("fast-belt", "angels-gear", "steel-bearing")
 bobmods.lib.recipe.replace_ingredient("fast-splitter", "angels-gear", "steel-bearing")
+
+data.raw.projectile["atomic-bomb-wave"].action[1].action_delivery.target_effects.damage.amount=5000
+data.raw.projectile["atomic-bomb-wave"].action[1].action_delivery.target_effects.upper_distance_threshold=100
+data.raw["artillery-projectile"]["artillery-projectile-nuclear"].action.action_delivery.target_effects[5].action.radius = 66
+data.raw["artillery-projectile"]["artillery-projectile-thermonuclear"].action.action_delivery.target_effects[7].damage.amount = 666666
+data.raw["artillery-projectile"]["artillery-projectile-thermonuclear"].action.action_delivery.target_effects[10].radius = 240
+data.raw["artillery-projectile"]["artillery-projectile-thermonuclear"].action.action_delivery.target_effects[13].action.radius = 78
+data.raw["artillery-projectile"]["artillery-projectile-thermonuclear"].action.action_delivery.target_effects[13].action.repeat_count = 4000
+data.raw["artillery-projectile"]["artillery-projectile-thermonuclear"].action.action_delivery.target_effects[14].action.radius = 103
+data.raw["artillery-projectile"]["artillery-projectile-thermonuclear"].action.action_delivery.target_effects[14].action.repeat_count = 4000
+data.raw["artillery-projectile"]["artillery-projectile-thermonuclear"].action.action_delivery.target_effects[15].action.radius = 132
+data.raw["artillery-projectile"]["artillery-projectile-thermonuclear"].action.action_delivery.target_effects[15].action.repeat_count = 4000
