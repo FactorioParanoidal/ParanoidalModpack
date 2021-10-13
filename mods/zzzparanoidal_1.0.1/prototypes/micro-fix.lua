24a49591e7646c9bc6079408bc3ccc15993cf8ce
_KiRiK_ <kirill.katusev@gmail.com>
2021-10-14 00:08:12 +0300

    улучшение рельс, станций и светофоров планнером вырезание био-рельс и миграция рецепты для улучшения примитивов перестановки в технологиях жд

--- mods/zzzparanoidal_1.0.1/prototypes/micro-fix.lua
+++ mods/zzzparanoidal_1.0.1/prototypes/micro-fix.lua
@@ -66,4 +66,141 @@ data:extend({
     },
 },
 -------------------------------------------------------------------------------------------------
+--добавляем рецепт для модернизации рельс
+{
+  type = "recipe",
+  name = "scrap-rail-to-rail",
+  icons = {
+    {
+      icon = "__base__/graphics/icons/rail.png",
+      icon_size = 64,
+      icon_mipmaps = 4
+    },
+    {
+      icon = "__zzzparanoidal__/graphics/upgrade-icon.png",
+      icon_size = 16,
+      icon_mipmaps = 1,
+      scale = 0.9,
+      shift = {-10, -10}
+    }
+  },
+  category = "crafting",
+  group = "logistics",
+  subgroup = "transport",
+  order = "aa",
+  energy_required = 0.5,
+  enabled = false,
+  allow_decomposition = false,
+  always_show_products = true,
+  ingredients = 
+    {
+      {type = "item", name = "scrap-rail", amount = 2},
+      {type = "item", name = "concrete", amount = 6}
+    },
+  results = {{type = "item", name = "rail", amount = 2}},
+},
+-------------------------------------------------------------------------------------------------
+--добавляем рецепт для модернизации светофора
+{
+  type = "recipe",
+  name = "rail-signal-scrap-to-rail-signal",
+  icons = {
+    {
+      icon = "__base__/graphics/icons/rail-signal.png",
+      icon_size = 64,
+      icon_mipmaps = 4
+    },
+    {
+      icon = "__zzzparanoidal__/graphics/upgrade-icon.png",
+      icon_size = 16,
+      icon_mipmaps = 1,
+      scale = 0.9,
+      shift = {-10, -10}
+    }
+  },
+  category = "crafting",
+  group = "logistics",
+  subgroup = "transport",
+  order = "ab",
+  energy_required = 0.5,
+  enabled = false,
+  allow_decomposition = false,
+  always_show_products = true,
+  ingredients = 
+    {
+      {type = "item", name = "rail-signal-scrap", amount = 1},
+      {type = "item", name = "electronic-circuit", amount = 1}
+    },
+  results = {{type = "item", name = "rail-signal", amount = 1}},
+},
+-------------------------------------------------------------------------------------------------
+--добавляем рецепт для модернизации проходного светофора
+{
+  type = "recipe",
+  name = "rail-chain-signal-scrap-to-rail-chain-signal",
+  icons = {
+    {
+      icon = "__base__/graphics/icons/rail-chain-signal.png",
+      icon_size = 64,
+      icon_mipmaps = 4
+    },
+    {
+      icon = "__zzzparanoidal__/graphics/upgrade-icon.png",
+      icon_size = 16,
+      icon_mipmaps = 1,
+      scale = 0.9,
+      shift = {-10, -10}
+    }
+  },
+  category = "crafting",
+  group = "logistics",
+  subgroup = "transport",
+  order = "ac",
+  energy_required = 0.5,
+  enabled = false,
+  allow_decomposition = false,
+  always_show_products = true,
+  ingredients = 
+    {
+      {type = "item", name = "rail-chain-signal-scrap", amount = 1},
+      {type = "item", name = "electronic-circuit", amount = 1}
+    },
+  results = {{type = "item", name = "rail-chain-signal", amount = 1}},
+},
+-------------------------------------------------------------------------------------------------
+--добавляем рецепт для модернизации станции
+{
+  type = "recipe",
+  name = "train-stop-scrap-to-train-stop",
+  icons = {
+    {
+      icon = "__base__/graphics/icons/train-stop.png",
+      icon_size = 64,
+      icon_mipmaps = 4
+    },
+    {
+      icon = "__zzzparanoidal__/graphics/upgrade-icon.png",
+      icon_size = 16,
+      icon_mipmaps = 1,
+      scale = 0.9,
+      shift = {-10, -10}
+    }
+  },
+  category = "crafting",
+  group = "logistics",
+  subgroup = "transport",
+  order = "ad",
+  energy_required = 0.5,
+  enabled = false,
+  allow_decomposition = false,
+  always_show_products = true,
+  ingredients = 
+    {
+      {type = "item", name = "train-stop-scrap", amount = 1},
+      {type = "item", name = "electronic-circuit", amount = 5},
+      {type = "item", name = "steel-plate", amount = 10}
+    },
+  results = {{type = "item", name = "train-stop", amount = 1}},
+},
+
 })
