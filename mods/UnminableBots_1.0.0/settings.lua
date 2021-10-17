220d5f6146a4fdaddb6bd97a8bd9663e206ada29
drd-avel <avelinadmitrieva@gmail.com>
2021-10-17 12:34:59 +0300

    ЗАМЕНА мода на UnminableBots_1.0.0

---
+++ mods/UnminableBots_1.0.0/settings.lua
@@ -0,0 +1,24 @@
+data:extend({
+  {
+    type = "bool-setting",
+    name = "Unminable-bots-construction",
+    order = "aa",
+    setting_type = "startup",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "Unminable-bots-logistic",
+    order = "ab",
+    setting_type = "startup",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "Unminable-bots-click-through",
+    order = "ac",
+    setting_type = "startup",
+    default_value = false,
+  },
+
+})
