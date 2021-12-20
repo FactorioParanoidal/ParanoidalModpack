e8b47a8483f0d3657b9291a1db87de686871458f
drd-avel <avelinadmitrieva@gmail.com>
2021-12-20 17:24:15 +0300

    Обновление до LtnManager_0.4.8

---
+++ mods/LtnManager_0.4.8/scripts/player-data.lua
@@ -0,0 +1,44 @@
+local dictionary = require("__flib__.dictionary")
+
+local player_data = {}
+
+function player_data.init(player, index)
+  local player_table = {
+    dictionaries = nil,
+    flags = {
+      can_open_gui = false,
+      translate_on_join = false,
+      translations_finished = false,
+    },
+    guis = {},
+    language = nil,
+    last_update = game.tick,
+  }
+  player.set_shortcut_available("ltnm-toggle-gui", false)
+  global.players[index] = player_table
+end
+
+function player_data.refresh(player, player_table)
+  local Gui = player_table.guis.main
+  if Gui then
+    Gui:destroy()
+  end
+
+  player_table.flags.can_open_gui = false
+  player_table.flags.translations_finished = false
+
+  player.set_shortcut_toggled("ltnm-toggle-gui", false)
+  player.set_shortcut_available("ltnm-toggle-gui", false)
+
+  player_table.dictionaries = nil
+  if player.connected then
+    dictionary.translate(player)
+  end
+end
+
+function player_data.set_setting(player_index, setting_name, setting_value)
+  local player = game.get_player(player_index)
+  player.mod_settings["ltnm-" .. setting_name] = { value = setting_value }
+end
+
+return player_data
---
+++ mods/LtnManager_0.4.8/settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+  -- global
+  {
+    type = "int-setting",
+    name = "ltnm-iterations-per-tick",
+    setting_type = "runtime-global",
+    minimum_value = 1,
+    default_value = 10,
+    order = "a",
+  },
+})
