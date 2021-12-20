2239a42cb78db18120ae8b9983d25c22aad47382
drd-avel <avelinadmitrieva@gmail.com>
2021-12-20 17:14:45 +0300

    Обновление до angelsrefining_0.12.1

--- mods/angelsrefining_0.12.0/prototypes/override/refining-override-bobtech.lua
+++
@@ -1,9 +0,0 @@
-local OV = angelsmods.functions.OV
-
--------------------------------------------------------------------------------
--- STEAM BURNER PHASE ---------------------------------------------------------
--------------------------------------------------------------------------------
-if mods["bobtech"] and settings.startup["bobmods-burnerphase"].value then
-  OV.add_prereq("ore-crushing", "automation-science-pack")
-  OV.add_prereq("ore-crushing", "electricity")
-end
