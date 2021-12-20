9e22e45601922f94b2f51ef577395863c1470782
drd-avel <avelinadmitrieva@gmail.com>
2021-12-20 12:15:23 +0300

    Обновление до angelspetrochem_0.9.21

---
+++ mods/angelspetrochem_0.9.21/prototypes/override/angelssmelting.lua
@@ -0,0 +1,39 @@
+local OV = angelsmods.functions.OV
+local move_item = angelsmods.functions.move_item
+
+if angelsmods.smelting then
+  if angelsmods.trigger.smelting_products["aluminium"].plate then
+    OV.add_prereq("angels-advanced-chemistry-2", "angels-aluminium-smelting-1")
+  end
+  if angelsmods.trigger.smelting_products["titanium"].plate then
+    OV.add_prereq("angels-advanced-chemistry-3", "angels-titanium-smelting-1")
+  end
+  if angelsmods.trigger.smelting_products["tungsten"].plate then
+    OV.add_prereq("angels-advanced-chemistry-4", "angels-tungsten-smelting-1")
+  end
+  if angelsmods.trigger.smelting_products["silver"].ingot or
+     angelsmods.trigger.smelting_products["silver"].wire or
+     angelsmods.trigger.smelting_products["silver"].plate or
+     angelsmods.trigger.smelting_products["silver"].powder or
+     angelsmods.trigger.smelting_products["gold"].ingot or
+     angelsmods.trigger.smelting_products["gold"].wire or
+     angelsmods.trigger.smelting_products["gold"].plate or
+     angelsmods.trigger.smelting_products["gold"].powder then
+  else
+    OV.disable_recipe(
+      {
+        "solid-sodium-cyanide",
+      }
+    )
+    angelsmods.functions.add_flag("solid-sodium-cyanide", "hidden")
+  end
+else
+  OV.disable_recipe(
+    {
+      "solid-sodium-cyanide",
+      "solid-sodium-carbonate",
+      "solid-sodium-sulfate-separation"
+    }
+  )
+  angelsmods.functions.add_flag("solid-sodium-cyanide", "hidden")
+end
