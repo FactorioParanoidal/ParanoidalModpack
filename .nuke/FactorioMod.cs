9e5130e818d17dde3f3428b414132114ead79454
SKProCH <shadowkillerpropro@yandex.ru>
2022-03-27 09:07:13 +0000

    Add ParanoidalTools - build scripts for basically checking modpack launch-ability

---
+++ .nuke/FactorioMod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Nuke.Common;
+using Nuke.Common.Utilities;
+
+public class FactorioMod {
+    private string _modPath;
+    public FactorioMod(string path) {
+        Assert.DirectoryExists(path, "Required mod directory does not exists");
+        _modPath = path;
+    }
+
+    private JObject GetModInfo() {
+        var infoPath = Path.Combine(_modPath, "info.json");
+        var infoText = File.ReadAllText(infoPath);
+        return JObject.Parse(infoText);
+    }
+
+    public IEnumerable<string> GetDependencies() {
+        var modInfo = GetModInfo();
+        return modInfo.Property("dependencies").First().ToObject<List<string>>();
+    }
+
+    public Version GetDependsOnFactorioVersion() {
+        // BUG Currently it can works only with dependency in format "base >= 1.1.1"
+        var versionString = GetDependencies().FirstOrDefault(s => s.StartsWith("base"))?[8..]
+                         ?? GetModInfo().GetPropertyStringValue("factorio_version");
+        return Version.Parse(versionString);
+    }
+}
---
+++ .nuke/Utils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using Nuke.Common.Tools.NSwag;
+public static class Utils {
+    public static readonly HttpClient HttpClient = new();
+    public static string GetFactorioDownloadLinkForCurrentOs(string build = "latest", string version = "alpha", string? os = null) {
+        return $"https://www.factorio.com/get-download/{build}/{version}/{os ?? GetFactorioDistroNameForCurrentOs()}";
+    }
+
+    public static string GetFactorioDistroNameForCurrentOs() {
+        return true switch {
+            _ when OperatingSystem.IsWindows() => "win64-manual",
+            _ when OperatingSystem.IsMacOS() => "osx",
+            _ when OperatingSystem.IsLinux() => "linux64",
+        };
+    }
+
+    public static void Chmod(string path) {
+        if (!OperatingSystem.IsLinux()) return;
+        using var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "/bin/bash",
+                Arguments = $"-c \"chmod +x {path}\""
+            }
+        };
+
+        process.Start();
+        process.WaitForExit();
+    }
+}
