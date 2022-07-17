ffa0005c078e8f8ee6c665b96d601b445fc40867
drd-avel <avelinadmitrieva@gmail.com>
2022-07-17 16:38:47 +0300

    Обновление до angelspetrochem_0.9.22

---
+++ mods/angelspetrochem_0.9.22/settings-updates.lua
@@ -0,0 +1,6 @@
+if mods["bobplates"] then
+  angelsmods.settings.hide_setting("bool-setting", "angels-disable-vanilla-chemical-plants", true)
+else
+  angelsmods.settings.hide_setting("bool-setting", "angels-disable-bobs-electrolysers", true)
+  angelsmods.settings.hide_setting("bool-setting", "angels-disable-bobs-chemical-plants", true)
+end
