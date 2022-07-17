33f0912bda05a023f58c021dbdece16971a5a019
drd-avel <avelinadmitrieva@gmail.com>
2022-07-17 21:17:08 +0300

    Вшит НОВЫЙ мод - Uniform recipe 1.0.0

--- mods/zzzparanoidal_1.0.1/data-updates.lua
+++ mods/zzzparanoidal_1.0.1/data-updates.lua
@@ -9,3 +9,9 @@ if mods.bobequipment then
 	data.raw.item["personal-roboport-mk4-equipment"].subgroup = "misc1"
 end
 
+-- Uniform recipe mod
+for _,r in pairs(data.raw["recipe"]) do
+		r.always_show_products=true;
+    r.show_amount_in_title=false;
+end
+-- Uniform recipe end
