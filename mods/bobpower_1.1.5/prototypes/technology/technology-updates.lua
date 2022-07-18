5c5a7b140a25d602475b85501b6be826e7a27ce2
drd-avel <avelinadmitrieva@gmail.com>
2022-07-18 13:57:42 +0300

    Обновление до bobpower_1.1.5

---
+++ mods/bobpower_1.1.5/prototypes/technology/technology-updates.lua
@@ -0,0 +1,30 @@
+if settings.startup["bobmods-power-accumulators"].value == true then
+
+  if data.raw.item["lithium-ion-battery"] and data.raw.technology["battery-2"] then
+    bobmods.lib.tech.add_prerequisite ("bob-electric-energy-accumulators-3", "battery-2")
+  end
+
+  if data.raw.item["silver-zinc-battery"] and data.raw.technology["battery-3"] then
+    bobmods.lib.tech.add_prerequisite ("bob-electric-energy-accumulators-4", "battery-3")
+  end
+
+end
+
+
+if settings.startup["bobmods-power-poles"].value == true then
+
+  if data.raw.tool["advanced-logistic-science-pack"] then
+    bobmods.lib.tech.add_science_pack("electric-pole-2", "chemical-science-pack", 1)
+    bobmods.lib.tech.add_prerequisite("electric-pole-2", "chemical-science-pack", 1)
+    bobmods.lib.tech.add_science_pack("electric-pole-3", "advanced-logistic-science-pack", 1)
+    bobmods.lib.tech.add_science_pack("electric-pole-4", "advanced-logistic-science-pack", 1)
+    bobmods.lib.tech.add_prerequisite("electric-pole-3", "advanced-logistic-science-pack")
+    bobmods.lib.tech.add_science_pack("electric-substation-3", "advanced-logistic-science-pack", 1)
+    bobmods.lib.tech.add_science_pack("electric-substation-4", "advanced-logistic-science-pack", 1)
+    bobmods.lib.tech.add_prerequisite("electric-substation-3", "advanced-logistic-science-pack")
+  end
+
+  bobmods.lib.tech.set_science_pack_count("electric-energy-distribution-1", 100)
+  bobmods.lib.tech.set_science_pack_count("electric-energy-distribution-2", 250)
+end
+
