973e42d0f069c9126874020ca3a96ae904569f7d
drd-avel <avelinadmitrieva@gmail.com>
2022-07-21 12:39:00 +0300

    Обновление до Rocket-Silo-Construction_1.3.0

--- mods/Rocket-Silo-Construction_1.3.0/utils.lua
+++ mods/Rocket-Silo-Construction_1.3.0/utils.lua
@@ -100,4 +100,57 @@ function is_in_table (tab, val)
     return false
 end
 
+function update_science_pack_amount(technology, amount)
+  if data.raw.technology[technology] then
+	local new ={}	
+	for i, ingredient in pairs(data.raw.technology[technology].unit.ingredients) do
+		table.insert (new,{ingredient[1], ingredient[2] + amount})
+    end
+	data.raw.technology[technology].unit.ingredients = new
+	end
+end
 
+
+function change_science_pack_amount(technology, pack, amount)
+  if data.raw.technology[technology] then
+	for i, ingredient in pairs(data.raw.technology[technology].unit.ingredients) do
+		if ingredient[1]==pack then ingredient[2]=amount end
+		end
+	end
+end
+
+
+function add_new_science_pack(technology, pack, amount)
+  if data.raw.technology[technology] and data.raw.tool[pack] then
+    local addit = true
+    for i, ingredient in pairs(data.raw.technology[technology].unit.ingredients) do
+      if ingredient[1] == pack or ingredient.name == pack then addit = false end
+    end
+    if addit then table.insert(data.raw.technology[technology].unit.ingredients,{pack, amount}) end
+  else
+    if not data.raw.technology[technology] then
+      log("Technology " .. technology .. " does not exist.")
+    end
+    if not data.raw.tool[pack] then
+      log("Science pack " .. new .. " does not exist.")
+    end
+  end
+end
+
+function remove_science_pack(technology, pack)
+  if data.raw.technology[technology] and data.raw.tool[pack] then
+	local ingredients = table.deepcopy(data.raw.technology[technology].unit.ingredients)
+    for i=#ingredients,1,-1 do
+		local ingredient = ingredients[i]
+        if ingredient[1] == pack or ingredient.name == pack then table.remove(data.raw.technology[technology].unit.ingredients,i) end
+    end
+	end
+end
+
+function remove_raw_ingredient(raw, name)
+  if raw then
+    for i=#raw,1,-1 do 
+      if raw[i][1] == name or raw[i].name == name then table.remove(raw,i) end
+    end
+	end
+end
