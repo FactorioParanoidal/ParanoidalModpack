7291d7846239663944fa0f74fc7ed9f9b07f7f71
drd-avel <avelinadmitrieva@gmail.com>
2022-07-30 23:38:20 +0300

    Обновление до AdditionalPasteSettings_1.5.1

---
+++ mods/AdditionalPasteSettings_1.5.1/lib.lua
@@ -0,0 +1,69 @@
+local lib = {} ---@class HelperLibrary
+
+---@param str string
+---@param args table
+---@return string
+lib.parse_string = function(str, args)
+    local count = 1
+    for _, arg in pairs(args) do
+        local var = '__' .. count .. '__'
+        local s, e = string.find(str, var)
+        while s ~= nil and e ~= nil do
+            str = string.gsub(str, var, arg)
+            s, e = string.find(str, var)
+        end
+        count = count + 1
+    end
+    return str
+end
+
+---Find a name in flib's locales dictonary
+---@param item_name string
+---@param item_type string
+---@return string|nil
+lib.find_name_in_flib_dictonary = function(item_name, item_type)
+    for _, _dict in pairs(global.player_dictionaries) do
+        if _dict[item_type] and _dict[item_type][item_name] then
+            return _dict[item_type][item_name]
+        end
+    end
+    return nil
+end
+
+---Parse signal data to nice text format
+---comment
+---@param signal_data table
+---@return string|nil
+lib.parse_signal_to_rich_text = function(signal_data)
+    if signal_data ~= nil then
+        local text_type = signal_data.type or "item"
+        if text_type == "virtual" then
+            text_type = "virtual-signal"
+        end
+
+        return string.format("[img=%s/%s]", text_type, signal_data.name)
+    end
+    return nil
+end
+
+---Get list of keys from a table
+---@param t table @table to get keys from
+---@return table keys @sorts list of keys from table
+lib.get_keys = function(t)
+    local keys = {}
+    for key, _ in pairs(t) do
+        table.insert(keys, key)
+    end
+    table.sort(keys)
+    return keys
+end
+
+---@class Colors
+lib.colors = {
+    white = { r = 1.0, g = 1.0, b = 1.0 }, ---@type Color
+}
+
+
+
+
+return lib
