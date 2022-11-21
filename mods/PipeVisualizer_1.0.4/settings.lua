a2830cfbe4a21e77790c2493ceed38ff60b93778
drd-avel <avelinadmitrieva@gmail.com>
2022-11-22 01:14:27 +0300

    НОВЫЙ МОД PipeVisualizer_1.0.4

---
+++ mods/PipeVisualizer_1.0.4/settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+  {
+    type = "double-setting",
+    name = "pv-overlay-opacity",
+    setting_type = "runtime-per-user",
+    default_value = 0.6,
+    minimum_value = 0,
+    maximum_value = 1,
+  },
+})
