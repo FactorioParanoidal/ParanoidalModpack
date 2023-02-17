51edf3876704fa1d2c8fe002bca34a2908bdfd6c
drd_avel <avelinadmitrieva@gmail.com>
2023-02-17 14:53:18 +0300

    ФИКС невозможности создания первых слитков никеля и цинка - добавлены рецепты электролиза.

--- mods/KaoExtended_9.16.12/data.lua
+++ mods/KaoExtended_9.16.12/data.lua
@@ -2,3 +2,5 @@ require("library.technology")
 require("library.recipe")
 
 require("prototypes.recipes.UsedCoolant")
+
+require("prototypes.Bobs-alloy-recipe") --Re-enabled Alloy Smelting as bob's mods
