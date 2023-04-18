df656e02c3299fa78675d823a741c45eaea8a899
SKProCH <skproch@yandex.ru>
2023-04-18 15:59:19 +0300

    Добавляет nuke PullLocalization, что бы обновлять мод ParanoidalLocale. Интегрирует это в CI скрипт

--- .nuke/Utils.cs
+++ .nuke/Utils.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Net.Http;
-using System.Runtime.InteropServices;
-using Nuke.Common.Tools.NSwag;
 public static class Utils {
+    public const string LocalizationModFolderPullAddress = "https://gitlab.com/paranoidal/locale/-/archive/main/locale-main.zip?path=ParanoidalLocale";
     public static readonly HttpClient HttpClient = new();
     public static string GetFactorioDownloadLinkForCurrentOs(string build = "latest", string version = "alpha", string? os = null) {
         return $"https://www.factorio.com/get-download/{build}/{version}/{os ?? GetFactorioDistroNameForCurrentOs()}";
@@ -12,17 +11,15 @@ public static class Utils {
     public static string GetFactorioDistroNameForCurrentOs() {
         return true switch {
             _ when OperatingSystem.IsWindows() => "win64-manual",
-            _ when OperatingSystem.IsMacOS() => "osx",
-            _ when OperatingSystem.IsLinux() => "linux64",
+            _ when OperatingSystem.IsMacOS()   => "osx",
+            _ when OperatingSystem.IsLinux()   => "linux64",
         };
     }
 
     public static void Chmod(string path) {
         if (!OperatingSystem.IsLinux()) return;
-        using var process = new Process
-        {
-            StartInfo = new ProcessStartInfo
-            {
+        using var process = new Process {
+            StartInfo = new ProcessStartInfo {
                 RedirectStandardOutput = true,
                 UseShellExecute = false,
                 CreateNoWindow = true,
