ec00fa16a39362f5c284d501de14480c2a4c4672
Kiska Ra <kiskara@gmail.com>
2023-10-17 02:25:41 +0100

    Updated to beautiful_straight_bridge_railway_1.1.3

--- mods/beautiful_straight_bridge_railway_1.1.3/data.lua
+++ mods/beautiful_straight_bridge_railway_1.1.3/data.lua
@@ -1,1 +1,11 @@
 require("prototypes.item")
+
+data:extend({
+	{
+    type = "custom-input",
+    name = "kap-bbr-cycle-key",
+    key_sequence = "CONTROL + SHIFT + R",
+	consuming = "game-only",
+  }
+ })
+
