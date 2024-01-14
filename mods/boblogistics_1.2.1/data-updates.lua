8fc0a36142d350975311647175401557e3aee4fb
drd_avel <avelinadmitrieva@gmail.com>
2024-01-14 22:12:14 +0300

    Обновление до boblogistics_1.2.1

---
+++ mods/boblogistics_1.2.1/data-updates.lua
@@ -0,0 +1,177 @@
+require("prototypes.recipe.roboport-recipe-updates")
+require("prototypes.recipe.robots-recipe-updates")
+require("prototypes.recipe.inserter-recipe-updates")
+require("prototypes.recipe.belt-recipe-updates")
+require("prototypes.recipe.storage-tank-recipe-updates")
+require("prototypes.recipe.train-recipe-updates")
+require("prototypes.recipe.pump-recipe-updates")
+require("prototypes.recipe.logistic-container-recipe-updates")
+
+require("prototypes.technology-updates")
+require("prototypes.productivity-limitations")
+
+require("prototypes.repair-pack-updates")
+
+require("prototypes.entity.machine-output-height")
+
+-- Allow burner inserters to fuel themselves from the burner inventory of a machine they're pulling items out of.
+data.raw.inserter["burner-inserter"].allow_burner_leech = true
+
+for i, pipe in pairs(data.raw["pipe-to-ground"]) do
+  table.insert(pipe.flags, "fast-replaceable-no-build-while-moving")
+end
+for i, pipe in pairs(data.raw["pipe"]) do
+  table.insert(pipe.flags, "fast-replaceable-no-build-while-moving")
+end
+
+if settings.startup["bobmods-logistics-trains"].value == true then
+  if data.raw["damage-type"]["bob-pierce"] then
+    table.insert(data.raw.locomotive["locomotive"].resistances, { type = "bob-pierce", decrease = 5, percent = 25 })
+    table.insert(
+      data.raw["cargo-wagon"]["cargo-wagon"].resistances,
+      { type = "bob-pierce", decrease = 5, percent = 25 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["fluid-wagon"].resistances,
+      { type = "bob-pierce", decrease = 5, percent = 25 }
+    )
+
+    table.insert(
+      data.raw.locomotive["bob-locomotive-2"].resistances,
+      { type = "bob-pierce", decrease = 5, percent = 25 }
+    )
+    table.insert(
+      data.raw["cargo-wagon"]["bob-cargo-wagon-2"].resistances,
+      { type = "bob-pierce", decrease = 5, percent = 25 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["bob-fluid-wagon-2"].resistances,
+      { type = "bob-pierce", decrease = 5, percent = 25 }
+    )
+
+    table.insert(
+      data.raw.locomotive["bob-locomotive-3"].resistances,
+      { type = "bob-pierce", decrease = 5, percent = 25 }
+    )
+    table.insert(
+      data.raw["cargo-wagon"]["bob-cargo-wagon-3"].resistances,
+      { type = "bob-pierce", decrease = 5, percent = 25 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["bob-fluid-wagon-3"].resistances,
+      { type = "bob-pierce", decrease = 5, percent = 25 }
+    )
+
+    table.insert(
+      data.raw.locomotive["bob-armoured-locomotive"].resistances,
+      { type = "bob-pierce", decrease = 15, percent = 90 }
+    )
+    table.insert(
+      data.raw["cargo-wagon"]["bob-armoured-cargo-wagon"].resistances,
+      { type = "bob-pierce", decrease = 15, percent = 90 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["bob-armoured-fluid-wagon"].resistances,
+      { type = "bob-pierce", decrease = 15, percent = 90 }
+    )
+
+    table.insert(
+      data.raw.locomotive["bob-armoured-locomotive-2"].resistances,
+      { type = "bob-pierce", decrease = 15, percent = 90 }
+    )
+    table.insert(
+      data.raw["cargo-wagon"]["bob-armoured-cargo-wagon-2"].resistances,
+      { type = "bob-pierce", decrease = 15, percent = 90 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["bob-armoured-fluid-wagon-2"].resistances,
+      { type = "bob-pierce", decrease = 15, percent = 90 }
+    )
+  end
+
+  if data.raw["damage-type"]["plasma"] then
+    table.insert(data.raw.locomotive["locomotive"].resistances, { type = "plasma", decrease = 15, percent = 50 })
+    table.insert(data.raw["cargo-wagon"]["cargo-wagon"].resistances, { type = "plasma", decrease = 15, percent = 50 })
+    table.insert(data.raw["fluid-wagon"]["fluid-wagon"].resistances, { type = "plasma", decrease = 15, percent = 50 })
+
+    table.insert(data.raw.locomotive["bob-locomotive-2"].resistances, { type = "plasma", decrease = 15, percent = 50 })
+    table.insert(
+      data.raw["cargo-wagon"]["bob-cargo-wagon-2"].resistances,
+      { type = "plasma", decrease = 15, percent = 50 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["bob-fluid-wagon-2"].resistances,
+      { type = "plasma", decrease = 15, percent = 50 }
+    )
+
+    table.insert(data.raw.locomotive["bob-locomotive-3"].resistances, { type = "plasma", decrease = 15, percent = 50 })
+    table.insert(
+      data.raw["cargo-wagon"]["bob-cargo-wagon-3"].resistances,
+      { type = "plasma", decrease = 15, percent = 50 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["bob-fluid-wagon-3"].resistances,
+      { type = "plasma", decrease = 15, percent = 50 }
+    )
+
+    table.insert(
+      data.raw.locomotive["bob-armoured-locomotive"].resistances,
+      { type = "plasma", decrease = 25, percent = 90 }
+    )
+    table.insert(
+      data.raw["cargo-wagon"]["bob-armoured-cargo-wagon"].resistances,
+      { type = "plasma", decrease = 25, percent = 90 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["bob-armoured-fluid-wagon"].resistances,
+      { type = "plasma", decrease = 25, percent = 90 }
+    )
+
+    table.insert(
+      data.raw.locomotive["bob-armoured-locomotive-2"].resistances,
+      { type = "plasma", decrease = 25, percent = 90 }
+    )
+    table.insert(
+      data.raw["cargo-wagon"]["bob-armoured-cargo-wagon-2"].resistances,
+      { type = "plasma", decrease = 25, percent = 90 }
+    )
+    table.insert(
+      data.raw["fluid-wagon"]["bob-armoured-fluid-wagon-2"].resistances,
+      { type = "plasma", decrease = 25, percent = 90 }
+    )
+  end
+end
+
+for index, inserter in pairs(data.raw.inserter) do
+  inserter.allow_custom_vectors = true
+  if settings.startup["bobmods-logistics-drainlessinserters"].value == true then
+    inserter.energy_source.drain = "0W"
+  end
+end
+
+if settings.startup["bobmods-logistics-disableroboports"].value == true then
+  bobmods.lib.item.hide_entity("roboport", "roboport")
+  bobmods.lib.item.hide("roboport")
+  bobmods.lib.recipe.hide("roboport")
+end
+
+bobmods.lib.item.set_subgroup("transport-belt", "bob-logistic-tier-1")
+bobmods.lib.item.set_subgroup("underground-belt", "bob-logistic-tier-1")
+bobmods.lib.item.set_subgroup("splitter", "bob-logistic-tier-1")
+
+bobmods.lib.item.set_subgroup("fast-transport-belt", "bob-logistic-tier-2")
+bobmods.lib.item.set_subgroup("fast-underground-belt", "bob-logistic-tier-2")
+bobmods.lib.item.set_subgroup("fast-splitter", "bob-logistic-tier-2")
+
+bobmods.lib.item.set_subgroup("express-transport-belt", "bob-logistic-tier-3")
+bobmods.lib.item.set_subgroup("express-underground-belt", "bob-logistic-tier-3")
+bobmods.lib.item.set_subgroup("express-splitter", "bob-logistic-tier-3")
+
+-- because there's no specific section for it.
+data.raw["pump"]["pump"].next_upgrade = "bob-pump-2"
+
+if settings.startup["bobmods-logistics-highpipes"].value == true then
+  bobmods.logistics.scale_machine_output_base_level()
+end
+
+require("prototypes.se-updates")
---
+++ mods/boblogistics_1.2.1/prototypes/transport-belt-pictures.lua
@@ -0,0 +1,59 @@
+bobmods.logistics.black_belt_animation_set = {
+  animation_set = {
+    filename = "__boblogistics__/graphics/entity/transport-belt/black-transport-belt.png",
+    priority = "extra-high",
+    width = 64,
+    height = 64,
+    frame_count = 16,
+    direction_count = 20,
+    hr_version = {
+      filename = "__boblogistics__/graphics/entity/transport-belt/hr-black-transport-belt.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5,
+      frame_count = 16,
+      direction_count = 20,
+    },
+  },
+}
+
+bobmods.logistics.purple_belt_animation_set = {
+  animation_set = {
+    filename = "__boblogistics__/graphics/entity/transport-belt/purple-transport-belt.png",
+    priority = "extra-high",
+    width = 64,
+    height = 64,
+    frame_count = 32,
+    direction_count = 20,
+    hr_version = {
+      filename = "__boblogistics__/graphics/entity/transport-belt/hr-purple-transport-belt.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5,
+      frame_count = 32,
+      direction_count = 20,
+    },
+  },
+}
+
+bobmods.logistics.green_belt_animation_set = {
+  animation_set = {
+    filename = "__boblogistics__/graphics/entity/transport-belt/green-transport-belt.png",
+    priority = "extra-high",
+    width = 64,
+    height = 64,
+    frame_count = 32,
+    direction_count = 20,
+    hr_version = {
+      filename = "__boblogistics__/graphics/entity/transport-belt/hr-green-transport-belt.png",
+      priority = "extra-high",
+      width = 128,
+      height = 128,
+      scale = 0.5,
+      frame_count = 32,
+      direction_count = 20,
+    },
+  },
+}
