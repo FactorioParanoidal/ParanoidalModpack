220373d8b9b3661cb974a1daaabcffb3bceee93b
drd_avel <avelinadmitrieva@gmail.com>
2024-01-14 21:56:11 +0300

    Обновление до bobwarfare_1.2.0

---
+++ mods/bobwarfare_1.2.0/data-updates.lua
@@ -0,0 +1,49 @@
+require("prototypes.overides")
+require("prototypes.recipe.recipe-updates")
+require("prototypes.technology.technology-updates")
+require("prototypes.productivity-limitations")
+
+require("prototypes.robots-updates")
+require("prototypes.train-updates")
+require("prototypes.recipe.drone-updates")
+require("prototypes.spidertron-updates")
+
+if settings.startup["bobmods-warfare-drainlesslaserturrets"].value == true then
+  for index, turret in pairs(data.raw["electric-turret"]) do
+    turret.energy_source.drain = "0W"
+  end
+end
+
+table.insert(data.raw.car.tank.resistances, { type = "plasma", decrease = 15, percent = 50 })
+
+if settings.startup["bobmods-warfare-vehicleflamethrowerstartsfires"].value == true then
+  data.raw.ammo["flamethrower-ammo"].ammo_type = {
+    {
+      category = "flamethrower",
+      clamp_position = true,
+      source_type = "default",
+      target_type = "position",
+      action = {
+        type = "direct",
+        action_delivery = {
+          type = "stream",
+          stream = "handheld-flamethrower-fire-stream",
+        },
+      },
+    },
+    {
+      category = "flamethrower",
+      clamp_position = true,
+      source_type = "vehicle",
+      target_type = "position",
+      consumption_modifier = 1.125,
+      action = {
+        type = "direct",
+        action_delivery = {
+          type = "stream",
+          stream = "flamethrower-fire-stream",
+        },
+      },
+    },
+  }
+end
---
+++ mods/bobwarfare_1.2.0/prototypes/item/mine.lua
@@ -0,0 +1,37 @@
+data:extend({
+  {
+    type = "item",
+    name = "poison-mine",
+    icon = "__base__/graphics/icons/land-mine.png",
+    icon_size = 64,
+    icon_mipmaps = 4,
+    subgroup = "gun",
+    order = "f[poison-mine]",
+    place_result = "poison-mine",
+    stack_size = 20,
+  },
+
+  {
+    type = "item",
+    name = "distractor-mine",
+    icon = "__base__/graphics/icons/land-mine.png",
+    icon_size = 64,
+    icon_mipmaps = 4,
+    subgroup = "gun",
+    order = "f[distractor-mine]",
+    place_result = "distractor-mine",
+    stack_size = 20,
+  },
+
+  {
+    type = "item",
+    name = "slowdown-mine",
+    icon = "__base__/graphics/icons/land-mine.png",
+    icon_size = 64,
+    icon_mipmaps = 4,
+    subgroup = "gun",
+    order = "f[slowdown-mine]",
+    place_result = "slowdown-mine",
+    stack_size = 20,
+  },
+})
