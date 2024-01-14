2c6bc305e6720c421cb9346865de7ae15598ca9a
drd_avel <avelinadmitrieva@gmail.com>
2024-01-14 19:19:05 +0300

    Удалён мод nixie-tubes_1.1.3 - сами ставят себе те, кому он нужен. Декоративный и поджирает UPS на старых компах даже без использования.

--- mods/nixie-tubes_1.1.3/data.lua
+++
@@ -1,4 +0,0 @@
-require("prototypes.nixie")
-require("prototypes.technology")
-require("prototypes.signals")
-require("prototypes.simulations")
--- mods/nixie-tubes_1.1.3/migrations/alphanixie.lua
+++
@@ -1,4 +0,0 @@
-for _,f in pairs(game.forces) do 
-  f.reset_recipes()
-  f.recipes['nixie-tube-alpha'].enabled = f.technologies['cathodes'].researched 
-end
