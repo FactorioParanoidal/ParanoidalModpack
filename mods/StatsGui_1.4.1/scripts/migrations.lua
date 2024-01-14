39c8aa864500058e58770f3324b1892f750e70d4
drd_avel <avelinadmitrieva@gmail.com>
2024-01-14 12:08:10 +0300

    Удаление StatsGui - дублирует функции EvoGUI, если кому надо - ставит это сам.

--- mods/StatsGui_1.4.1/scripts/migrations.lua
+++
@@ -1,5 +0,0 @@
-return {
-  ["1.1.0"] = function()
-    global.research_progress_strings = {}
-  end,
-}
--- mods/StatsGui_1.4.1/scripts/sensor/playtime.lua
+++
@@ -1,9 +0,0 @@
-local misc = require("__flib__.misc")
-
-return function(player)
-  if not global.players[player.index].settings.show_playtime then
-    return
-  end
-
-  return { "", { "statsgui.playtime" }, " = ", misc.ticks_to_timestring(game.ticks_played) }
-end
