0b4a5060849aa8c6e00b857d4128f92ad927fa47
drd_avel <avelinadmitrieva@gmail.com>
2024-01-22 02:15:59 +0300

    Вынос хардкода за пределы bobplates_1.2.0

---
+++ mods/zzzparanoidal_1.0.1/prototypes/bobfix.lua
@@ -0,0 +1,6 @@
+ --bobplates small icon size saves vram
+ 
+ data.raw.technology["fluid-canister-processing"].icon_size = 128
+ data.raw.technology["fluid-canister-processing"].icon = "__zzzparanoidal__/graphics/bob/fluid-canister.png"
+ 
+ --
