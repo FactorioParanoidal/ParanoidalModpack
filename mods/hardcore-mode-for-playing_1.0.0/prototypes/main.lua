4fbd66404e0e24bdd4110d64954bba50901899a7
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-01-22 22:48:40 +0300

    add fuel category resetting, update stream-recipes, burner prototypes with fuel category

--- mods/hardcore-mode-for-playing_1.0.0/prototypes/main.lua
+++ mods/hardcore-mode-for-playing_1.0.0/prototypes/main.lua
@@ -1,9 +1,9 @@
 flib = require("__flib__.data-util")
 require("__automated-utility-protocol__.util.main")
 require("resources")
-require("prototypes.items")
-require("prototypes.entities")
-require("prototypes.recipes")
-require("prototypes.technologies")
+require("items")
+require("entities")
+require("recipes")
+require("technologies")
 require("crash-site-spaceship-entries")
 flib = nil
