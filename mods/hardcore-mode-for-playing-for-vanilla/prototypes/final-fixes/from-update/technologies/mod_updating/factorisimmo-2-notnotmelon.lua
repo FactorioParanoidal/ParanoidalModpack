01586cb4bd5dc1faa2b5ffd54bb2313d17dd046c
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-02-04 21:59:07 +0300

    hardcore-mode-for-playing-for-zzzparanoidal доведён до запускаемого состояния

--- mods/hardcore-mode-for-playing-for-vanilla/prototypes/final-fixes/from-update/technologies/mod_updating/factorisimmo-2-notnotmelon.lua
+++ mods/hardcore-mode-for-playing-for-vanilla/prototypes/final-fixes/from-update/technologies/mod_updating/factorisimmo-2-notnotmelon.lua
@@ -5,30 +5,30 @@ local function updateFactorissimoMod(mode)
 	end
 	local technologies = data.raw["technology"]
 	local connection_chest_tech = technologies["factory-connection-type-chest"]
-	techUtil.resetTechnologyPrerequisites(connection_chest_tech, { "factory-architecture-t1", "logistics" }, mode)
+	techUtil.reset_prerequisites_for_technology(connection_chest_tech, { "factory-architecture-t1", "logistics" }, mode)
 	local connection_fluid_tech = technologies["factory-connection-type-fluid"]
-	techUtil.resetTechnologyPrerequisites(
+	techUtil.reset_prerequisites_for_technology(
 		connection_fluid_tech,
 		{ "factory-architecture-t1", "coal-wooden-fluid-handling" },
 		mode
 	)
 	local connection_circuit_tech = technologies["factory-connection-type-circuit"]
-	techUtil.resetTechnologyPrerequisites(
+	techUtil.reset_prerequisites_for_technology(
 		connection_circuit_tech,
 		{ "factory-architecture-t1", "circuit-network" },
 		mode
 	)
 	local connection_lights_tech = technologies["factory-interior-upgrade-lights"]
-	techUtil.resetTechnologyPrerequisites(connection_lights_tech, { "factory-architecture-t1", "optics" }, mode)
+	techUtil.reset_prerequisites_for_technology(connection_lights_tech, { "factory-architecture-t1", "optics" }, mode)
 	local connection_display_tech = technologies["factory-interior-upgrade-display"]
-	techUtil.resetTechnologyPrerequisites(
+	techUtil.reset_prerequisites_for_technology(
 		connection_display_tech,
 		{ "factory-architecture-t1", "factory-connection-type-circuit" },
 		mode
 	)
-	techUtil.removeSciencePackFrom(connection_chest_tech, "logistic-science-pack", mode)
-	techUtil.removeSciencePackFrom(connection_circuit_tech, "logistic-science-pack", mode)
-	techUtil.removeSciencePackFrom(connection_display_tech, "logistic-science-pack", mode)
+	techUtil.remove_science_pack_from_technology_units(connection_chest_tech, "logistic-science-pack", mode)
+	techUtil.remove_science_pack_from_technology_units(connection_circuit_tech, "logistic-science-pack", mode)
+	techUtil.remove_science_pack_from_technology_units(connection_display_tech, "logistic-science-pack", mode)
 	local archtecture_t2_tech = technologies["factory-architecture-t2"]
 	archtecture_t2_tech.unit = {
 		ingredients = {
@@ -38,17 +38,21 @@ local function updateFactorissimoMod(mode)
 		count = 500,
 		time = 60,
 	}
-	techUtil.addPrerequisitesToTechnology(technologies["military-science-pack"], { "factory-architecture-t2" }, mode)
-	techUtil.addPrerequisitesToTechnology(technologies["chemical-science-pack"], { "factory-architecture-t2" }, mode)
-	techUtil.addPrerequisitesToTechnology(technologies["production-science-pack"], { "factory-recursion-t1" }, mode)
+	techUtil.add_prerequisites_to_technology(technologies["military-science-pack"], { "factory-architecture-t2" }, mode)
+	techUtil.add_prerequisites_to_technology(technologies["chemical-science-pack"], { "factory-architecture-t2" }, mode)
+	techUtil.add_prerequisites_to_technology(technologies["production-science-pack"], { "factory-recursion-t1" }, mode)
 	if mods["bobpower"] then
-		techUtil.addPrerequisitesToTechnology(technologies["factory-connection-type-heat"], { "bob-heat-pipe-1" }, mode)
+		techUtil.add_prerequisites_to_technology(
+			technologies["factory-connection-type-heat"],
+			{ "bob-heat-pipe-1" },
+			mode
+		)
 	end
 	local architecture_t3_tech = technologies["factory-architecture-t3"]
-	techUtil.addPrerequisitesToTechnology(architecture_t3_tech, { "utility-science-pack" }, mode)
-	techUtil.removeSciencePackFrom(architecture_t3_tech, "production-science-pack", mode)
-	techUtil.addSciencePacksToTechnologyUnits(architecture_t3_tech, { { "utility-science-pack", 1 } }, mode)
-	techUtil.addPrerequisitesToTechnology(technologies["space-science-pack"], { "factory-recursion-t2" }, mode)
+	techUtil.add_prerequisites_to_technology(architecture_t3_tech, { "utility-science-pack" }, mode)
+	techUtil.remove_science_pack_from_technology_units(architecture_t3_tech, "production-science-pack", mode)
+	techUtil.add_science_packs_to_technology_units(architecture_t3_tech, { { "utility-science-pack", 1 } }, mode)
+	techUtil.add_prerequisites_to_technology(technologies["space-science-pack"], { "factory-recursion-t2" }, mode)
 end
 _table.each(GAME_MODES, function(mode)
 	updateFactorissimoMod(mode)
