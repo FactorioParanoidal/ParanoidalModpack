bcc0bfd948fc1bab43a5fc7fdf73066ec289486a
AKMFrtx <157271387+AKMFrtx@users.noreply.github.com>
2024-02-11 21:10:13 +0300

    Update micro-final-fix.lua (#20)
    Иконки топливных ячеек

--- mods/zzzparanoidal/prototypes/micro-final-fix.lua
+++ mods/zzzparanoidal/prototypes/micro-final-fix.lua
@@ -953,11 +953,12 @@ data.raw["recipe"]["angels-concrete"].hidden = true
 bobmods.lib.tech.remove_recipe_unlock("angels-stone-smelting-2", "angels-concrete")
 -------------------------------------------------------------------------------------------------
 --графические фиксы
-data.raw.technology["plutonium-fuel-cell"].icon_size = 64
-data.raw.technology["plutonium-fuel-cell"].icon_mipmaps = 4
-
-data.raw.technology["thorium-plutonium-fuel-cell"].icon_size = 64
-data.raw.technology["thorium-plutonium-fuel-cell"].icon_mipmaps = 4
+data.raw.technology["plutonium-fuel-cell"].icons = {{icon = "__bobplates__/graphics/icons/nuclear/plutonium-fuel-cell.png", icon_size = 64, icon_mipmaps = 4}} --AKMF
+data.raw.technology["thorium-plutonium-fuel-cell"].icons = {{icon = "__bobplates__/graphics/icons/nuclear/thorium-plutonium-fuel-cell.png", icon_size = 64, icon_mipmaps = 4}} --AKMF
+--data.raw.technology["plutonium-fuel-cell"].icon_size = 64 не знаю почему, но этот вариант не работает --AKMF
+--data.raw.technology["plutonium-fuel-cell"].icon_mipmaps = 4
+--data.raw.technology["thorium-plutonium-fuel-cell"].icon_size = 64
+--data.raw.technology["thorium-plutonium-fuel-cell"].icon_mipmaps = 4
 
 data.raw.technology["effect-transmission"].icon = "__base__/graphics/technology/effect-transmission.png"
 data.raw.technology["effect-transmission"].icon_size = 256
