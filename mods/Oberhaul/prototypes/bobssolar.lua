6fba03d4b75f246569d378efcf47023d2f74920e
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-03-03 00:03:19 +0300

    удалён несуществующий ингредиент

--- mods/Oberhaul/prototypes/bobssolar.lua
+++ mods/Oberhaul/prototypes/bobssolar.lua
@@ -1,22 +1,21 @@
-bobmods.lib.recipe.add_ingredient("solar-panel-small-2", {"solar-panel-small", 3})
-bobmods.lib.recipe.add_ingredient("solar-panel-2", {"solar-panel", 3})
-bobmods.lib.recipe.add_ingredient("solar-panel-large-2", {"solar-panel-large", 3})
+bobmods.lib.recipe.add_ingredient("solar-panel-small-2", { "solar-panel-small", 3 })
+bobmods.lib.recipe.add_ingredient("solar-panel-2", { "solar-panel", 3 })
+bobmods.lib.recipe.add_ingredient("solar-panel-large-2", { "solar-panel-large", 3 })
 
 data.raw["solar-panel"]["solar-panel-small-2"].production = "106.68kW"
 data.raw["solar-panel"]["solar-panel-2"].production = "240kW"
 data.raw["solar-panel"]["solar-panel-large-2"].production = "426.72kW"
 
-bobmods.lib.recipe.add_ingredient("solar-panel-small-3", {"solar-panel-small-2", 3})
-bobmods.lib.recipe.add_ingredient("solar-panel-3", {"solar-panel-2", 3})
-bobmods.lib.recipe.add_ingredient("solar-panel-large-3", {"solar-panel-large-2", 3})
+bobmods.lib.recipe.add_ingredient("solar-panel-small-3", { "solar-panel-small-2", 3 })
+bobmods.lib.recipe.add_ingredient("solar-panel-3", { "solar-panel-2", 3 })
+bobmods.lib.recipe.add_ingredient("solar-panel-large-3", { "solar-panel-large-2", 3 })
 
 data.raw["solar-panel"]["solar-panel-small-3"].production = "426.72kW"
 data.raw["solar-panel"]["solar-panel-3"].production = "960kW"
 data.raw["solar-panel"]["solar-panel-large-3"].production = "1706.88kW"
 
-bobmods.lib.recipe.add_ingredient("large-accumulator-2", {"large-accumulator", 3})
-bobmods.lib.recipe.add_ingredient("fast-accumulator-2", {"fast-accumulator", 3})
-bobmods.lib.recipe.add_ingredient("slow-accumulator-2", {"slow-accumulator", 3})
+bobmods.lib.recipe.add_ingredient("fast-accumulator-2", { "fast-accumulator", 3 })
+bobmods.lib.recipe.add_ingredient("slow-accumulator-2", { "slow-accumulator", 3 })
 
 data.raw["accumulator"]["large-accumulator-2"].energy_source.buffer_capacity = "40MJ"
 data.raw["accumulator"]["large-accumulator-2"].energy_source.input_flow_limit = "240kW"
@@ -30,9 +29,9 @@ data.raw["accumulator"]["slow-accumulator-2"].energy_source.buffer_capacity = "1
 data.raw["accumulator"]["slow-accumulator-2"].energy_source.input_flow_limit = "960kW"
 data.raw["accumulator"]["slow-accumulator-2"].energy_source.output_flow_limit = "120kW"
 
-bobmods.lib.recipe.add_ingredient("large-accumulator-3", {"large-accumulator-2", 3})
-bobmods.lib.recipe.add_ingredient("fast-accumulator-3", {"fast-accumulator-2", 3})
-bobmods.lib.recipe.add_ingredient("slow-accumulator-3", {"slow-accumulator-2", 3})
+bobmods.lib.recipe.add_ingredient("large-accumulator-3", { "large-accumulator-2", 3 })
+bobmods.lib.recipe.add_ingredient("fast-accumulator-3", { "fast-accumulator-2", 3 })
+bobmods.lib.recipe.add_ingredient("slow-accumulator-3", { "slow-accumulator-2", 3 })
 
 data.raw["accumulator"]["large-accumulator-3"].energy_source.buffer_capacity = "160MJ"
 data.raw["accumulator"]["large-accumulator-3"].energy_source.input_flow_limit = "960kW"
