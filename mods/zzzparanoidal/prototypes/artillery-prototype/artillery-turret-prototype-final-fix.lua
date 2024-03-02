7dbfc09f980f62d1a5672e4a5c6230745f960953
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-03-03 00:17:10 +0300

    исправление ошибок технологий

--- mods/zzzparanoidal/prototypes/artillery-prototype/artillery-turret-prototype-final-fix.lua
+++ mods/zzzparanoidal/prototypes/artillery-prototype/artillery-turret-prototype-final-fix.lua
@@ -1,7 +1,5 @@
 --добавляем к рецепту базовой артиллерии прототип.
-bobmods.lib.recipe.add_ingredient("artillery-turret", {"artillery-turret-prototype", 2})
+bobmods.lib.recipe.add_ingredient("artillery-turret", { "artillery-turret-prototype", 2 })
 -------------------------------------------------------------------------------------------------
 --привязываем базовую артиллерию к новому прототипу
 bobmods.lib.tech.add_prerequisite("artillery", "artillery-prototype")
---удаляем лишние зависимости
-bobmods.lib.tech.remove_prerequisite("artillery", "bi-tech-bio-cannon")
