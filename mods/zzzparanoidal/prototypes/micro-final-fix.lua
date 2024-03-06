032cc1bc4f05ce1eb1a6ec6e6a0339fcbc2c48d5
DESKTOP-FT62ADR\test2 <111>
2024-03-06 23:58:49 +0300

    ФИКС уровеней технологий для Жидк.Бойлеров 4, 5 в соответствии с их рецептом by AKMF

--- mods/zzzparanoidal/prototypes/micro-final-fix.lua
+++ mods/zzzparanoidal/prototypes/micro-final-fix.lua
@@ -1173,3 +1173,7 @@ bobmods.lib.tech.add_recipe_unlock("angels-metallurgy-3", "advanced-structure-co
 --фикс технологий: Продвинутое исследование после турбо пакетников (AKMF)
 bobmods.lib.tech.remove_prerequisite("advanced-research", "express-inserters")
 bobmods.lib.tech.add_prerequisite("advanced-research", "stack-inserter-3")
+
+--Добавлен дополнительный уровень технологий для Жидк.Бойлеров 4, 5 в соответсвии с их рецептом (AKMF)
+data.raw.technology["OilBurning-4"].unit.ingredients = {{"automation-science-pack", 2}, {"logistic-science-pack", 2}, {"chemical-science-pack", 2}, {"production-science-pack", 2}}
+data.raw.technology["OilBurning-5"].unit.ingredients = {{"automation-science-pack", 2}, {"logistic-science-pack", 2}, {"chemical-science-pack", 2}, {"production-science-pack", 2}, {"utility-science-pack", 2}}
