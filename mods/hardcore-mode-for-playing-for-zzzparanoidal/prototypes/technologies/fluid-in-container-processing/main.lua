355f59ab14bdb005f71d279958f6c9932dd39690
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-03-28 00:52:20 +0300

    пофикшены импорты

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/fluid-in-container-processing/main.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/fluid-in-container-processing/main.lua
@@ -1,10 +1,4 @@
 require("__hardcore-mode-for-playing__.prototypes.fluid-in-container-processing")
-require(
-	"prototypes.final-fixes.from-update.technologies.fluid-in-container-processing.fluid-barrel-processing-technology-fixes"
-)
-require(
-	"prototypes.final-fixes.from-update.technologies.fluid-in-container-processing.fluid-canister-processing-technology-fixes"
-)
-require(
-	"prototypes.final-fixes.from-update.technologies.fluid-in-container-processing.gas-canister-proccessing-technology-fixes"
-)
+require("fluid-barrel-processing-technology-fixes")
+require("fluid-canister-processing-technology-fixes")
+require("gas-canister-proccessing-technology-fixes")
