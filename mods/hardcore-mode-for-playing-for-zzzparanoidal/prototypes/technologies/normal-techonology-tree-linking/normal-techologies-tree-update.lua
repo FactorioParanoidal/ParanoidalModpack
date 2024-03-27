32005ad9391e50b98ffa68760264a78fa7cee830
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-03-27 22:28:49 +0300

    обновлена структура мода, внесены фиксы в дерево технологий.

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/normal-techologies-tree-update.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/normal-techologies-tree-update.lua
@@ -1,5 +1,3 @@
-require("prototypes.final-fixes.from-update.technologies.mod_updating.junk-train-3")
-require("prototypes.final-fixes.from-update.technologies.mod_updating.factorisimmo-2-notnotmelon")
 --
 require("__hardcore-mode-for-playing__.prototypes.fuel-category-prototype-updates")
 require("__hardcore-mode-for-playing__.prototypes.steam-processing")
