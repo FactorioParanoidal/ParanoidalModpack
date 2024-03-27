bd0b95c770bf3f15019f2f16ae846ca684fa0df5
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-03-28 00:53:08 +0300

    удалён ненужный импорт, содержимое импортируемого файла перенесено в main.lua

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/normal-techologies-tree-update.lua
+++
@@ -1,4 +0,0 @@
---
-require("__hardcore-mode-for-playing__.prototypes.fuel-category-prototype-updates")
-require("__hardcore-mode-for-playing__.prototypes.steam-processing")
-require("__hardcore-mode-for-playing__.prototypes.burner-prototypes")
