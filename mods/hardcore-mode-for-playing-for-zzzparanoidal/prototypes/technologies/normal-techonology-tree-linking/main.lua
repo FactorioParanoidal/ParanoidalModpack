f8fe0aaac0c159127043c6a88cb2e32d43943f57
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-04-28 18:00:13 +0300

    Вынесение фиксов рецептов бойлеров в отдельный файл, обновление списка пофикшенных рецептов после проверки дерева технологий.

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/main.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/main.lua
@@ -6,24 +6,4 @@ require("__hardcore-mode-for-playing__.prototypes.burner-prototypes.main")
 require(
 	"__make-moded-technology-tree-mods-protocol__.prototypes.copying-for-modes.copy-to-normal-expensive-mode-recipe-data"
 )
-
-local boiler_recipe_ingredients = {
-	{
-		type = "item",
-		name = "bi-wood-pipe",
-		amount = 6,
-	},
-	{
-		type = "item",
-		name = "stone-furnace",
-		amount = 1,
-	},
-}
-
-local boiler_wood_165_recipe = data.raw["recipe"]["boiler-steam-165-with-fuel-item-wood"]
-boiler_wood_165_recipe.normal.ingredients = boiler_recipe_ingredients
-boiler_wood_165_recipe.expensive.ingredients = boiler_recipe_ingredients
-
-local boiler_coal_165_recipe = data.raw["recipe"]["boiler-steam-165-with-fuel-item-coal"]
-boiler_coal_165_recipe.normal.ingredients = boiler_recipe_ingredients
-boiler_coal_165_recipe.expensive.ingredients = boiler_recipe_ingredients
+require("recipe-final-fixes-after-technology-linking")
