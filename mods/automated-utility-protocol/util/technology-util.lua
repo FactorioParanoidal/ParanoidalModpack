0782ae7b29fdfe080672e4ee1704648610add11a
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-05-26 15:17:53 +0300

    Переименование переменных с подчёркиванием

--- mods/automated-utility-protocol/util/technology-util.lua
+++ mods/automated-utility-protocol/util/technology-util.lua
@@ -1,5 +1,5 @@
 local TechUtil = {}
-local recipeUtil = require("__automated-utility-protocol__.util.recipe-util")
+local recipe_util = require("__automated-utility-protocol__.util.recipe-util")
 require("__automated-utility-protocol__.util.technology-tree-util")
 local function technology_not_found()
 	error("technology is null!!")
@@ -19,10 +19,8 @@ local function get_moded_technology(technology_candidate, mode)
 	return result
 end
 local function filter_technology_effects_unlock_recipe(effect)
-	return effect
-		and effect.type == "unlock-recipe"
-		and effect.recipe
-	--	and not recipeUtil.is_contain_dry411_srev(effect.recipe)
+	return effect and effect.type == "unlock-recipe" and effect.recipe
+	--	and not recipe_util.is_contain_dry411_srev(effect.recipe)
 end
 
 local function get_technology_object_effect_recipes_by_name(technology_candidate, mode)
@@ -36,22 +34,28 @@ end
 TechUtil.get_all_recipe_ingredients_for_specified_technology = function(technology_name, mode)
 	local result = {}
 	local unlocked_recipes = get_technology_object_effect_recipes_by_name(data.raw["technology"][technology_name], mode)
-	_table.each(unlocked_recipes, function(unlocked_recipe)
-		local recipe_name = unlocked_recipe.recipe
-		local ingredients = recipeUtil.get_all_recipe_ingredients(recipe_name, mode)
-		_table.insert_all_if_not_exists(result, ingredients)
-	end)
+	_table.each(
+		unlocked_recipes,
+		function(unlocked_recipe)
+			local recipe_name = unlocked_recipe.recipe
+			local ingredients = recipe_util.get_all_recipe_ingredients(recipe_name, mode)
+			_table.insert_all_if_not_exists(result, ingredients)
+		end
+	)
 	return result
 end
 
 TechUtil.get_all_recipe_results_for_specified_technology = function(technology_name, mode)
 	local result = {}
 	local unlocked_recipes = get_technology_object_effect_recipes_by_name(data.raw["technology"][technology_name], mode)
-	_table.each(unlocked_recipes, function(unlocked_recipe)
-		local recipe_name = unlocked_recipe.recipe
-		local results = recipeUtil.get_all_recipe_results(recipe_name, mode)
-		_table.insert_all_if_not_exists(result, results)
-	end)
+	_table.each(
+		unlocked_recipes,
+		function(unlocked_recipe)
+			local recipe_name = unlocked_recipe.recipe
+			local results = recipe_util.get_all_recipe_results(recipe_name, mode)
+			_table.insert_all_if_not_exists(result, results)
+		end
+	)
 	return result
 end
 
@@ -61,11 +65,14 @@ TechUtil.get_all_tool_units_for_specified_technology = function(technology_name,
 	if not technology_object or not technology_object.unit or not technology_object.unit.ingredients then
 		return
 	end
-	_table.each(technology_object.unit.ingredients, function(research_unit_ingredient)
-		local ingredient_name = research_unit_ingredient.name or research_unit_ingredient[1]
-		local research_unit_ingredient_type = research_unit_ingredient.type or "item"
-		table.insert(result, { type = research_unit_ingredient_type, name = ingredient_name })
-	end)
+	_table.each(
+		technology_object.unit.ingredients,
+		function(research_unit_ingredient)
+			local ingredient_name = research_unit_ingredient.name or research_unit_ingredient[1]
+			local research_unit_ingredient_type = research_unit_ingredient.type or "item"
+			table.insert(result, {type = research_unit_ingredient_type, name = ingredient_name})
+		end
+	)
 	return result
 end
 
@@ -79,8 +86,9 @@ TechUtil.get_all_recipe_names_for_specified_technology = function(technology_nam
 end
 
 TechUtil.get_all_technology_names_with_fuel_result_specified_in_another_recipe_by_name = function(recipe_name, mode)
-	local with_fuel_value_items_or_fluids = _table.filter(
-		recipeUtil.get_all_recipe_results(recipe_name, mode),
+	local with_fuel_value_items_or_fluids =
+		_table.filter(
+		recipe_util.get_all_recipe_results(recipe_name, mode),
 		function(result_data)
 			local result_data_type = result_data.type
 			local result_data_name = result_data.name or result_data[1]
@@ -92,44 +100,59 @@ TechUtil.get_all_technology_names_with_fuel_result_specified_in_another_recipe_b
 	end
 	local technology_names = TechUtil.get_all_active_technology_names(mode)
 	local result = {}
-	_table.each(with_fuel_value_items_or_fluids, function(with_fuel_value_item_or_fluid)
-		local with_fuel_value_item_or_fluid_name = with_fuel_value_item_or_fluid.name
-			or with_fuel_value_item_or_fluid[1]
-		local target_item_or_fluid = with_fuel_value_item_or_fluid
-		if string.find(with_fuel_value_item_or_fluid_name, "-barrel", 1, true) then
-			target_item_or_fluid =
-				_table.filter(recipeUtil.get_all_recipe_ingredients(recipe_name, mode), function(recipe_ingredient)
-					return recipe_ingredient.type == "fluid"
-				end)[1]
+	_table.each(
+		with_fuel_value_items_or_fluids,
+		function(with_fuel_value_item_or_fluid)
+			local with_fuel_value_item_or_fluid_name = with_fuel_value_item_or_fluid.name or with_fuel_value_item_or_fluid[1]
+			local target_item_or_fluid = with_fuel_value_item_or_fluid
+			if string.find(with_fuel_value_item_or_fluid_name, "-barrel", 1, true) then
+				target_item_or_fluid =
+					_table.filter(
+					recipe_util.get_all_recipe_ingredients(recipe_name, mode),
+					function(recipe_ingredient)
+						return recipe_ingredient.type == "fluid"
+					end
+				)[1]
+			end
+			local technology_name_with_result =
+				_table.filter(
+				technology_names,
+				function(technology_name)
+					local results = TechUtil.get_all_recipe_results_for_specified_technology(technology_name, mode)
+					return _table.contains_f_deep(results, target_item_or_fluid)
+				end
+			)
+			result[target_item_or_fluid] = technology_name_with_result
 		end
-		local technology_name_with_result = _table.filter(technology_names, function(technology_name)
-			local results = TechUtil.get_all_recipe_results_for_specified_technology(technology_name, mode)
-			return _table.contains_f_deep(results, target_item_or_fluid)
-		end)
-		result[target_item_or_fluid] = technology_name_with_result
-	end)
+	)
 	return result
 end
 
 TechUtil.get_all_active_technology_names = function(mode)
 	local result = {}
-	_table.each(data.raw["technology"], function(technology_candidate)
-		local technology_name = technology_candidate.name
-		local technology = get_moded_technology(technology_candidate, mode)
-		if
-			not technology.hidden
-			or technology.hidden and _string.ends_with(technology_name, DETECTED_RESOURCE_TECHNOLOGY_SUFFIX)
-		then
-			table.insert(result, technology_name)
+	_table.each(
+		data.raw["technology"],
+		function(technology_candidate)
+			local technology_name = technology_candidate.name
+			local technology = get_moded_technology(technology_candidate, mode)
+			if
+				not technology.hidden or
+					technology.hidden and _string.ends_with(technology_name, DETECTED_RESOURCE_TECHNOLOGY_SUFFIX)
+			 then
+				table.insert(result, technology_name)
+			end
 		end
-	end)
+	)
 	return result
 end
 
 TechUtil.get_all_technology_names_with_hidden = function()
-	return _table.map(data.raw["technology"], function(technology)
-		return technology.name
-	end)
+	return _table.map(
+		data.raw["technology"],
+		function(technology)
+			return technology.name
+		end
+	)
 end
 
 TechUtil.add_prerequisites_to_technology = function(technology_candidate, prerequisites, mode)
@@ -140,30 +163,26 @@ TechUtil.add_prerequisites_to_technology = function(technology_candidate, prereq
 	local technology = get_moded_technology(technology_candidate, mode)
 	local technology_candidate_name = technology_candidate.name
 	--проверяем на создание циклов.
-	_table.each(prerequisites, function(prerequisite_name)
-		if TechnologyTreeUtil.have_technology_in_tree(prerequisite_name, technology_candidate_name, mode) then
-			--[[local candidates = get_candidates_with_current_cycled_prerequisite_name_contains(
-				prerequisite_name,
-				technology_candidate_name,
-				mode
-			)]]
-			TechnologyTreeUtil.print_technology_tree(prerequisite_name, mode)
-			error(
-				"prerequisite_name "
-					.. prerequisite_name
-					.. " contains implicitly technology "
-					.. technology_candidate_name
-					.. " and can't be added to it prerequisites!! NO CYCLE IN TREE!!!"
-					.. Utils.dump_to_console(candidates)
-			)
+	_table.each(
+		prerequisites,
+		function(prerequisite_name)
+			if TechnologyTreeUtil.have_technology_in_tree(prerequisite_name, technology_candidate_name, mode) then
+				TechnologyTreeUtil.print_technology_tree(prerequisite_name, mode)
+				error(
+					"prerequisite_name " ..
+						prerequisite_name ..
+							" contains implicitly technology " ..
+								technology_candidate_name .. " and can't be added to it prerequisites!! NO CYCLE IN TREE!!!"
+				)
+			end
+			if not technology.prerequisites then
+				technology.prerequisites = {}
+			end
+			if not _table.contains(technology.prerequisites, prerequisite_name) then
+				table.insert(technology.prerequisites, prerequisite_name)
+			end
 		end
-		if not technology.prerequisites then
-			technology.prerequisites = {}
-		end
-		if not _table.contains(technology.prerequisites, prerequisite_name) then
-			table.insert(technology.prerequisites, prerequisite_name)
-		end
-	end)
+	)
 end
 TechUtil.remove_prerequisites_from_technology = function(technology_candidate, prerequisites, mode)
 	if not prerequisites then
@@ -173,9 +192,12 @@ TechUtil.remove_prerequisites_from_technology = function(technology_candidate, p
 	if not technology.prerequisites then
 		error("try to remove from not exists prerequisistes")
 	end
-	_table.each(prerequisites, function(prerequisite)
-		_table.remove_item(technology.prerequisites, prerequisite)
-	end)
+	_table.each(
+		prerequisites,
+		function(prerequisite)
+			_table.remove_item(technology.prerequisites, prerequisite)
+		end
+	)
 end
 
 TechUtil.reset_prerequisites_for_technology = function(technology_candidate, prerequisites, mode)
@@ -196,7 +218,7 @@ TechUtil.add_recipe_effect_to_technology = function(technology_candidate, recipe
 	if not technology.effects then
 		technology.effects = {}
 	end
-	_table.insert_all_if_not_exists(technology.effects, { { type = "unlock-recipe", recipe = recipe_name } })
+	_table.insert_all_if_not_exists(technology.effects, {{type = "unlock-recipe", recipe = recipe_name}})
 	local recipe = Utils.get_moded_object(data.raw.recipe[recipe_name], mode)
 	if not recipe then
 		error("recipe with name " .. recipe_name .. " not found!")
@@ -212,7 +234,7 @@ TechUtil.remove_recipe_effect_from_technology = function(technology_candidate, r
 	if not technology.effects then
 		error("technology effects not specified!")
 	end
-	_table.remove_item(technology.effects, { type = "unlock-recipe", recipe = recipe_name })
+	_table.remove_item(technology.effects, {type = "unlock-recipe", recipe = recipe_name})
 end
 
 local function add_science_pack_to_technology_units(technology_candidate, ingredient_value, mode)
@@ -224,11 +246,15 @@ local function add_science_pack_to_technology_units(technology_candidate, ingred
 		--log("technology.unit.ingredients " .. Utils.dump_to_console(technology.unit.ingredients))
 		_table.insert_all_if_not_exists_with_compare(
 			technology.unit.ingredients,
-			{ ingredient_value },
+			{ingredient_value},
 			function(__table, inserting_item)
-				local ingredient_names = _table.map(__table, function(item)
-					return item[1]
-				end)
+				local ingredient_names =
+					_table.map(
+					__table,
+					function(item)
+						return item[1]
+					end
+				)
 				--log("ingredient_names " .. Utils.dump_to_console(ingredient_names))
 				local inserting_ingrediend_name = inserting_item[1]
 				--log("inserting_ingredient_name " .. inserting_ingrediend_name)
@@ -243,18 +269,25 @@ TechUtil.add_science_packs_to_technology_units = function(technology_candidate, 
 	if not technology_units then
 		error("technology_units not specified")
 	end
-	_table.each(technology_units, function(technology_unit)
-		add_science_pack_to_technology_units(technology_candidate, technology_unit, mode)
-	end)
+	_table.each(
+		technology_units,
+		function(technology_unit)
+			add_science_pack_to_technology_units(technology_candidate, technology_unit, mode)
+		end
+	)
 end
 TechUtil.remove_science_pack_from_technology_units = function(technology_candidate, science_pack_name, mode)
 	if not science_pack_name then
 		error("science_pack_name not specified")
 	end
 	local technology = get_moded_technology(technology_candidate, mode)
-	_table.remove_item(technology.unit.ingredients, science_pack_name, function(table_item, item_for_remove)
-		return table_item[1] == item_for_remove
-	end)
+	_table.remove_item(
+		technology.unit.ingredients,
+		science_pack_name,
+		function(table_item, item_for_remove)
+			return table_item[1] == item_for_remove
+		end
+	)
 end
 
 TechUtil.hide_technology = function(technology_candidate, mode)
@@ -298,15 +331,15 @@ TechUtil.has_technology_recipe_effects = function(technology_candidate, recipe_n
 	if not technology.effects then
 		error("technology effects not specified!")
 	end
-	return _table.get_item_index(technology.effects, { type = "unlock-recipe", recipe = recipe_name }) ~= nil
+	return _table.get_item_index(technology.effects, {type = "unlock-recipe", recipe = recipe_name}) ~= nil
 end
 TechUtil.replace_all_occurs_prerequisite_to_another_in_active_technologies = function(from, to, mode)
 	local all_active_technology_names = TechUtil.get_all_active_technology_names(mode)
-	local with_deleting_prerequisite_technology_names = _table.filter(
+	local with_deleting_prerequisite_technology_names =
+		_table.filter(
 		all_active_technology_names,
 		function(all_active_technology_name)
-			local prerequisites =
-				Utils.get_moded_object(data.raw["technology"][all_active_technology_name], mode).prerequisites
+			local prerequisites = Utils.get_moded_object(data.raw["technology"][all_active_technology_name], mode).prerequisites
 			return prerequisites and _table.contains(prerequisites, from)
 		end
 	)
@@ -316,10 +349,13 @@ TechUtil.replace_all_occurs_prerequisite_to_another_in_active_technologies = fun
 			.. " for technology "
 			.. from
 	)]]
-	_table.each(with_deleting_prerequisite_technology_names, function(with_deleting_prerequisite_technology_name)
-		local technology = data.raw["technology"][with_deleting_prerequisite_technology_name]
-		TechUtil.remove_prerequisites_from_technology(technology, { from }, mode)
-		TechUtil.add_prerequisites_to_technology(technology, { to }, mode)
-	end)
+	_table.each(
+		with_deleting_prerequisite_technology_names,
+		function(with_deleting_prerequisite_technology_name)
+			local technology = data.raw["technology"][with_deleting_prerequisite_technology_name]
+			TechUtil.remove_prerequisites_from_technology(technology, {from}, mode)
+			TechUtil.add_prerequisites_to_technology(technology, {to}, mode)
+		end
+	)
 end
 return TechUtil
--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/mod_updating/junk-train-3.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/mod_updating/junk-train-3.lua
@@ -1,4 +1,4 @@
-local techUtil = require("__automated-utility-protocol__.util.technology-util")
+local tech_util = require("__automated-utility-protocol__.util.technology-util")
 
 local function update_junk_train3_mod(mode)
 	-- железнодорожные сигналы теперь зависят от простейшей автоматики, так же, как и железные дороги.
@@ -6,17 +6,17 @@ local function update_junk_train3_mod(mode)
 		return
 	end
 	local technologies = data.raw["technology"]
-	techUtil.reset_prerequisites_for_technology(
+	tech_util.reset_prerequisites_for_technology(
 		technologies["fluid-wagon"],
 		{ "JunkTrain_tech", "fluid-handling" },
 		mode
 	)
-	techUtil.add_prerequisites_to_technology(
+	tech_util.add_prerequisites_to_technology(
 		technologies["rail-signals"],
 		{ "automated-scrap-rail-transportation" },
 		mode
 	)
-	techUtil.add_prerequisites_to_technology(technologies["railway"], { "automated-scrap-rail-transportation" }, mode)
+	tech_util.add_prerequisites_to_technology(technologies["railway"], { "automated-scrap-rail-transportation" }, mode)
 end
 _table.each(GAME_MODES, function(mode)
 	update_junk_train3_mod(mode)
