529063902e6b426002ca4b32f2b9c666c5b0529d
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-05-26 15:25:36 +0300

    Химический завод теперь имеет третий вход(сбоку, начиная с уровня 2 для производства рецептов с трёмя жидкостями на входе). добавление категорий рецептов в химические заводы разрешаем ядерным печам выпускать 2 продукта для плавки рецептов вида (1,0)->(2,0)

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/entities.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/entities.lua
@@ -6,7 +6,7 @@ local coal_bi_bio_farm_entity = copy_entity("assembling-machine", "bi-bio-farm",
 coal_bi_bio_farm_entity.energy_source = {
 	type = "burner",
 	fuel_inventory_size = 5,
-	effectivity = 0.2,
+	effectivity = 0.2
 }
 coal_bi_bio_farm_entity.crafting_speed = 0.1
 
@@ -14,7 +14,7 @@ local coal_bi_bio_greenhouse_entity = copy_entity("assembling-machine", "bi-bio-
 coal_bi_bio_greenhouse_entity.energy_source = {
 	type = "burner",
 	fuel_inventory_size = 20,
-	effectivity = 0.15,
+	effectivity = 0.15
 }
 coal_bi_bio_greenhouse_entity.crafting_speed = 0.1
 
@@ -22,14 +22,14 @@ local salvaged_ofshore_pump_0_entity = copy_entity("offshore-pump", "offshore-pu
 salvaged_ofshore_pump_0_entity.energy_source = {
 	type = "burner",
 	fuel_inventory_size = 20,
-	effectivity = 0.15,
+	effectivity = 0.15
 }
 salvaged_ofshore_pump_0_entity.pumping_speed = 50
 
 coal_bi_bio_greenhouse_entity.energy_source = {
 	type = "burner",
 	fuel_inventory_size = 20,
-	effectivity = 0.15,
+	effectivity = 0.15
 }
 
 local coal_seedling_entity = flib.copy_prototype(data.raw["simple-entity-with-force"]["seedling"], "coal-seedling")
@@ -38,7 +38,7 @@ local salvaged_mining_drill_entity = copy_entity("mining-drill", "burner-mining-
 coal_bi_bio_greenhouse_entity.energy_source = {
 	type = "burner",
 	fuel_inventory_size = 20,
-	effectivity = 0.15,
+	effectivity = 0.15
 }
 
 salvaged_mining_drill_entity.mining_speed = 0.1
@@ -46,18 +46,56 @@ local salvaged_ore_crusher_entity = copy_entity("assembling-machine", "burner-or
 salvaged_ore_crusher_entity.energy_source = {
 	type = "burner",
 	fuel_inventory_size = 20,
-	effectivity = 0.15,
+	effectivity = 0.15
 }
 salvaged_ore_crusher_entity.crafting_speed = 0.1
 
-data:extend({
-	coal_bi_bio_farm_entity,
-	coal_bi_bio_greenhouse_entity,
-	coal_seedling_entity,
-	salvaged_ofshore_pump_0_entity,
-	salvaged_mining_drill_entity,
-	salvaged_ore_crusher_entity,
-})
+data:extend(
+	{
+		coal_bi_bio_farm_entity,
+		coal_bi_bio_greenhouse_entity,
+		coal_seedling_entity,
+		salvaged_ofshore_pump_0_entity,
+		salvaged_mining_drill_entity,
+		salvaged_ore_crusher_entity
+	}
+)
 -- для последних уровней бойлера ставим эффективностьв 0.9, вместо 1.15 и 1.25...
 data.raw["boiler"]["boiler-4"].energy_source.effectivity = 0.9
 data.raw["boiler"]["boiler-5"].energy_source.effectivity = 0.9
+-- разрешаем ядерным печам выпускать 2 продукта для плавки рецептов вида (1,0)->(2,0)
+if data.raw["furnace"]["nuclear-furnace"] then
+	data.raw["furnace"]["nuclear-furnace"].result_inventory_size = 2
+end
+-- добавление категорий рецептов в химические заводы
+if data.raw["assembling-machine"]["angels-chemical-plant"] then
+	data.raw["assembling-machine"]["angels-chemical-plant"].crafting_categories =
+		data.raw["assembling-machine"]["angels-chemical-plant"].crafting_categories or {}
+	table.insert(data.raw["assembling-machine"]["angels-chemical-plant"].crafting_categories, "liquifying")
+end
+if data.raw["assembling-machine"]["angels-chemical-plant-2"] then
+	data.raw["assembling-machine"]["angels-chemical-plant-2"].crafting_categories =
+		data.raw["assembling-machine"]["angels-chemical-plant-2"].crafting_categories or {}
+	table.insert(data.raw["assembling-machine"]["angels-chemical-plant-2"].crafting_categories, "liquifying")
+end
+if data.raw["assembling-machine"]["angels-chemical-plant-3"] then
+	data.raw["assembling-machine"]["angels-chemical-plant-3"].crafting_categories =
+		data.raw["assembling-machine"]["angels-chemical-plant-3"].crafting_categories or {}
+	table.insert(data.raw["assembling-machine"]["angels-chemical-plant-3"].crafting_categories, "petrochem-boiler")
+end
+
+--Химический завод теперь имеет третий вход(сбоку, начиная с уровня 2 для производства рецептов с трёмя жидкостями на входе)
+if data.raw.item["angels-chemical-plant"] then
+	for i = 2, 4 do
+		_table.insert(
+			data.raw["assembling-machine"]["angels-chemical-plant-" .. i].fluid_boxes,
+			{
+				production_type = "input",
+				pipe_covers = pipecoverspictures(),
+				base_area = 10,
+				base_level = -1,
+				pipe_connections = {{type = "input", position = {-2, 0}}}
+			}
+		)
+	end
+end
