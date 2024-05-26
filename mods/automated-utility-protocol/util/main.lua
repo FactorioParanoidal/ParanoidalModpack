0a4ffaba016694c59793ef9ae0f30b4cff3ed3ad
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-05-26 15:14:40 +0300

    Добавление проверки на то, что технология является технологией знакомства с ресурсом.

--- mods/automated-utility-protocol/util/main.lua
+++ mods/automated-utility-protocol/util/main.lua
@@ -1,41 +1,53 @@
 _table = require("__stdlib__/stdlib/utils/table")
 _string = require("__stdlib__/stdlib/utils/string")
-GAME_MODES = { "normal", "expensive" }
+GAME_MODES = {"normal", "expensive"}
 DETECTED_RESOURCE_TECHNOLOGY_SUFFIX = "-detected-resource-technology"
 Utils = {}
 
 _table.contains = function(__table, value)
-	return _table.find(__table, function(__table_item)
-		return __table_item == value
-	end) ~= nil
+	return _table.find(
+		__table,
+		function(__table_item)
+			return __table_item == value
+		end
+	) ~= nil
 end
 
 _table.contains_f = function(__table, value_function)
-	return _table.find(__table, function(__table_item)
-		return value_function(__table_item)
-	end) ~= nil
+	return _table.find(
+		__table,
+		function(__table_item)
+			return value_function(__table_item)
+		end
+	) ~= nil
 end
 
 _table.contains_f_deep = function(__table, item)
 	return _table.get_item_index(__table, item) ~= nil
 end
 _table.insert_all_if_not_exists = function(table1, table2)
-	_table.each(table2, function(item)
-		if not _table.contains_f_deep(table1, item) then
-			table.insert(table1, #table1 + 1, item)
+	_table.each(
+		table2,
+		function(item)
+			if not _table.contains_f_deep(table1, item) then
+				table.insert(table1, #table1 + 1, item)
+			end
 		end
-	end)
+	)
 end
 
 _table.insert_all_if_not_exists_with_compare = function(table1, table2, comparing_function)
 	if not comparing_function or type(comparing_function) ~= "function" then
 		error(tostring(comparing_function) .. " is not function!")
 	end
-	_table.each(table2, function(item)
-		if comparing_function(table1, item) then
-			table.insert(table1, #table1 + 1, item)
+	_table.each(
+		table2,
+		function(item)
+			if comparing_function(table1, item) then
+				table.insert(table1, #table1 + 1, item)
+			end
 		end
-	end)
+	)
 end
 _table.get_item_index = function(__table, item)
 	for k, v in pairs(__table) do
@@ -52,9 +64,15 @@ _table.remove_item = function(__table, item_for_remove, key_evaluate_function, a
 	local target_item = item_for_remove
 
 	if key_evaluate_function then
-		target_item = _table.first(_table.filter(__table, function(table_item)
-			return key_evaluate_function(table_item, item_for_remove)
-		end))
+		target_item =
+			_table.first(
+			_table.filter(
+				__table,
+				function(table_item)
+					return key_evaluate_function(table_item, item_for_remove)
+				end
+			)
+		)
 	end
 	local item_key = _table.get_item_index(__table, target_item)
 
@@ -64,11 +82,9 @@ _table.remove_item = function(__table, item_for_remove, key_evaluate_function, a
 	end
 	if not allow_key_missing then
 		error(
-			"Can't delete item "
-				.. Utils.dump_to_console(item_for_remove)
-				.. " with key "
-				.. Utils.dump_to_console(item_key)
-				.. " from given table!"
+			"Can't delete item " ..
+				Utils.dump_to_console(item_for_remove) ..
+					" with key " .. Utils.dump_to_console(item_key) .. " from given table" .. Utils.dump_to_console(__table) .. "!"
 		)
 	end
 end
@@ -101,6 +117,8 @@ Utils.get_moded_object = function(object, mode)
 end
 
 Utils.is_freeplay_scenario = function()
-	log("script.level " .. Utils.dump_to_console(script.level))
 	return script.level.level_name == "freeplay" and script.level.mod_name == "base"
+end
+function get_resource_detected_technology_name(resource_name)
+	return resource_name .. DETECTED_RESOURCE_TECHNOLOGY_SUFFIX
 end
