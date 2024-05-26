b77a2624b49d9edcadbf1800f97f6974b478cebf
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-05-26 15:22:50 +0300

    Исправление форматирования

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/items.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/items.lua
@@ -2,16 +2,18 @@ require("__hardcore-mode-for-playing__.prototypes.fuel-category.fuel-categories"
 local function copy_item(name, new_name)
 	return flib.copy_prototype(data.raw["item"][name], new_name)
 end
-data:extend({
-	copy_item("bi-bio-farm", "coal-bi-bio-farm"),
-	copy_item("bi-bio-greenhouse", "coal-bi-bio-greenhouse"),
-	copy_item("bi-seed", "coal-tree-seed"),
-	copy_item("seedling", "coal-seedling"),
-	copy_item("mining-drill-bit-mk0", "salvaged-mining-drill-bit-mk0"),
-	copy_item("offshore-pump-0", "salvaged-offshore-pump-0"),
-	copy_item("burner-mining-drill", "salvaged-mining-drill"),
-	copy_item("burner-ore-crusher", "salvaged-ore-crusher"),
-})
+data:extend(
+	{
+		copy_item("bi-bio-farm", "coal-bi-bio-farm"),
+		copy_item("bi-bio-greenhouse", "coal-bi-bio-greenhouse"),
+		copy_item("bi-seed", "coal-tree-seed"),
+		copy_item("seedling", "coal-seedling"),
+		copy_item("mining-drill-bit-mk0", "salvaged-mining-drill-bit-mk0"),
+		copy_item("offshore-pump-0", "salvaged-offshore-pump-0"),
+		copy_item("burner-mining-drill", "salvaged-mining-drill"),
+		copy_item("burner-ore-crusher", "salvaged-ore-crusher")
+	}
+)
 
 -- поделки из дерева, кроме стружки и чего-то там ещё - не горят, резина не горит
 -- электричка - не горит, тем более с лампами!
