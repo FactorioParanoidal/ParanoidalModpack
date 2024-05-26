ba17839ff3a97110495d0ec8a1eb0ef0d14a45f7
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-05-26 15:22:05 +0300

    Возарващение ранее удалённого в zzzparanoidal(и ангелболе) рецепта припоя дешёвого, чтобы иметь на старте какой-то вариант с припоем, так как он нужен для производства процессоров до исследования логистического пакета и технологий расплавов(angels-metallurgy-1 и коллег).

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/recipes.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/recipes.lua
@@ -16,19 +16,25 @@ end
 local coal_bi_bio_farm_recipe = flib.copy_prototype(data.raw["recipe"]["bi-bio-farm"], "coal-bi-bio-farm")
 
 local function fixBiBioFarmIngredients(ingredients)
-	table.insert(ingredients, {
-		type = "item",
-		name = "stone-furnace",
-		amount = 5,
-	})
-	_table.each(ingredients, function(ingredient)
-		if ingredient.name == "bi-bio-greenhouse" then
-			ingredient.name = "coal-bi-bio-greenhouse"
+	table.insert(
+		ingredients,
+		{
+			type = "item",
+			name = "stone-furnace",
+			amount = 5
+		}
+	)
+	_table.each(
+		ingredients,
+		function(ingredient)
+			if ingredient.name == "bi-bio-greenhouse" then
+				ingredient.name = "coal-bi-bio-greenhouse"
+			end
+			if ingredient.name == "glass" then
+				ingredient.name = "iron-plate"
+			end
 		end
-		if ingredient.name == "glass" then
-			ingredient.name = "iron-plate"
-		end
-	end)
+	)
 end
 fixBiBioFarmIngredients(coal_bi_bio_farm_recipe.normal.ingredients)
 fixBiBioFarmIngredients(coal_bi_bio_farm_recipe.expensive.ingredients)
@@ -44,16 +50,22 @@ local coal_bi_bio_greenhouse_recipe =
 	flib.copy_prototype(data.raw["recipe"]["bi-bio-greenhouse"], "coal-bi-bio-greenhouse")
 
 local function fixBiBioGreenHouseIngredients(ingredients)
-	table.insert(ingredients, {
-		type = "item",
-		name = "stone-furnace",
-		amount = 1,
-	})
-	_table.each(ingredients, function(ingredient)
-		if ingredient.name == "small-lamp" then
-			ingredient.name = "deadlock-copper-lamp"
+	table.insert(
+		ingredients,
+		{
+			type = "item",
+			name = "stone-furnace",
+			amount = 1
+		}
+	)
+	_table.each(
+		ingredients,
+		function(ingredient)
+			if ingredient.name == "small-lamp" then
+				ingredient.name = "deadlock-copper-lamp"
+			end
 		end
-	end)
+	)
 end
 
 local function fixBiBioGreenHouseResult(recipeData)
@@ -66,10 +78,12 @@ fixBiBioGreenHouseResult(coal_bi_bio_greenhouse_recipe.normal)
 fixBiBioGreenHouseResult(coal_bi_bio_greenhouse_recipe.expensive)
 
 -- скопипащенные рецепты.
-data:extend({
-	coal_bi_bio_farm_recipe,
-	coal_bi_bio_greenhouse_recipe,
-})
+data:extend(
+	{
+		coal_bi_bio_farm_recipe,
+		coal_bi_bio_greenhouse_recipe
+	}
+)
 
 local salvaged_offsore_pump_0_recipe = copy_salvaged_recipe("offshore-pump-0", "salvaged-offshore-pump-0")
 
@@ -77,47 +91,47 @@ salvaged_offsore_pump_0_recipe.ingredients = {
 	{
 		type = "item",
 		name = "wood",
-		amount = 40,
+		amount = 40
 	},
 	{
 		type = "item",
 		name = "salvaged-iron-gear-wheel",
-		amount = 8,
+		amount = 8
 	},
 	{
 		type = "item",
 		name = "bi-wood-pipe",
-		amount = 3,
-	},
+		amount = 3
+	}
 }
 
-data.raw["recipe"]["bi-wood-pipe"]["normal"].ingredients = { { "wood", 12 } }
-data.raw["recipe"]["bi-wood-pipe"]["expensive"].ingredients = { { "wood", 16 } }
-data.raw["recipe"]["bi-wood-pipe-to-ground"]["normal"].ingredients = { { "wood", 16 }, { "bi-wood-pipe", 5 } }
-data.raw["recipe"]["bi-wood-pipe-to-ground"]["expensive"].ingredients = { { "wood", 20 }, { "bi-wood-pipe", 6 } }
+data.raw["recipe"]["bi-wood-pipe"]["normal"].ingredients = {{"wood", 12}}
+data.raw["recipe"]["bi-wood-pipe"]["expensive"].ingredients = {{"wood", 16}}
+data.raw["recipe"]["bi-wood-pipe-to-ground"]["normal"].ingredients = {{"wood", 16}, {"bi-wood-pipe", 5}}
+data.raw["recipe"]["bi-wood-pipe-to-ground"]["expensive"].ingredients = {{"wood", 20}, {"bi-wood-pipe", 6}}
 local salvaged_ore_crusher_recipe = copy_salvaged_recipe("burner-ore-crusher", "salvaged-ore-crusher")
 salvaged_ore_crusher_recipe.ingredients = {
 	{
 		type = "item",
 		name = "wood",
-		amount = 120,
+		amount = 120
 	},
 	{
 		type = "item",
 		name = "salvaged-iron-gear-wheel",
-		amount = 16,
+		amount = 16
 	},
 	{
 		type = "item",
 		name = "salvaged-mining-drill-bit-mk0",
-		amount = 4,
+		amount = 4
 	},
 	-- для жидкостей
 	{
 		type = "item",
 		name = "bi-wood-pipe",
-		amount = 6,
-	},
+		amount = 6
+	}
 }
 local salvaged_mining_drill_bit_mk0_recipe =
 	copy_salvaged_recipe("mining-drill-bit-mk0", "salvaged-mining-drill-bit-mk0")
@@ -128,56 +142,56 @@ local basic_coal_production_wood_recipe = {
 		{
 			icon = "__zzzparanoidal__/graphics/Bio_Industries_graphics/graphics/icons/seedling.png",
 			icon_size = 64,
-			icon_mipmaps = 4,
+			icon_mipmaps = 4
 		},
 		{
 			icon = "__base__/graphics/icons/fluid/water.png",
 			icon_size = 64,
 			icon_mipmaps = 4,
 			scale = 0.25,
-			shift = { -8, 8 },
+			shift = {-8, 8}
 		},
 		{
 			icon = "__base__/graphics/icons/coal.png",
 			icon_size = 64,
 			icon_mipmaps = 4,
 			scale = 0.25,
-			shift = { 0, 8 },
-		},
+			shift = {0, 8}
+		}
 	},
 	ingredients = {
 		{
 			type = "fluid",
 			name = "water",
-			amount = 100,
+			amount = 100
 		},
 		{
 			type = "item",
 			name = "coal-seedling",
-			amount = 4,
+			amount = 4
 		},
 		{
 			type = "item",
 			name = "coal",
-			amount = 2,
-		},
+			amount = 2
+		}
 	},
 	results = {
 		{
 			type = "item",
 			name = "wood",
-			amount = 9,
+			amount = 9
 		},
 		{
 			type = "item",
 			name = "coal-tree-seed",
-			amount = 16,
-		},
+			amount = 16
+		}
 	},
 	subgroup = "bio-bio-farm-fluid-1",
 	category = "biofarm-mod-greenhouse",
 	energy_required = 10,
-	enabled = false,
+	enabled = false
 }
 local basic_coal_production_seedling_recipe = {
 	type = "recipe",
@@ -186,104 +200,118 @@ local basic_coal_production_seedling_recipe = {
 		{
 			icon = "__zzzparanoidal__/graphics/Bio_Industries_graphics/graphics/icons/tree_seed.png",
 			icon_size = 64,
-			icon_mipmaps = 4,
+			icon_mipmaps = 4
 		},
 		{
 			icon = "__base__/graphics/icons/fluid/water.png",
 			icon_size = 64,
 			icon_mipmaps = 4,
 			scale = 0.25,
-			shift = { -8, 8 },
+			shift = {-8, 8}
 		},
 		{
 			icon = "__base__/graphics/icons/coal.png",
 			icon_size = 64,
 			icon_mipmaps = 4,
 			scale = 0.25,
-			shift = { 0, 8 },
-		},
+			shift = {0, 8}
+		}
 	},
 	ingredients = {
 		{
 			type = "fluid",
 			name = "water",
-			amount = 100,
+			amount = 100
 		},
 		{
 			type = "item",
 			name = "coal-tree-seed",
-			amount = 4,
+			amount = 4
 		},
 		{
 			type = "item",
 			name = "coal",
-			amount = 2,
-		},
+			amount = 2
+		}
 	},
 	results = {
 		{
 			type = "item",
 			name = "coal-seedling",
-			amount = 1,
-		},
+			amount = 1
+		}
 	},
 	subgroup = "bio-bio-farm-fluid-1",
 	category = "biofarm-mod-greenhouse",
 	energy_required = 25,
-	enabled = false,
+	enabled = false
 }
-data:extend({
-	salvaged_mining_drill_bit_mk0_recipe,
-	salvaged_offsore_pump_0_recipe,
-	salvaged_ore_crusher_recipe,
-	basic_coal_production_wood_recipe,
-	basic_coal_production_seedling_recipe,
-})
+data:extend(
+	{
+		salvaged_mining_drill_bit_mk0_recipe,
+		salvaged_offsore_pump_0_recipe,
+		salvaged_ore_crusher_recipe,
+		basic_coal_production_wood_recipe,
+		basic_coal_production_seedling_recipe
+	}
+)
 local salvaged_mining_drill_recipe = data.raw["recipe"]["salvaged-mining-drill"]
 salvaged_mining_drill_recipe.ingredients = {
 	{
 		type = "item",
 		name = "salvaged-mining-drill-bit-mk0",
-		amount = 2,
+		amount = 2
 	},
 	-- для жидкостей
 	{
 		type = "item",
 		name = "bi-wood-pipe",
-		amount = 6,
-	},
+		amount = 6
+	}
 }
 
-_table.each(GAME_MODES, function(mode)
-	merge_recipe_for_modes(salvaged_offsore_pump_0_recipe, mode)
-	merge_recipe_for_modes(salvaged_ore_crusher_recipe, mode)
-	merge_recipe_for_modes(salvaged_mining_drill_bit_mk0_recipe, mode)
-	merge_recipe_for_modes(basic_coal_production_wood_recipe, mode)
-	merge_recipe_for_modes(basic_coal_production_seedling_recipe, mode)
-	merge_recipe_for_modes(salvaged_mining_drill_recipe, mode)
-end)
+_table.each(
+	GAME_MODES,
+	function(mode)
+		merge_recipe_for_modes(salvaged_offsore_pump_0_recipe, mode)
+		merge_recipe_for_modes(salvaged_ore_crusher_recipe, mode)
+		merge_recipe_for_modes(salvaged_mining_drill_bit_mk0_recipe, mode)
+		merge_recipe_for_modes(basic_coal_production_wood_recipe, mode)
+		merge_recipe_for_modes(basic_coal_production_seedling_recipe, mode)
+		merge_recipe_for_modes(salvaged_mining_drill_recipe, mode)
+	end
+)
 -- рецепты, которые описывают использование предметов в машинах или реакторах
 local riteg1_to_used_up_riteg1_recipe_data = {
 	enabled = false,
 	energy_required = 30,
 	ingredients = {
-		{ "RITEG-1", 1 },
+		{"RITEG-1", 1}
 	},
-	result = "used-up-RITEG-1",
+	result = "used-up-RITEG-1"
 }
 
-data:extend({
+data:extend(
 	{
-		normal = riteg1_to_used_up_riteg1_recipe_data,
-		expensive = riteg1_to_used_up_riteg1_recipe_data,
-		icons = {
-			{ icon = "__RITEG__/graphics/icons/RITEG-1.png" },
-			{ icon = "__RITEG__/graphics/icons/recycling.png", scale = 0.5, shift = { 8, 8 } },
-		},
-		icon_size = 32,
-
-		name = "used-up-RITEG-1",
-		type = "recipe",
-	},
-})
+		{
+			normal = riteg1_to_used_up_riteg1_recipe_data,
+			expensive = riteg1_to_used_up_riteg1_recipe_data,
+			icons = {
+				{icon = "__RITEG__/graphics/icons/RITEG-1.png"},
+				{icon = "__RITEG__/graphics/icons/recycling.png", scale = 0.5, shift = {8, 8}}
+			},
+			icon_size = 32,
+			name = "used-up-RITEG-1",
+			type = "recipe"
+		}
+	}
+)
 --конец рецепты, которые описывают использование предметов в машинах или реакторах
+-- возвращаем дешёвый вариант производства припоя.
+local angels_solder_mixture_smelting_recipe = data.raw["recipe"]["angels-solder-mixture-smelting"]
+if angels_solder_mixture_smelting_recipe then
+	local angels_solder_mixture_smelting_recipe_results = {{type = "item", name = "solder", amount = 2}}
+
+	angels_solder_mixture_smelting_recipe.normal.results = angels_solder_mixture_smelting_recipe_results
+	angels_solder_mixture_smelting_recipe.normal.results = angels_solder_mixture_smelting_recipe_results
+end
