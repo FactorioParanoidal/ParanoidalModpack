86a1a5824173cf77ed1548c58a8f673a0cff9081
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-05-26 15:58:12 +0300

    Добавление следующих настроек 1. hardcore-mode-for-playing-move-basic-recipe-to-new-basic-technology-hard-start - отвечает за перенос рецептов в базовые технологии и блокирование этих технологий с начала новой игры(ни один рецепт не может быть бесплатным) 2. hardcore-mode-for-playing-use-separated-boilers-for-every-fuel - каждый котёл и каждый бойлер будут созданы под свой вид топлива(вместе с рецептами конвертации одного в другой, например, на одном уровне технологий) 3. hardcore-mode-for-playing-use-separated-technologies-for-every-resource - каждый ресурс выделен в отдельную технологию, чтобы начать исследовать что-то  в принципе, с данными ресурсами требуется познакомиться с помощью радара(находит все ископаемые ресурсы и воду), либо собрать их, в случае добываемых ресурсов(например, инопланетные материалы) 4. добавление префикса hardcore-mode-for-playing для остальных настроек.

--- mods/hardcore-mode-for-playing/settings.lua
+++ mods/hardcore-mode-for-playing/settings.lua
@@ -1,32 +1,44 @@
 data:extend({
 	{
 		type = "bool-setting",
-		name = "disable-hand-resource-mining",
+		name = "hardcore-mode-for-playing-disable-hand-resource-mining",
 		setting_type = "runtime-global",
-		default_value = false,
+		default_value = true,
 	},
 	{
 		type = "bool-setting",
-		name = "disable-hand-crafting",
+		name = "hardcore-mode-for-playing-disable-hand-crafting",
 		setting_type = "runtime-global",
-		default_value = false,
+		default_value = true,
 	},
 	{
 		type = "bool-setting",
-		name = "enable-technology-research-reevaluting",
+		name = "hardcore-mode-for-playing-enable-technology-research-reevaluting",
 		setting_type = "runtime-global",
-		default_value = false,
+		default_value = true,
 	},
 	{
 		type = "bool-setting",
-		name = "disable-production-entities-beyond-factorissimo-building",
+		name = "hardcore-mode-for-playing-disable-production-entities-beyond-factorissimo-building",
 		setting_type = "runtime-global",
-		default_value = false,
-	} --[[,
+		default_value = true,
+	},
 	{
 		type = "bool-setting",
-		name = "move-basic-recipe-to-new-basic-technology-hard-start",
+		name = "hardcore-mode-for-playing-move-basic-recipe-to-new-basic-technology-hard-start",
 		setting_type = "startup",
-		default_value = false,
-	},]],
+		default_value = true,
+	},
+	{
+		type = "bool-setting",
+		name = "hardcore-mode-for-playing-use-separated-boilers-for-every-fuel",
+		setting_type = "startup",
+		default_value = true,
+	},
+	{
+		type = "bool-setting",
+		name = "hardcore-mode-for-playing-use-separated-technologies-for-every-resource",
+		setting_type = "startup",
+		default_value = true,
+	}
 })
