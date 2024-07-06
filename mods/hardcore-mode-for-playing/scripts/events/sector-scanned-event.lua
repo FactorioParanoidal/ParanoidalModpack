10050b9c112e5e0a03b2f0a81be95b6c8399f4ee
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-07-06 20:37:51 +0300

    добавлены условия для исследования деревьев и камней, как не являющиеся ресурсными клетками элементы(подсчёт количестов клеток с горами и деревьями любых типов).

--- mods/hardcore-mode-for-playing/scripts/events/sector-scanned-event.lua
+++ mods/hardcore-mode-for-playing/scripts/events/sector-scanned-event.lua
@@ -1,10 +1,10 @@
 require("__automated-utility-protocol__.util.main")
 
 
-local function find_all_resources_in_or_at_ground(area_for_looking_up)
+local function find_all_resources_in_or_at_ground(area_for_looking_up, radar_surface)
     local result = {}
     -- все клетки с ресурсами, которые закопаны в земле(руды, жидкости, газы, что угодно)
-    local found_resouces_entities = _table.map(
+    local found_resources_entities = _table.map(
         radar_surface.find_entities_filtered { type = "resource", area = area_for_looking_up },
         function(resource)
             return {
@@ -12,6 +12,22 @@ local function find_all_resources_in_or_at_ground(area_for_looking_up)
                 name = resource.name
             }
         end)
+    -- если найдены деревья - значит есть дерево на планете!
+    local tree_count = radar_surface.count_entities_filtered { type = "tree", area = area_for_looking_up }
+    if tree_count > 0 then
+        _table.insert_all_if_not_exists(result, { {
+            type = "resource",
+            name = "wood"
+        } })
+    end
+    -- если найдены камни - значит есть камень на планете!
+    local cliff_count = radar_surface.count_entities_filtered { type = "cliff", area = area_for_looking_up }
+    if cliff_count > 0 then
+        _table.insert_all_if_not_exists(result, { {
+            type = "resource",
+            name = "stone"
+        } })
+    end
     --клетки, содержащие воду(ресурсом формально не является)
     local tile_entities = _table.map(
         radar_surface.find_tiles_filtered { area = area_for_looking_up, name = "water" },
@@ -21,7 +37,7 @@ local function find_all_resources_in_or_at_ground(area_for_looking_up)
                 name = tile.name
             }
         end)
-    _table.insert_all_if_not_exists(result, found_resouces_entities)
+    _table.insert_all_if_not_exists(result, found_resources_entities)
     _table.insert_all_if_not_exists(result, tile_entities)
     return result
 end
@@ -32,9 +48,10 @@ function on_sector_scanned(e)
     log('area ' .. Utils.dump_to_console(radar_area))
     log('chunk_position ' .. Utils.dump_to_console(e.chunk_position))
     local radar_force = radar.force
-    local distinct_found_resouces = find_all_resources_in_or_at_ground(radar_area)
-    research_technologies_for_resources_if_exists_not_researched(distinct_found_resouces, radar_force,
+    local radar_surface = radar.surface
+    local distinct_found_resources = find_all_resources_in_or_at_ground(radar_area, radar_surface)
+    research_technologies_for_resources_if_exists_not_researched(distinct_found_resources, radar_force,
         "На планете на поверхности " ..
         radar_surface.name .. " найден новый ресурс '", "' в области с координатами " ..
-        Utils.dump_to_console(area) .. " игроком ")
+        Utils.dump_to_console(radar_area) .. " игроком ")
 end
