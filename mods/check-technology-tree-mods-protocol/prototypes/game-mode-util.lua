789a55349b03e4f56cf8aa156de08d262e71cc6c
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-07-06 20:26:43 +0300

    Добавлена проверка на используемость всех результатов всех доступных к исследованию рецептов. Исправлено нзвание функции

--- mods/check-technology-tree-mods-protocol/prototypes/game-mode-util.lua
+++ mods/check-technology-tree-mods-protocol/prototypes/game-mode-util.lua
@@ -4,11 +4,11 @@ local technology_handler = require("technology-handler")
 local GameModeUtil = {}
 
 local function handle_game_mode_data(mode)
-	technology_handler.handle_techonologies(mode)
+    technology_handler.handle_technologies(mode)
 end
 
 GameModeUtil.handle_game_mode_datas = function()
-	_table.each(GAME_MODES, handle_game_mode_data)
+    _table.each(GAME_MODES, handle_game_mode_data)
 end
 
 return GameModeUtil
