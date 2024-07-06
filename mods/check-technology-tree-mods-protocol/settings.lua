e841948ef4a8b4765a84066a61f17721b6177813
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-07-06 20:27:19 +0300

    check-technology-tree-mods-protocol-use-recipe-result-as-ingredient-or-placeable-item-validating новая настройка для включения и отключения ранее введённой проверки

--- mods/check-technology-tree-mods-protocol/settings.lua
+++ mods/check-technology-tree-mods-protocol/settings.lua
@@ -1,14 +1,20 @@
 data:extend({
-	{
-		type = "bool-setting",
-		name = "check-technology-tree-mods-protocol-use-recipe-ingredient-reacheable-validating",
-		setting_type = "startup",
-		default_value = true,
-	},
-	{
-		type = "bool-setting",
-		name = "check-technology-tree-mods-protocol-use-recipe-signature-in-machine-automated-validating",
-		setting_type = "startup",
-		default_value = true,
-	},
+    {
+        type = "bool-setting",
+        name = "check-technology-tree-mods-protocol-use-recipe-ingredient-reacheable-validating",
+        setting_type = "startup",
+        default_value = true,
+    },
+    {
+        type = "bool-setting",
+        name = "check-technology-tree-mods-protocol-use-recipe-signature-in-machine-automated-validating",
+        setting_type = "startup",
+        default_value = true,
+    },
+    {
+        type = "bool-setting",
+        name = "check-technology-tree-mods-protocol-use-recipe-result-as-ingredient-or-placeable-item-validating",
+        setting_type = "startup",
+        default_value = true,
+    }
 })
