53549d0d51c7f0deadd07f5bdeae3e8e29d75df6
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-07-14 21:36:11 +0300

    устранение жёсткой связанности с P-U-M-P-S

--- mods/zzzparanoidal/prototypes/landfill-pump.lua
+++ mods/zzzparanoidal/prototypes/landfill-pump.lua
@@ -1,30 +1,39 @@
+if not mods["P-U-M-P-S"] then return end
 -- Начало мода на помпы Pumps on Landfill
-for _,tile in pairs(data.raw.tile) do
-	if(tile.name=="landfill" or tile.water_contained) then
-		table.insert(tile.collision_mask,water_contained_layer);
-	end
+for _, tile in pairs(data.raw.tile) do
+    if (tile.name == "landfill" or tile.water_contained) then
+        table.insert(tile.collision_mask, water_contained_layer);
+    end
 end
 
 local collision_mask_util = require("collision-mask-util")
 local layer = collision_mask_util.get_first_unused_layer()
 collision_mask_util.add_layer(data.raw.tile["landfill"].collision_mask, layer)
-collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-0-placeholder"].adjacent_tile_collision_test, layer)
+collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-0-placeholder"].adjacent_tile_collision_test,
+    layer)
 data.raw["offshore-pump"]["offshore-pump-0-placeholder"].adjacent_tile_collision_mask = nil
-collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-1-placeholder"].adjacent_tile_collision_test, layer)
+collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-1-placeholder"].adjacent_tile_collision_test,
+    layer)
 data.raw["offshore-pump"]["offshore-pump-1-placeholder"].adjacent_tile_collision_mask = nil
-collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-2-placeholder"].adjacent_tile_collision_test, layer)
+collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-2-placeholder"].adjacent_tile_collision_test,
+    layer)
 data.raw["offshore-pump"]["offshore-pump-2-placeholder"].adjacent_tile_collision_mask = nil
-collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-3-placeholder"].adjacent_tile_collision_test, layer)
+collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-3-placeholder"].adjacent_tile_collision_test,
+    layer)
 data.raw["offshore-pump"]["offshore-pump-3-placeholder"].adjacent_tile_collision_mask = nil
-collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-4-placeholder"].adjacent_tile_collision_test, layer)
+collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-4-placeholder"].adjacent_tile_collision_test,
+    layer)
 data.raw["offshore-pump"]["offshore-pump-4-placeholder"].adjacent_tile_collision_mask = nil
-collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-4-placeholder"].adjacent_tile_collision_test, layer)
+collision_mask_util.add_layer(data.raw["offshore-pump"]["offshore-pump-4-placeholder"].adjacent_tile_collision_test,
+    layer)
 data.raw["offshore-pump"]["offshore-pump-4-placeholder"].adjacent_tile_collision_mask = nil
 collision_mask_util.add_layer(data.raw["offshore-pump"]["seafloor-pump-placeholder"].adjacent_tile_collision_test, layer)
 data.raw["offshore-pump"]["seafloor-pump-placeholder"].adjacent_tile_collision_mask = nil
-collision_mask_util.add_layer(data.raw["offshore-pump"]["seafloor-pump-2-placeholder"].adjacent_tile_collision_test, layer)
+collision_mask_util.add_layer(data.raw["offshore-pump"]["seafloor-pump-2-placeholder"].adjacent_tile_collision_test,
+    layer)
 data.raw["offshore-pump"]["seafloor-pump-2-placeholder"].adjacent_tile_collision_mask = nil
-collision_mask_util.add_layer(data.raw["offshore-pump"]["seafloor-pump-3-placeholder"].adjacent_tile_collision_test, layer)
+collision_mask_util.add_layer(data.raw["offshore-pump"]["seafloor-pump-3-placeholder"].adjacent_tile_collision_test,
+    layer)
 data.raw["offshore-pump"]["seafloor-pump-3-placeholder"].adjacent_tile_collision_mask = nil
 
 -- Конец мода на помпы Pumps on Landfill
