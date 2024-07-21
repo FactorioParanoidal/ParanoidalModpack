6439433fc17c84133ef0ff895ae64263e61d1589
SEO <32990223+ExpertLove@users.noreply.github.com>
2024-07-21 16:42:34 +0300

    Ремонт дерева исследований (#84)

--- mods/zzzparanoidal/prototypes/micro-final-fix.lua
+++ mods/zzzparanoidal/prototypes/micro-final-fix.lua
@@ -1283,6 +1283,23 @@ bobmods.lib.tech.add_prerequisite ("bi-tech-garden-3", "production-science-pack"
 bobmods.lib.tech.add_prerequisite ("angels-metallurgy-4", "advanced-electronics-2")  -- Промышленная металлургия 4 под Продвинутую электронику 2 
 bobmods.lib.tech.add_prerequisite ("strand-casting-1", "angels-stone-smelting-1")  -- Машины непрерывного литья 1 под кирпичи 1
 bobmods.lib.tech.add_prerequisite ("angels-brass-smelting-1", "ore-floatation")  -- Латунь под Гидропромывку 1
+--21.07.24
+bobmods.lib.tech.add_prerequisite ("angels-metallurgy-3", "ore-leaching")  -- Промышленную металлургию 3 под Химическую обработку руды(кристаллы)
+bobmods.lib.tech.add_prerequisite ("modules", "angels-gold-smelting-1") -- Модули под Золото! 
+data.raw.technology["modules"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}} --модули теперь за синие банки (как и должно быть)
+bobmods.lib.tech.add_prerequisite ("OilBurning-2", "bob-boiler-2") -- Сжигание жидкого и газообразного топлива 2 под Бойлер 2
+bobmods.lib.tech.add_prerequisite ("OilBurning-3", "bob-boiler-3") -- Сжигание жидкого и газообразного топлива 3 под Бойлер 3
+bobmods.lib.tech.add_prerequisite ("OilBurning-4", "bob-boiler-4") -- Сжигание жидкого и газообразного топлива 4 под Бойлер 4
+bobmods.lib.tech.add_prerequisite ("OilBurning-5", "bob-boiler-5") -- Сжигание жидкого и газообразного топлива 5 под Бойлер 5
+data.raw.technology["bob-drills-2"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}} --синие банки для буров мк3
+data.raw.technology["bob-area-drills-2"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}} --синие банки для буров мк3
+bobmods.lib.tech.add_prerequisite ("energy-weapons-damage-4", "chemical-science-pack") -- Урон энергетического оружия под правильные банки
+bobmods.lib.tech.add_prerequisite ("energy-weapons-damage-5", "utility-science-pack") -- Урон энергетического оружия под правильные банки
+bobmods.lib.tech.add_prerequisite ("ober-nuclear-processing", "utility-science-pack") -- Высокотемпературная переработка сырья под правильные банки
+bobmods.lib.tech.add_prerequisite ("refined-flammables-4", "utility-science-pack") -- Высокотемпературная переработка сырья под правильные банки
+bobmods.lib.tech.add_prerequisite ("advanced-uranium-processing-1", "utility-science-pack") -- Продвинутая переработка урана 1 под правильные банки
+data.raw.technology["warehouse-logistics-research-2"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"advanced-logistic-science-pack", 1} } --Складская логистика 2 делаем правильные банки
+bobmods.lib.recipe.set_ingredients("yellow-filter-inserter", { { "electric-motor", 2 }, { "burner-filter-inserter", 1 }, { "electronic-circuit", 4 } }) --в рецепт к фильтрующему добавляем фильтрующий твердотопливный
 
 bobmods.lib.tech.remove_recipe_unlock("optics", "deadlock-large-lamp") --лампы
 bobmods.lib.tech.remove_recipe_unlock("optics", "deadlock-floor-lamp") --лампы
