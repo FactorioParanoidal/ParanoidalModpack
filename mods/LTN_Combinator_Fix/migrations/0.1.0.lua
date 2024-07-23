c874744db68d814f89b2898d06fce4bc9da3a9e3
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-07-23 20:55:05 +0300

    Удалены моды LTN

--- mods/LTN_Combinator_Fix/migrations/0.1.0.lua
+++
@@ -1,5 +0,0 @@
-for index, force in pairs(game.forces) do
-  if force.technologies["logistic-train-network"] ~= nil and force.technologies["logistic-train-network"].researched then
-    force.recipes["ltn-combinator"].enabled = true
-  end
-end
