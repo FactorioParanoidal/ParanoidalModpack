240ec2fbc98071296ce6fbfe7425f8ea48062432
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-08-23 23:38:46 +0300

    Удалён ненужный выбор, ограничение скорости будет назначаться только из логической сети

--- mods/SpeedLimitSignsForTrains/control.lua
+++ mods/SpeedLimitSignsForTrains/control.lua
@@ -20,10 +20,10 @@ function onConfigurationChanged(data)
             v.recipes["electronic-train-unlimit"].enabled = true
         end
     end
-    
+
     -- reset our globals for safety
     local our_mod = data.mod_changes["SpeedLimitSignsForTrains"]
-    
+
     if our_mod and our_mod.old_version ~= our_mod.new_version then
         global.trains_lookup = {}
         global.last_built_sign = {}
@@ -34,7 +34,7 @@ script.on_init(onInit)
 script.on_configuration_changed(onConfigurationChanged)
 
 function getBoundingBox(position, radius)
-    return {{position.x - radius, position.y - radius}, {position.x + radius, position.y + radius}}
+    return { { position.x - radius, position.y - radius }, { position.x + radius, position.y + radius } }
 end
 
 function isTrainArriving(train)
@@ -44,7 +44,8 @@ end
 function shouldBrakeTrain(train, allowed_speed)
     -- for train braking before station or signal we're adding additional ~10 km/h to the target speed
     -- this way it will not roll slowly for a half of minute before reaching the station/signal
-    return not train.manual_mode and ((not isTrainArriving(train) and math.abs(train.speed) >= allowed_speed) or (isTrainArriving(train) and math.abs(train.speed) >= allowed_speed + 0.05))
+    return not train.manual_mode and
+        ((not isTrainArriving(train) and math.abs(train.speed) >= allowed_speed) or (isTrainArriving(train) and math.abs(train.speed) >= allowed_speed + 0.05))
 end
 
 function calcDeceleration(train)
@@ -54,18 +55,18 @@ function calcDeceleration(train)
     if isTrainArriving(train) then
         factor = 0.75
     end
-	
-	factor = factor * 10.0 -- line added to decellerate much quicker
+
+    factor = factor * 10.0 -- line added to decellerate much quicker
 
     if abs_speed > 0.6 then
-        factor = 0.005*factor
+        factor = 0.005 * factor
     elseif abs_speed > 0.3 then
-        factor =  0.0075*factor
+        factor = 0.0075 * factor
     else
-        factor = 0.01*factor
+        factor = 0.01 * factor
     end
-	
-	return math.min(1, factor) -- line added to avoid factors > 1 which would turn around the train
+
+    return math.min(1, factor) -- line added to avoid factors > 1 which would turn around the train
 end
 
 function calcTickSpeed(speed)
@@ -89,36 +90,36 @@ function processSignsForTrain(twrap)
         return
     end
 
-    lst = loco.surface.find_entities_filtered{area = getBoundingBox(loco.position, 2), type = "constant-combinator"}
+    lst = loco.surface.find_entities_filtered { area = getBoundingBox(loco.position, 2), type = "constant-combinator" }
     for i, ent in pairs(lst) do
         if ent.name == "placed-train-limit" and isOnLeft(ent.position, loco.position, getCardinal(loco.orientation)) then
             local limit_value = getLimitIndication(ent)
-            
+
             if limit_value and limit_value >= 5 and limit_value <= 500 then
                 twrap.limit = calcTickSpeed(limit_value)
             end
-            
+
             return
         elseif ent.name == "placed-train-unlimit" and isOnLeft(ent.position, loco.position, getCardinal(loco.orientation)) then
-            local circuit_signal = readCircuitSignal(ent, {type = "item", name = "train-unlimit"})
-            
+            local circuit_signal = readCircuitSignal(ent, { type = "item", name = "train-unlimit" })
+
             if circuit_signal == nil or circuit_signal > 0 then
                 twrap.limit = false
             end
-            
+
             return
         end
     end
 
     local area = getBoundingBox(loco.position, twrap.train.speed * 10 + 1)
 
-    if loco.surface.count_entities_filtered{area = area, type = "constant-combinator", count = 1} == 0 then
+    if loco.surface.count_entities_filtered { area = area, type = "constant-combinator", count = 1 } == 0 then
         twrap.deactivation = 6
     end
 end
 
 script.on_event(defines.events.on_tick, function(event)
-    for i=#global.trains_lookup,1,-1 do
+    for i = #global.trains_lookup, 1, -1 do
         local twrap = global.trains_lookup[i]
 
         if not twrap.train.valid then
@@ -128,7 +129,7 @@ script.on_event(defines.events.on_tick, function(event)
             if twrap.deactivation == 0 and twrap.train.speed ~= 0 then
                 processSignsForTrain(twrap)
             elseif twrap.deactivation > 0 then
-                 twrap.deactivation = twrap.deactivation - 1
+                twrap.deactivation = twrap.deactivation - 1
             elseif twrap.deactivation < 0 then
                 twrap.deactivation = 0
             end
@@ -136,19 +137,19 @@ script.on_event(defines.events.on_tick, function(event)
             -- decelerate the train if needed
             if twrap.limit and twrap.train.speed ~= 0 and shouldBrakeTrain(twrap.train, twrap.limit) then
                 local decel = calcDeceleration(twrap.train)
-				
+
                 if twrap.train.speed > 0 then
-					if (twrap.train.speed - decel) > 0 then
-						twrap.train.speed = math.max(twrap.train.speed - decel, twrap.limit)
-					else
-						twrap.train.speed = twrap.limit
-					end
+                    if (twrap.train.speed - decel) > 0 then
+                        twrap.train.speed = math.max(twrap.train.speed - decel, twrap.limit)
+                    else
+                        twrap.train.speed = twrap.limit
+                    end
                 else
-					if (twrap.train.speed + decel) < 0 then
-						twrap.train.speed = math.min(twrap.train.speed + decel, -twrap.limit)
-					else
-						twrap.train.speed = -twrap.limit
-					end
+                    if (twrap.train.speed + decel) < 0 then
+                        twrap.train.speed = math.min(twrap.train.speed + decel, -twrap.limit)
+                    else
+                        twrap.train.speed = -twrap.limit
+                    end
                 end
             end
         end
@@ -163,24 +164,24 @@ function onTrainState(event)
             if v.train.manual_mode then
                 v.limit = false
             end
-            
+
             return
         end
     end
 
     local front_loco = nil
     local back_loco = nil
-    
+
     if #event.train.locomotives.front_movers > 0 then
         front_loco = event.train.locomotives.front_movers[1]
     end
-    
+
     if #event.train.locomotives.back_movers > 0 then
         back_loco = event.train.locomotives.back_movers[#event.train.locomotives.back_movers]
     end
-    
+
     table.insert(global.trains_lookup, {
-        train=event.train, deactivation=0, limit=false, front_loco=front_loco, back_loco=back_loco
+        train = event.train, deactivation = 0, limit = false, front_loco = front_loco, back_loco = back_loco
     })
 end
 
@@ -189,11 +190,11 @@ function readCircuitSignal(entity, signal_type)
 
     local red_network = entity.get_circuit_network(defines.wire_type.red)
     local green_network = entity.get_circuit_network(defines.wire_type.green)
-   
+
     if not red_network and not green_network then
         return nil
     end
-    
+
     if red_network then
         sign_value = sign_value + red_network.get_signal(signal_type)
     end
@@ -207,11 +208,11 @@ end
 
 function getLimitIndication(entity, red_network, green_network)
     local sign_value = entity.get_or_create_control_behavior().get_signal(1).count
-    
+
     if sign_value ~= nil and sign_value ~= 0 then
         return sign_value
     else
-        return readCircuitSignal(entity, {type = "item", name = "train-limit"})
+        return readCircuitSignal(entity, { type = "item", name = "train-limit" })
     end
 end
 
@@ -219,23 +220,30 @@ function setLimitIndication(entity, value)
     if not entity or not entity.valid then
         return false
     end
-    
-    entity.get_or_create_control_behavior().set_signal(1, 
-        {count = value, signal = {type = "item", name = "train-limit"}})
-    
+
+    entity.get_or_create_control_behavior().set_signal(1,
+        { count = value, signal = { type = "item", name = "train-limit" } })
+
     return true
 end
 
 function onBuiltEntity(event)
     local player = nil
-    
+
     if event.player_index then
         player = game.players[event.player_index]
     end
-
-    if event.created_entity.name == "placed-train-limit" then
+    if event.created_entity.name == "placed-train-limit" or event.created_entity.name == "placed-train-unlimit" then
         event.created_entity.operable = false
-        
+        if not setLimitIndication(event.created_entity, 0) then
+            if player then
+                player.print({ "gui-set-failed" })
+            end
+        end
+    end
+    --[[  then
+
+
         if player then
             setLimitIndication(event.created_entity, 20)
 
@@ -257,30 +265,30 @@ function onBuiltEntity(event)
         end
     elseif event.created_entity.name == "placed-train-unlimit" then
         event.created_entity.operable = false
-    end
+    end]]
 end
 
-function onGuiClick(event)
+--[[function onGuiClick(event)
     local player = game.players[event.player_index]
-	local frame = player.gui.center["tsl-value"]
+    local frame = player.gui.center["tsl-value"]
     local manual_frame = player.gui.center["tsl-manual-value"]
-    
+
     if manual_frame and event.element.parent == manual_frame then
         if event.element.name == "tsl-manual-ok" then
             local limit_value = tonumber(manual_frame["tsl-manual-input"].text)
-            
+
             if not limit_value or limit_value < 5 or limit_value > 500 or limit_value % 1 ~= 0 then
                 manual_frame["tsl-manual-input"].text = ""
-                player.print({"gui-invalid-limit"})
+                player.print({ "gui-invalid-limit" })
                 return
             end
 
             if not setLimitIndication(global.last_built_sign[event.player_index], limit_value) then
-                player.print({"gui-set-failed"})
+                player.print({ "gui-set-failed" })
             end
-            
+
             global.last_built_sign[event.player_index] = nil
-            
+
             manual_frame.destroy()
         end
     elseif frame and event.element.parent == frame then
@@ -289,10 +297,10 @@ function onGuiClick(event)
         if event.element.name == "tsl-value-other" then
             frame.destroy()
 
-            manual_frame = player.gui.center.add{type="frame", name="tsl-manual-value", caption={"gui-limit-manual"}, direction="vertical"}
-            manual_frame.add{type="label", caption={"gui-limit-value"}}
-            manual_frame.add{type="textfield", name="tsl-manual-input"}
-            manual_frame.add{type="button", name="tsl-manual-ok", caption={"gui-ok"}}
+            manual_frame = player.gui.center.add { type = "frame", name = "tsl-manual-value", caption = { "gui-limit-manual" }, direction = "vertical" }
+            manual_frame.add { type = "label", caption = { "gui-limit-value" } }
+            manual_frame.add { type = "textfield", name = "tsl-manual-input" }
+            manual_frame.add { type = "button", name = "tsl-manual-ok", caption = { "gui-ok" } }
             return
         elseif event.element.name == "tsl-value-20" then
             limit_value = 20
@@ -313,24 +321,24 @@ function onGuiClick(event)
         end
 
         if not setLimitIndication(global.last_built_sign[event.player_index], limit_value) then
-            player.print({"gui-set-failed"})
+            player.print({ "gui-set-failed" })
         end
 
         global.last_built_sign[event.player_index] = nil
-        
+
         frame.destroy()
     end
 end
 
 function onPlayerJoinedGame(event)
     local player = game.players[event.player_index]
-	local frame = player.gui.center["tsl-value"]
+    local frame = player.gui.center["tsl-value"]
     local manual_frame = player.gui.center["tsl-manual-value"]
-    
+
     if frame then
         frame.destroy()
     end
-    
+
     if manual_frame then
         manual_frame.destroy()
     end
@@ -338,11 +346,11 @@ end
 
 function onPlayerLeft(event)
     global.last_built_sign[event.player_index] = nil
-end
+end]]
 
 script.on_event(defines.events.on_train_changed_state, onTrainState)
 script.on_event(defines.events.on_built_entity, onBuiltEntity)
 script.on_event(defines.events.on_robot_built_entity, onBuiltEntity)
-script.on_event(defines.events.on_gui_click, onGuiClick)
+--[[script.on_event(defines.events.on_gui_click, onGuiClick)
 script.on_event(defines.events.on_player_joined_game, onPlayerJoinedGame)
-script.on_event(defines.events.on_player_left_game, onPlayerLeft)
+script.on_event(defines.events.on_player_left_game, onPlayerLeft)]]
