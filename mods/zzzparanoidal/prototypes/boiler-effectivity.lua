d92553ac79914d1b4f3263b216077c7214e9b61f
SEO <32990223+ExpertLove@users.noreply.github.com>
2024-08-27 14:26:44 +0000

    КПД бойлеров не первышает 98%

--- mods/zzzparanoidal/prototypes/boiler-effectivity.lua
+++ mods/zzzparanoidal/prototypes/boiler-effectivity.lua
@@ -5,13 +5,6 @@
 -- т.е.           +17   +13     +10     +8
 -- или    0.6 -- 0.8 -- 1.0 -- 1.15 -- 1.25
 
-for name, value in pairs ({["boiler"]=0.6, ["boiler-2"]=0.8, ["boiler-3"]=1, ["boiler-4"]=1.15, ["boiler-5"]=1.25}) do
+for name, value in pairs ({["boiler"]=0.5, ["boiler-2"]=0.67, ["boiler-3"]=0.8, ["boiler-4"]=0.9, ["boiler-5"]=0.98}) do
   data.raw.boiler[name].energy_source.effectivity=value
 end
---[[
-data.raw.boiler["boiler"].energy_source.effectivity=0.75
-data.raw.boiler["boiler-2"].energy_source.effectivity=0.90
-data.raw.boiler["boiler-3"].energy_source.effectivity=1
-data.raw.boiler["boiler-4"].energy_source.effectivity=1.1
-data.raw.boiler["boiler-5"].energy_source.effectivity=1.25
-]]--
