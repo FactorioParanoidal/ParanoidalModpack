5d9c2cad4de7d51d31218959d95a45a5a3095d3d
SEO <32990223+ExpertLove@users.noreply.github.com>
2024-08-31 22:57:37 +0300

    НОВЫЙ МОД - Show Max Underground Distance - Показывает максимальные показатели дальности, помогающие размещать подземные ленты и трубы. (#122)

---
+++ mods/show-max-underground-distance/settings.lua
@@ -0,0 +1,15 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "show-max-underground-distance-on-hover",
+        setting_type = "startup",
+        default_value = false
+    },
+    {
+        type = "string-setting",
+        name = "show-max-underground-distance-icon",
+        setting_type = "startup",
+        default_value = "thick",
+        allowed_values = {"thick", "thin", "small"}
+    }
+})
