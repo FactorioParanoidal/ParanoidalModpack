d4a93569a75744ee6ada86bc0d2d5162ea0aebe6
SEO <32990223+ExpertLove@users.noreply.github.com>
2024-08-31 12:33:32 +0000

    УДАЛЕНИЕ мода Crafting Combinator

--- mods/crafting_combinator/migrations/0.10.8.lua
+++
@@ -1,7 +0,0 @@
-if not late_migrations then return end
-
-late_migrations["Use module chests as overflow"] = function()
-	local cc_control = require 'script.cc'
-	cc_control.on_load()
-	for _, combinator in pairs(global.cc.data) do combinator:find_chest(); end
-end
--- mods/crafting_combinator/migrations/0.15.2.lua
+++
@@ -1,9 +0,0 @@
-if not late_migrations then return end
-
-late_migrations['0.15.2'] = function()
-	local signals = require 'script.signals'
-	
-	for _, cache in pairs(global.signals.cache) do signals.cache.drop(cache.__entity); end
-	global.signals = nil
-	signals.init_global()
-end
