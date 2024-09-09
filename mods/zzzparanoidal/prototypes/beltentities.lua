ad10999b765ef79a221d9fde293a7b01ceacb444
ProTriforcer <25221392+ProTriforcer@users.noreply.github.com>
2024-09-09 10:50:26 +0000

    Починить проходимость и описания минипогрузчиков (#146)
    * Починить проходимость и описания минипогрузчиков
    
    * fix

--- mods/zzzparanoidal/prototypes/beltentities.lua
+++ mods/zzzparanoidal/prototypes/beltentities.lua
@@ -56,3 +56,59 @@ if mods.boblogistics then
 	]]
 end
 
+if mods.miniloader then
+	local base_inserter_speed = base_speed * 4
+	local base_display_speed = base_speed * 480
+	local font_open_tags = "[font=default-semibold][color=255,230,192]"
+	local font_close_tags = ":[/color][/font] "
+	local speed_desc = {"description.belt-speed"}
+	local items_desc = {"description.belt-items"}
+	local suffix = {"per-second-suffix"}
+	if settings.startup["bobmods-logistics-beltoverhaul"].value then
+		data.raw["inserter"]["basic-miniloader-inserter"].rotation_speed = 0.5 * base_inserter_speed
+		data.raw["inserter"]["basic-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 0.5 * base_display_speed, " ", items_desc, suffix}
+		data.raw["loader-1x1"]["basic-miniloader-loader"].speed = 0.5 * base_speed
+	end
+	data.raw["inserter"]["miniloader-inserter"].rotation_speed = 1 * base_inserter_speed
+	data.raw["inserter"]["miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 1 * base_display_speed, " ", items_desc, suffix}
+	data.raw["loader-1x1"]["miniloader-loader"].speed = 1 * base_speed
+	data.raw["inserter"]["fast-miniloader-inserter"].rotation_speed = 2 * base_inserter_speed
+	data.raw["inserter"]["fast-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 2 * base_display_speed, " ", items_desc, suffix}
+	data.raw["loader-1x1"]["fast-miniloader-loader"].speed = 2 * base_speed
+	data.raw["inserter"]["express-miniloader-inserter"].rotation_speed = 4 * base_inserter_speed
+	data.raw["inserter"]["express-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 4 * base_display_speed, " ", items_desc, suffix}
+	data.raw["loader-1x1"]["express-miniloader-loader"].speed = 4 * base_speed
+	if mods.boblogistics then
+		data.raw["inserter"]["turbo-miniloader-inserter"].rotation_speed = 6 * base_inserter_speed
+		data.raw["inserter"]["turbo-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 6 * base_display_speed, " ", items_desc, suffix}
+		data.raw["loader-1x1"]["turbo-miniloader-loader"].speed = 6 * base_speed
+		data.raw["inserter"]["ultimate-miniloader-inserter"].rotation_speed = 12 * base_inserter_speed
+		data.raw["inserter"]["ultimate-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 12 * base_display_speed, " ", items_desc, suffix}
+		data.raw["loader-1x1"]["ultimate-miniloader-loader"].speed = 12 * base_speed
+	end
+	
+	if settings.startup["miniloader-enable-filter"].value then
+		if settings.startup["bobmods-logistics-beltoverhaul"].value then
+			data.raw["inserter"]["basic-filter-miniloader-inserter"].rotation_speed = 0.5 * base_inserter_speed
+			data.raw["inserter"]["basic-filter-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 0.5 * base_display_speed, " ", items_desc, suffix}
+			data.raw["loader-1x1"]["basic-filter-miniloader-loader"].speed = 0.5 * base_speed
+		end
+		data.raw["inserter"]["filter-miniloader-inserter"].rotation_speed = 1 * base_inserter_speed
+		data.raw["inserter"]["filter-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 1 * base_display_speed, " ", items_desc, suffix}
+		data.raw["loader-1x1"]["filter-miniloader-loader"].speed = 1 * base_speed
+		data.raw["inserter"]["fast-filter-miniloader-inserter"].rotation_speed = 2 * base_inserter_speed
+		data.raw["inserter"]["fast-filter-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 2 * base_display_speed, " ", items_desc, suffix}
+		data.raw["loader-1x1"]["fast-filter-miniloader-loader"].speed = 2 * base_speed
+		data.raw["inserter"]["express-filter-miniloader-inserter"].rotation_speed = 4 * base_inserter_speed
+		data.raw["inserter"]["express-filter-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 4 * base_display_speed, " ", items_desc, suffix}
+		data.raw["loader-1x1"]["express-filter-miniloader-loader"].speed = 4 * base_speed
+		if mods.boblogistics then
+			data.raw["inserter"]["turbo-filter-miniloader-inserter"].rotation_speed = 6 * base_inserter_speed
+			data.raw["inserter"]["turbo-filter-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 6 * base_display_speed, " ", items_desc, suffix}
+			data.raw["loader-1x1"]["turbo-filter-miniloader-loader"].speed = 6 * base_speed
+			data.raw["inserter"]["ultimate-filter-miniloader-inserter"].rotation_speed = 12 * base_inserter_speed
+			data.raw["inserter"]["ultimate-filter-miniloader-inserter"].localised_description = {"", font_open_tags, speed_desc, font_close_tags, 12 * base_display_speed, " ", items_desc, suffix}
+			data.raw["loader-1x1"]["ultimate-filter-miniloader-loader"].speed = 12 * base_speed
+		end
+	end
+end
