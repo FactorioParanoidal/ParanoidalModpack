eae68a7acecfe7aba7e0a2fecbb13e6380dedda5
SKProCH <29896317+SKProCH@users.noreply.github.com>
2024-10-23 00:22:12 +0300

    Update settings.lua

--- mods/SingleColorTerrain/settings.lua
+++ mods/SingleColorTerrain/settings.lua
@@ -6,8 +6,7 @@ data:extend(
 type = "bool-setting",
 name = "white-concrete",
 setting_type = "startup",
-default_value = "true",
-allowed_values = {"true", "false"},
+default_value = true,
 localised_name = "White Concrete?",
 order = "e"
 },
@@ -17,8 +16,7 @@ order = "e"
 type = "bool-setting",
 name = "white-stone",
 setting_type = "startup",
-default_value = "true",
-allowed_values = {"true", "false"},
+default_value = true,
 localised_name = "White Stone Path?",
 order = "d"
 },
@@ -28,8 +26,7 @@ order = "d"
 type = "bool-setting",
 name = "no-decor",
 setting_type = "startup",
-default_value = "false",
-allowed_values = {"true", "false"},
+default_value = false,
 localised_name = "No Decorations?",
 order = "b"
 },
@@ -41,8 +38,7 @@ order = "b"
 type = "bool-setting",
 name = "no-clouds",
 setting_type = "startup",
-default_value = "true",
-allowed_values = {"true", "false"},
+default_value = true,
 localised_name = "No Clouds?",
 order = "c"
 },
