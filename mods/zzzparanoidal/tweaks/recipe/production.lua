7428bc3a66e3fd7d77b5fe3e73f75d1b5ae3fcd9
xjomanx <joman@mail.ru>
2026-05-22 12:24:11 +0300

    Fix рецептов 1. сделал файлы для разных групп рецептов 2. пофиксил рецепты burner-mining-drill steam-engine motor (с переносом категории) electric-motor (с переносом категории) iron-gear-wheel iron-stick

---
+++ mods/zzzparanoidal/tweaks/recipe/production.lua
@@ -0,0 +1,8 @@
+local set_ingredients = bobmods.lib.recipe.set_ingredients
+
+set_ingredients("burner-mining-drill", {
+	{ type = "item", name = "iron-gear-wheel", amount = 4 },
+	{ type = "item", name = "motor", amount = 1 },
+	{ type = "item", name = "mining-drill-bit-mk0", amount = 1 },
+	{ type = "item", name = "stone-furnace", amount = 1 },
+})
