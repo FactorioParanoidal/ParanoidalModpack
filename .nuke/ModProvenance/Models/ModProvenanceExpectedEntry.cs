3be02b42b580a5950d657f2ba6529197343086db
SKProCH <29896317+SKProCH@users.noreply.github.com>
2026-07-29 13:59:39 +0300

    Обрабатывает локальные моды без хеширования (#326)
    Обрабатывает локальные моды без хеширования, обновляет логику проверки ModProvenance

--- .nuke/ModProvenance/Models/ModProvenanceExpectedEntry.cs
+++ .nuke/ModProvenance/Models/ModProvenanceExpectedEntry.cs
@@ -3,5 +3,5 @@ using FactorioParanoidal.FactorioMods.Mods;
 
 namespace ModProvenance.Models;
 
-public sealed record ModProvenanceExpectedEntry(string Name, FactorioVersion Version, string ContentHash,
+public sealed record ModProvenanceExpectedEntry(string Name, FactorioVersion Version, string? ContentHash,
     string? MetadataHash = null);
